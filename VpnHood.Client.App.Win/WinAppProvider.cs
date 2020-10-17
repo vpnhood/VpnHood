4f4d679d52be37ab7ef783f28e3bb15ef9b72a48
trudy <trudy@vpnhood.com>
2020-10-17 12:54:51 -0700

    add version to token; temporary remove partial SSL

--- VpnHood.Client.App.Win/WinAppProvider.cs
+++ VpnHood.Client.App.Win/WinAppProvider.cs
@@ -6,11 +6,6 @@ namespace VpnHood.Client.App
 {
     class WinAppProvider : IAppProvider
     {
-        public IDevice Device { get; }
-        
-        public WinAppProvider()
-        {
-            Device = new WinDivertDevice();            
-        }
+        public IDevice Device { get; } = new WinDivertDevice();
     }
 }

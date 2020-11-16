f4780d546f69fa6ca62a59b373647c6060bcea74
trudy <trudy@vpnhood.com>
2020-11-15 19:27:47 -0800

    add run.ps1 to publish

--- VpnHood.Server.App.NetCore/_publish.ps1
+++ VpnHood.Server.App.NetCore/_publish.ps1
@@ -1,13 +1,17 @@
 # paths
+$launchFilePath="VpnHoodServer.exe"
+$projectDir = $PSScriptRoot
+$credentials = (Get-Content "$projectDir\..\..\.user\credentials.json" | Out-String | ConvertFrom-Json)
+$versionBase = (Get-Content "$projectDir\..\version.json" | Out-String | ConvertFrom-Json)
+
 $projectDir=$PSScriptRoot
 $publishDir="$projectDir\bin\release\publish"
 $publishJsonFile = "$publishDir\publish.json"
-$launchFilePath="VpnHoodServer.exe"
-$ftpCredential=(Get-Content "$projectDir\_publish.credential" | Out-String | ConvertFrom-Json).FtpCredential
-$ftpAddress=(Get-Content "$projectDir\_publish.credential" | Out-String | ConvertFrom-Json).FtpAddress
-$versionBaseDate = [datetime]::new(2020, 1, 1)
-$versionMajor = 1;
-$versionMinor = 0;
+$ftpCredential=$credentials.ServerFtpCredential
+$ftpAddress=$credentials.ServerFtpAddress
+$versionBaseDate = [datetime]::new($versionBase.BaseYear, 1, 1)
+$versionMajor = $versionBase.Major
+$versionMinor = $versionBase.Minor
 
 # find current version
 $timeSpan = [datetime]::Now - $versionBaseDate
@@ -18,15 +22,29 @@ $json = @{Version=$version.ToString(4); LaunchPath=$Version.ToString(4) + "/$lau
 $outDir = "$publishDir\" + $json.Version
 
 # publish 
+Write-Host 
+Write-Host "*** Publishing..." -BackgroundColor Blue
 $versionParam=$version.ToString(4)
-dotnet publish "$projectDir" -c "Release" --output "$outDir" --framework net5.0 --no-self-contained /p:Version=$versionParam
+dotnet publish "$projectDir" -c "Release" --output "$outDir" --framework net5.0 --runtime win-x64 --no-self-contained /p:Version=$versionParam
 $json | ConvertTo-Json -depth 100 | Out-File $publishJsonFile
 if ($LASTEXITCODE -gt 0)
 {
     Throw "The publish exited with error code: " + $lastexitcode
 }
 
+# create run.ps1
+$run = @'
+$publish = (Get-Content "$PSScriptRoot\publish.json" | Out-String | ConvertFrom-Json)
+$launhcPath = "$PSScriptRoot\" + $publish.LaunchPath
+Start-Process -FilePath $launhcPath -NoNewWindow
+'@
+$runPath="$publishDir\run.ps1"
+
+Set-Content -Path $runPath -Value $run
+
 # upload publish folder
+Write-Host 
+Write-Host "*** Uploading..." -BackgroundColor Blue
 $files = get-childitem $outDir -recurse -File
 foreach ($file in $files)
 {
@@ -41,4 +59,9 @@ foreach ($file in $files)
 }
 
 # upload publish json
+curl.exe "$ftpAddress/run.ps1" -u "$ftpCredential" -T "$runPath" --ssl
 curl.exe "$ftpAddress/publish.json" -u "$ftpCredential" -T "$publishJsonFile" --ssl
+ if ($LASTEXITCODE -gt 0)
+{
+    Throw "curl exited with error code: " + $lastexitcode
+}

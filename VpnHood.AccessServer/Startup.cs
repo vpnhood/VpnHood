78b4725eb4e6a946962bc345a3ef909b420ed2be
trudy <trudy@vpnhood.com>
2020-11-15 18:06:36 -0800

    Add Swagger

--- VpnHood.AccessServer/Startup.cs
+++ VpnHood.AccessServer/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using VpnHood.AccessServer.Auth;
 
@@ -39,6 +40,10 @@ namespace VpnHood.AccessServer
                 services.AddAppAuthentication(App.AuthProviderItems);
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication2", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +52,8 @@ namespace VpnHood.AccessServer
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication2 v1"));
             }
 
             //before UseAuthentication

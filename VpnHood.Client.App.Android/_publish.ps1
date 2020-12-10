576c74a2dc28d2df8b47ad727f57bb60d28beb2e
trudy <trudy@vpnhood.com>
2020-12-09 19:12:53 -0800

    Change Androd publish script

--- VpnHood.Client.App.Android/_publish.ps1
+++ VpnHood.Client.App.Android/_publish.ps1
@@ -6,15 +6,20 @@ $msbuild = Join-Path ${Env:ProgramFiles(x86)} "Microsoft Visual Studio\2019\Comm
 $projectFile = (Get-ChildItem -path $projectDir -file -Filter "*.csproj").FullName;
 
 $credentials = (Get-Content "$solutionDir\..\.user\credentials.json" | Out-String | ConvertFrom-Json);
-$versionBase = (Get-Content "$solutionDir\Pub\version.json" | Out-String | ConvertFrom-Json);
-$versionBaseDate = [datetime]::new($versionBase.BaseYear, 1, 1);
-$versionMajor = $versionBase.Major;
-$versionMinor = $versionBase.Minor;
 
-# find current version
-$timeSpan = [datetime]::Now - $versionBaseDate;
-$version = [version]::new($versionMajor, $versionMinor, $timeSpan.Days, $timeSpan.Hours * 60 + $timeSpan.Minutes);
-$versionParam = $version.ToString();
+# version
+$versionFile = Join-Path $solutionDir "\Pub\version.json"
+$versionJson = (Get-Content $versionFile | Out-String | ConvertFrom-Json);
+$bumpTime = [datetime]::Parse($versionJson.BumpTime);
+$isVersionBumped=( ((Get-Date)-$bumpTime).TotalMinutes -ge 5);
+if ( $isVersionBumped )
+{
+	$versionJson.Build = $versionJson.Build + 1;
+	$versionJson.BumpTime = [datetime]::UtcNow.ToString("o");
+	$versionJson | ConvertTo-Json -depth 10 | Out-File $versionFile;
+}
+$version=[version]::new($versionJson.Major, $versionJson.Minor, $versionJson.Build);
+$versionParam = $version.ToString(3);
 
 # android
 $keystore = Join-Path "$solutionDir\..\.user\" $credentials.Android.KeyStoreFile
@@ -22,12 +27,31 @@ $keystorePass = $credentials.Android.KeyStorePass
 $keystoreAlias = $credentials.Android.KeyStoreAlias
 $apkPublishDir = Join-Path $projectDir "bin\release\publish";
 $signedApk= Join-Path $apkPublishDir "com.vpnhood.client.android-signed.apk"
+$manifestFile = Join-Path $projectDir "Properties\AndroidManifest.xml";
 
+# set android version
+$xmlDoc = [xml](Get-Content $manifestFile)
+$xmlDoc.manifest.versionCode = $version.Build.ToString()
+$xmlDoc.save($manifestFile);
+
+# bundle (aab)
 & $msbuild $projectFile /p:Configuration=Release /t:Clean
 & $msbuild $projectFile /p:Configuration=Release /t:SignAndroidPackage /p:Version=$versionParam /p:ArchiveOnBuild=true `
 	/p:AndroidKeyStore=True /p:AndroidSigningKeyStore=$keystore /p:AndroidSigningKeyAlias=$keystoreAlias /p:AndroidSigningKeyPass=$keystorePass /p:AndroidSigningStorePass=$keystorePass 
 
 
+# apk
 & $msbuild $projectFile /p:Configuration=Release /t:Clean /p:OutputPath="bin\ReleaseApk"
 & $msbuild $projectFile /p:Configuration=Release /t:SignAndroidPackage  /p:Version=$versionParam /p:OutputPath="bin\ReleaseApk" /p:AndroidPackageFormat="apk" `
 	/p:AndroidSigningKeyStore=$keystore /p:AndroidSigningKeyAlias=$keystoreAlias /p:AndroidSigningStorePass=$keystorePass /p:JarsignerTimestampAuthorityUrl="https://freetsa.org/tsr"
+
+
+# report version
+if ($isVersionBumped)
+{
+	Write-Host "New version: $versionParam" -ForegroundColor GREEN
+}
+else
+{
+	Write-Host "OLD Version: $versionParam" -ForegroundColor Yellow -BackgroundColor Red
+}

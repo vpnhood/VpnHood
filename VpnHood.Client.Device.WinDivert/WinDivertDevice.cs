7256cee557a9c00cdc03ae73d400ff52fbb51b5c
trudy <trudy@vpnhood.com>
2020-12-16 17:12:16 -0800

    Fix loading WinDivert on Test

--- VpnHood.Client.Device.WinDivert/WinDivertDevice.cs
+++ VpnHood.Client.Device.WinDivert/WinDivertDevice.cs
@@ -9,22 +9,8 @@ namespace VpnHood.Client.Device.WinDivert
 #pragma warning disable 0067
         public event EventHandler OnStartAsService;
 #pragma warning restore 0067
-
-        private void SetWinDivertDllFolder()
-        {
-            var dllFolderName = Environment.Is64BitOperatingSystem ? "x64" : "x86";
-            var assemblyFolder = Path.GetDirectoryName(typeof(WinDivertDevice).Assembly.Location);
-            var dllFolder = Path.Combine(assemblyFolder, dllFolderName);
-
-            string path = Environment.GetEnvironmentVariable("PATH");
-            if (path.IndexOf(dllFolder + ";") == -1)
-                Environment.SetEnvironmentVariable("PATH", dllFolder + ";" + path);
-        }
-
         public Task<IPacketCapture> CreatePacketCapture()
         {
-            SetWinDivertDllFolder();
-
             var res = (IPacketCapture)new WinDivertPacketCapture();
             return Task.FromResult(res);
         }

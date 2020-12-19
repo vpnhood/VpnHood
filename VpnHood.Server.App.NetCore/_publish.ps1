6dcffb67600779664dd8f5c784dea8495cd17421
trudy <trudy@vpnhood.com>
2020-12-19 02:35:52 -0800

    AppUpdater throw error if UpdateUrl in publish.json was empty string

--- VpnHood.Server.App.NetCore/_publish.ps1
+++ VpnHood.Server.App.NetCore/_publish.ps1
@@ -2,7 +2,6 @@ $packageName = "VpnHoodServer";
 
 . "$PSScriptRoot\..\Pub\PublishApp.ps1" `
 	$PSScriptRoot -withLauncher `
-	-packageName "$packageName" `
+	-packageName $packageName `
 	-updateUrl "https://github.com/vpnhood/VpnHood/releases/latest/download/$packageName.json" `
-	-packageDownloadUrl "https://github.com/vpnhood/VpnHood/releases/latest/download/$packageName.zip" `
-
+	-packageDownloadUrl "https://github.com/vpnhood/VpnHood/releases/latest/download/$packageName.zip"

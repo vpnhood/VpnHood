0c1e7e53b785aa8e1d5d0c904d5a91998519d85a
trudy <trudy@vpnhood.com>
2020-12-19 02:39:43 -0800

    fix publish script; update nugets

--- VpnHood.AccessServer/_publish.ps1
+++ VpnHood.AccessServer/_publish.ps1
@@ -1,1 +1,5 @@
-& "$PSScriptRoot\..\..\VpnHood\Pub\PublishApp.ps1" -projectDir $PSScriptRoot -launcher $true
+$packageName = "VpnHood-AccessServer";
+
+. "$PSScriptRoot\..\..\VpnHood\Pub\PublishApp.ps1" `
+	-projectDir $PSScriptRoot -withLauncher `
+	-packageName $packageName

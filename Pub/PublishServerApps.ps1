29d52c63503863a0dce31e3031d3ff69678a0a4c
trudy <trudy@vpnhood.com>
2020-12-22 22:42:21 -0800

    Some publish script

---
+++ Pub/PublishServerApps.ps1
@@ -0,0 +1,16 @@
+param([switch]$bump, [switch]$dis, [switch]$ftp) 
+. "$PSScriptRoot/Common.ps1" -bump:$bump;
+
+# clean solution
+& $msbuild "$solutionDir" /p:Configuration=Release /t:Clean;
+$noclean = $true;
+
+# publish server
+Remove-Item "$packagesDir/server" -ErrorAction Ignore -Recurse;
+& "$solutionDir/VpnHood.Server.App.Net/_publish.ps1" -ftp:$ftp;
+
+# upload
+if ($dis)
+{
+	& "$PSScriptRoot/PublishToGitHub.ps1";
+}

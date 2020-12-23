f3b6d4252b4099cb906e8e531d0b4e083e0e7655
trudy <trudy@vpnhood.com>
2020-12-22 21:51:42 -0800

    Publish v1.1.117

--- VpnHood.Server.App.NetCore/_publish.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = "VpnHoodServer";
-
-. "$PSScriptRoot\..\Pub\PublishApp.ps1" `
-	$PSScriptRoot -withLauncher `
-	-packageName $packageName `
-	-updateUrl "https://github.com/vpnhood/VpnHood/releases/latest/download/$packageName.json" `
-	-packageDownloadUrl "https://github.com/vpnhood/VpnHood/releases/latest/download/$packageName.zip"

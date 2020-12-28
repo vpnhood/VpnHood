f13a7dba96589ab858ff5c219f44e94a46d267c7
trudy <trudy@vpnhood.com>
2020-12-28 02:00:56 -0800

    Update DB for UsageLog

--- VpnHood.AccessServer.Db/dbo/Tables/AccessUsage.sql
+++ VpnHood.AccessServer.Db/dbo/Tables/AccessUsage.sql
@@ -1,29 +1,10 @@
 ﻿CREATE TABLE [dbo].[AccessUsage] (
     [accessTokenId]        UNIQUEIDENTIFIER NOT NULL,
     [clientIp]             VARCHAR (20)     NOT NULL,
-    [sentTraffic]          BIGINT           CONSTRAINT [DF_AccessUsage_sentByteCount] DEFAULT ((0)) NOT NULL,
-    [receivedTraffic]      BIGINT           CONSTRAINT [DF_AccessUsage_receivedByteCount] DEFAULT ((0)) NOT NULL,
+    [cycleSentTraffic]     BIGINT           CONSTRAINT [DF_AccessUsage_cycleSentByteCount] DEFAULT ((0)) NOT NULL,
+    [cycleReceivedTraffic] BIGINT           CONSTRAINT [DF_AccessUsage_cycleReceivedByteCount] DEFAULT ((0)) NOT NULL,
     [totalSentTraffic]     BIGINT           CONSTRAINT [DF_AccessUsage_totalSentBytes] DEFAULT ((0)) NOT NULL,
     [totalReceivedTraffic] BIGINT           CONSTRAINT [DF_AccessUsage_totalRecievedBytes] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_AccessUsage] PRIMARY KEY CLUSTERED ([accessTokenId] ASC, [clientIp] ASC),
     CONSTRAINT [FK_AccessUsage_accessTokenId] FOREIGN KEY ([accessTokenId]) REFERENCES [dbo].[AccessToken] ([accessTokenId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
-
-
-GO
-
-
-
-
-
-GO
-
-
-
-
-
-GO
-
-

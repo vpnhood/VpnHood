8c2b6e72804203b1391b4945aa34c4bbec5ac34d
trudy <trudy@vpnhood.com>
2021-01-07 13:07:07 -0800

    fix disposeing natitem

--- VpnHood.Server.App.Net/AppSettings.cs
+++ VpnHood.Server.App.Net/AppSettings.cs
@@ -9,5 +9,6 @@ namespace VpnHood.Server.App
         public ushort Port { get; set; } = 443;
         public bool IsAnonymousTrackerEnabled { get; set; } = true;
         public string SslCertificatesPassword { get; set; }
+        public bool IsDebugMode { get; set; }
     }
 }

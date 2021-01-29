2be0fa0f8170e39a981db6c53e88f25b7463d87d
trudy <trudy@vpnhood.com>
2021-01-28 19:49:24 -0800

    fix saving default connection fix saving client id

--- VpnHood.Client.App/AppSettings.cs
+++ VpnHood.Client.App/AppSettings.cs
@@ -38,6 +38,7 @@ namespace VpnHood.Client.App
                 {
                     SettingsFilePath = settingsFilePath
                 };
+                ret.Save();
                 return ret;
             }
         }

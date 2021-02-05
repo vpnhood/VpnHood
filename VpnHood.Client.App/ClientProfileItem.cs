2d480a2b6c3ab7ff7506a62e1844000892769518
trudy <trudy@vpnhood.com>
2021-02-04 19:36:32 -0800

    update serves page

--- VpnHood.Client.App/ClientProfileItem.cs
+++ VpnHood.Client.App/ClientProfileItem.cs
@@ -1,9 +1,11 @@
-﻿using VpnHood.Common;
+﻿using System;
+using VpnHood.Common;
 
 namespace VpnHood.Client.App
 {
     public class ClientProfileItem
     {
+        public Guid Id => ClientProfile.ClientProfileId;
         public ClientProfile ClientProfile { get; set; }
         public Token Token { get; set; }
     }

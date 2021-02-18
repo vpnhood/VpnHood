1e7ab7bb5dae463718285afe54a73c1b1f7cee70
trudy <trudy@vpnhood.com>
2021-02-17 16:43:44 -0800

    add linux installstion

--- Pub/PublishToGitHub.ps1
+++ Pub/PublishToGitHub.ps1
@@ -1,6 +1,9 @@
 . "$PSScriptRoot/Common.ps1"
 
-$tag="v$versionParam-beta"
+# server install-linux.sh
+echo "Make Server installation script for this release"
+$linuxScript = (Get-Content -Path "$solutionDir/VpnHood.Server.App.Net/Install/install-linux.sh" -Raw).Replace('$installUrlParam', "https://github.com/vpnhood/VpnHood/releases/download/$versionTag/VpnHoodServer.zip");
+$linuxScript  | Out-File -FilePath "$packagesServerDir/install-linux.sh" -Encoding utf8 -Force;
 
 # update CHANGELOG
 $text = Get-Content "$solutionDir/CHANGELOG.md" -Raw;
@@ -9,7 +12,7 @@ $changeLog = $text -replace "# Upcoming", "# v$versionParam";
 $changeLog  | Out-File -FilePath "$solutionDir/CHANGELOG.md" -Encoding utf8 -Force;
 
 # create release note
-$releaseNote = $text -replace "# Upcoming", "$tag`n";
+$releaseNote = $text -replace "# Upcoming", "$versionTag`n";
 $releaseNote = $releaseNote.SubString(0, $releaseNote.IndexOf("`n# "));
 # $releaseNote += "To see a list of all changes visit: [Changelog](https://github.com/vpnhood/VpnHood/blob/main/CHANGELOG.md)";
 $releaseNote  | Out-File -FilePath "$packagesRootDir/ReleaseNote.txt" -Encoding utf8 -Force;
@@ -23,12 +26,14 @@ git --git-dir=$gitDir --work-tree=$solutionDir push;
 # publish using github CLI: https://github.com/github/hub
 # Use --prerelease for prerelease!
 hub --git-dir=$gitDir --work-tree=$solutionDir release create `
+	-p `
 	-a $packagesClientDir/VpnHoodClient-Android.apk `
 	-a $packagesClientDir/VpnHoodClient-win.exe  `
 	-a $packagesClientDir/VpnHoodClient-win.json `
 	-a $packagesClientDir/VpnHoodClient-win.zip `
 	-a $packagesServerDir/VpnHoodServer.json `
 	-a $packagesServerDir/VpnHoodServer.zip `
+	-a $packagesServerDir/install-linux.sh `
 	-F $packagesRootDir/ReleaseNote.txt `
-	"$tag";
+	"$versionTag";
 

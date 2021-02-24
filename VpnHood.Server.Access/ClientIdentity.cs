7ca5a7d226db684e821dbcd18947c7523c9789a0
trudy <trudy@vpnhood.com>
2021-02-24 13:32:29 -0800

    Send client version to access server

--- VpnHood.Server.Access/ClientIdentity.cs
+++ VpnHood.Server.Access/ClientIdentity.cs
@@ -8,5 +8,6 @@ namespace VpnHood.Server
         public string UserToken { get; set; }
         public Guid ClientId { get; set; }
         public string ClientIp { get; set; }
+        public string ClientVersion { get; set; }
     }
 }

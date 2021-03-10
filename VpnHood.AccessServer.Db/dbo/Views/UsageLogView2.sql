89e35705d5a5bb38dd409ffceded0f60584466af
trudy <trudy@vpnhood.com>
2021-03-09 22:51:20 -0800

    update Sql Log view

--- VpnHood.AccessServer.Db/dbo/Views/UsageLogView2.sql
+++ VpnHood.AccessServer.Db/dbo/Views/UsageLogView2.sql
@@ -4,13 +4,9 @@
 
 
 
-CREATE VIEW [dbo].[UsageLogView]
+CREATE VIEW [dbo].[UsageLogView2]
 AS
-with T(usageLogId) as
-(
-	select max(usageLogId) from UsageLog group by clientId
-)
-SELECT TOP 10000 SUBSTRING(convert(nvarchar(50), accessTokenId), 1, 4) AS accessTokenId
+SELECT TOP 1000 SUBSTRING(convert(nvarchar(50), accessTokenId), 1, 4) AS accessTokenId
 	, SUBSTRING(convert(nvarchar(50),clientId), 1, 4) AS clientId 
 	, clientIp --
 	, clientVersion --
@@ -24,6 +20,6 @@ SELECT TOP 10000 SUBSTRING(convert(nvarchar(50), accessTokenId), 1, 4) AS access
     , dbo.Convert_formatTraffic(U.totalReceivedTraffic) AS totalReceivedTraffic --
 	, dbo.Convert_formatTraffic(U.totalSentTraffic + U.totalReceivedTraffic) AS totalTraffic --
 	, CONVERT(NVARCHAR, dbo.Convert_toLocalTime(createdTime), 100) as createdTime,
-	U.usageLogId
+	usageLogId
   FROM  dbo.UsageLog AS U
-	inner join T on U.usageLogId=T.usageLogId order by U.usageLogId desc
+		ORDER BY U.usageLogId DESC

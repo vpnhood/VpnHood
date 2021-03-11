64ca6d16f15414625a0210733a7d14d91237ff98
trudy <trudy@vpnhood.com>
2021-03-11 03:02:14 -0800

    Windows: Bypass local network from tunneling

--- VpnHood.Client/ClientOptions.cs
+++ VpnHood.Client/ClientOptions.cs
@@ -10,7 +10,7 @@ namespace VpnHood.Client
         /// <summary>
         /// a never used ip that must be outside the machine
         /// </summary>
-        public IPAddress TcpProxyLoopbackAddress { get; set; } = IPAddress.Parse("10.255.255.255");
+        public IPAddress TcpProxyLoopbackAddress { get; set; } = IPAddress.Parse("11.0.0.0");
         public IPAddress DnsAddress { get; set; } =  IPAddress.Parse("8.8.8.8");
         public bool LeavePacketCaptureOpen { get; set; } = false;
         public int Timeout { get; set; } = 60 * 1000;

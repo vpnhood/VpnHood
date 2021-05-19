b037217283e93b46f3fcc4b8eb21df8412bc79fb
trudy <trudy@vpnhood.com>
2021-05-19 15:39:35 -0700

    Publish v1.1.240

--- Pub/PublishToGitHub.ps1
+++ Pub/PublishToGitHub.ps1
@@ -28,10 +28,9 @@ git --git-dir=$gitDir --work-tree=$solutionDir push;
 hub --git-dir=$gitDir --work-tree=$solutionDir release create `
 	-a $packagesClientDir/VpnHoodClient-Android.apk `
 	-a $packagesClientDir/VpnHoodClient-win.exe  `
-	-a $packagesClientDir/VpnHoodClient-win.zip `
 	-a $packagesServerDir/VpnHoodServer.json `
-	-a $packagesServerDir/VpnHoodServer.zip `
 	-a $packagesServerDir/install-linux.sh `
+	-m "sss"
 	-F $packagesRootDir/ReleaseNote.txt `
 	"$versionTag";
 

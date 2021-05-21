77a7681913e08dceb99ce6009e03f28ade1dcc50
trudy <trudy@vpnhood.com>
2021-05-21 15:29:50 -0700

    Publish v1.1.242

--- VpnHood.Client.App.Win.Setup/_publish.ps1
+++ VpnHood.Client.App.Win.Setup/_publish.ps1
@@ -2,6 +2,7 @@
 $advinstallerFile = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Caphyon\Advanced Installer" -Name "InstallRoot").InstallRoot;
 $advinstallerFile = Join-Path $advinstallerFile "bin\x86\AdvancedInstaller.com";
 $packageFile = Join-Path $PSScriptRoot "Release\VpnHoodClient-win.exe";
+$updaterConfigFile= Join-Path $PSScriptRoot "Release\VpnHoodClient-win.txt";
 
 Write-Host;
 Write-Host "*** Creating ClientSetup..." -BackgroundColor Blue -ForegroundColor White;
@@ -10,6 +11,28 @@ $aipFile= Join-Path $PSScriptRoot "VpnHood.Client.App.Win.Setup.aip";
 
 & $advinstallerFile /build $aipFile
 
+
+# Create Updater Config File
+$str=";aiu;
+
+[Update]
+Name = VpnHood
+ProductVersion = $versionParam
+URL = https://github.com/vpnhood/VpnHood/releases/download/$versionTag/VpnHoodClient-win.exe
+Size = $((Get-Item $packageFile).length)
+SHA256 = $((Get-FileHash $packageFile -Algorithm SHA256).Hash)
+MD5 = $((Get-FileHash $packageFile -Algorithm MD5).Hash)
+ServerFileName = VpnHoodClient-win.exe
+Flags = NoRedetect
+RegistryKey = HKUD\Software\VpnHood\VpnHood\Version
+Version = $versionParam
+UpdatedApplications = VpnHood(1.0-$versionParam)
+Description = <a href=""https://github.com/vpnhood/VpnHood/blob/main/CHANGELOG.md"">Release note</a>
+";
+$str | Out-File -FilePath $updaterConfigFile
+
+
 #####
 # copy to solution output
 Copy-Item -path $packageFile -Destination "$packagesClientDir\" -Force
+Copy-Item -path $updaterConfigFile -Destination "$packagesClientDir\" -Force
--- VpnHood.Client.Device.Android/_publish.ps1
+++ VpnHood.Client.Device.Android/_publish.ps1
@@ -16,11 +16,14 @@ Write-Host "*** Creating the nuget..." -BackgroundColor Blue -ForegroundColor Wh
  & $nuget pack $nugetSpec -version $versionParam -Symbols -BasePath $projectDir -OutputDirectory $publishDir -NonInteractive
 
 # publish nuget
-Write-Host
-Write-Host "*** Publishing..." -BackgroundColor Blue
-$packageFile = Join-Path $publishDir "$packageId.$versionParam.nupkg"
-dotnet nuget push $packageFile --api-key $nugetApiKey --source https://api.nuget.org/v3/index.json
-if ($LASTEXITCODE -gt 0) { Write-Host ("The publish exited with error code: " + $lastexitcode) -ForegroundColor Red;  }
+if (!$noPushNuget)
+{
+	Write-Host
+	Write-Host "*** Publishing..." -BackgroundColor Blue
+	$packageFile = Join-Path $publishDir "$packageId.$versionParam.nupkg"
+	dotnet nuget push $packageFile --api-key $nugetApiKey --source https://api.nuget.org/v3/index.json
+	if ($LASTEXITCODE -gt 0) { Write-Host ("The publish exited with error code: " + $lastexitcode) -ForegroundColor Red;  }
+}
 
 # report version
 ReportVersion

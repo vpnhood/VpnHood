62c0249dbe115a666641f2531f21f7c58b2cb2af
trudy <trudy@vpnhood.com>
2021-06-13 10:54:34 -0700

    fix UdpProxy

--- VpnHood.Client.App.UI/VpnHoodAppUI.cs
+++ VpnHood.Client.App.UI/VpnHoodAppUI.cs
@@ -63,6 +63,7 @@ namespace VpnHood.Client.App.UI
 
             Url = $"http://{Util.GetFreeEndPoint(IPAddress.Loopback, DefaultPort)}";
             _server = CreateWebServer(Url, GetSpaPath());
+            Swan.Logging.Logger.UnregisterLogger<Swan.Logging.ConsoleLogger>();
             return _server.RunAsync();
         }
 

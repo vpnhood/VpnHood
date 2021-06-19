74f9922afe66d98027311b8a36541aa9c0705c2f
trudy <trudy@vpnhood.com>
2021-06-18 18:43:07 -0700

    Publish v1.2.248

--- Samples/VpnHood.Samples.SimpleClient.Win/Program.cs
+++ Samples/VpnHood.Samples.SimpleClient.Win/Program.cs
@@ -15,9 +15,9 @@ namespace VpnHood.Samples.SimpleClient.Win
             Console.WriteLine("Hello VpnClient!");
 
             // clientId should be generated for each client
-            var clientId = Guid.Parse("7BD6C156-EEA3-43D5-90AF-B118FE47ED0A");
 
             // accessKey must obtain from the server
+            var clientId = Guid.Parse("7BD6C156-EEA3-43D5-90AF-B118FE47ED0A");
             var accessKey = "vh://eyJuYW1lIjoiUHVibGljIFNlcnZlciIsInYiOjEsInNpZCI6MTEsInRpZCI6IjEwNDczNTljLWExMDctNGU0OS04NDI1LWMwMDRjNDFmZmI4ZiIsInNlYyI6IlRmK1BpUTRaS1oyYW1WcXFPNFpzdGc9PSIsImRucyI6Im1vLmdpd293eXZ5Lm5ldCIsImlzdmRucyI6ZmFsc2UsInBraCI6Ik1Da3lsdTg0N2J5U0Q4bEJZWFczZVE9PSIsImNoIjoiM2dYT0hlNWVjdWlDOXErc2JPN2hsTG9rUWJBPSIsImVwIjpbIjUxLjgxLjgxLjI1MDo0NDMiXSwicGIiOnRydWUsInVybCI6Imh0dHBzOi8vd3d3LmRyb3Bib3guY29tL3MvaG1oY2g2YjA5eDdmdXgzL3B1YmxpYy5hY2Nlc3NrZXk/ZGw9MSJ9";
             var token = Token.FromAccessKey(accessKey);
 

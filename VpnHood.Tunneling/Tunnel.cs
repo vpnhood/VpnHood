1b27d0a002d3d57ab17e9578726084e02ddb2c96
trudy <trudy@vpnhood.com>
2021-06-25 18:36:59 -0700

    optimise tunnel's stream channel  add/remove

--- VpnHood.Tunneling/Tunnel.cs
+++ VpnHood.Tunneling/Tunnel.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using PacketDotNet;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -15,21 +14,35 @@ namespace VpnHood.Tunneling
         private readonly int _maxQueueLengh = 100;
         private readonly EventWaitHandle _packetQueueAddedEvent = new(false, EventResetMode.ManualReset);
         private readonly EventWaitHandle _packetQueueRemovedEvent = new(false, EventResetMode.AutoReset);
-        private readonly object _channelListObject = new();
+        private readonly object _channelListLock = new();
         private readonly object _packetQueueLock = new();
         private readonly Timer _timer;
         private readonly int _mtuWithFragment = TunnelUtil.MtuWithFragmentation;
         private readonly int _mtuNoFragment = TunnelUtil.MtuWithoutFragmentation;
-
-        private ILogger Logger => VhLogger.Instance;
-        public IChannel[] StreamChannels { get; private set; } = new IChannel[0];
-        public IDatagramChannel[] DatagramChannels { get; private set; } = new IDatagramChannel[0];
+        private readonly HashSet<IChannel> _streamChannels = new();
+        
+        public IDatagramChannel[] DatagramChannels { get; private set; } = Array.Empty<IDatagramChannel>();
 
         private long _receivedByteCount;
-        public long ReceivedByteCount => _receivedByteCount + StreamChannels.Sum(x => x.ReceivedByteCount) + DatagramChannels.Sum(x => x.ReceivedByteCount); //todo: lock
+        public long ReceivedByteCount
+        {
+            get
+            {
+                lock (_channelListLock)
+                    return _receivedByteCount + _streamChannels.Sum(x => x.ReceivedByteCount) + DatagramChannels.Sum(x => x.ReceivedByteCount);
+            }
+        }
 
         private long _sentByteCount;
-        public long SentByteCount => _sentByteCount + StreamChannels.Sum(x => x.SentByteCount) + DatagramChannels.Sum(x => x.SentByteCount); //todo: lock
+        public long SentByteCount
+        {
+            get
+            {
+                lock (_channelListLock)
+                    return _sentByteCount + _streamChannels.Sum(x => x.SentByteCount) + DatagramChannels.Sum(x => x.SentByteCount);
+            }
+        }
+
 
         public event EventHandler<ChannelPacketReceivedEventArgs> OnPacketReceived;
         public event EventHandler<ChannelEventArgs> OnChannelAdded;
@@ -45,7 +58,7 @@ namespace VpnHood.Tunneling
         public long ReceiveSpeed { get; private set; }
 
         public DateTime LastActivityTime { get; private set; } = DateTime.Now;
-
+        
         public Tunnel()
         {
             _timer = new Timer(SpeedMonitor, null, 0, 1000);
@@ -83,9 +96,12 @@ namespace VpnHood.Tunneling
 
         private bool IsChannelExists(IChannel channel)
         {
-            return (channel is IDatagramChannel)
-                ? DatagramChannels.Contains(channel)
-                : StreamChannels.Contains(channel);
+            lock (_channelListLock)
+            {
+                return (channel is IDatagramChannel)
+                    ? DatagramChannels.Contains(channel)
+                    : _streamChannels.Contains(channel);
+            }
         }
 
         public void AddChannel(IChannel channel)
@@ -94,7 +110,7 @@ namespace VpnHood.Tunneling
                 throw new ObjectDisposedException(typeof(Tunnel).Name);
 
             // add to channel list
-            lock (_channelListObject)
+            lock (_channelListLock)
             {
                 if (IsChannelExists(channel))
                     throw new Exception($"{VhLogger.FormatTypeName(channel)} already exists in the collection!");
@@ -109,12 +125,12 @@ namespace VpnHood.Tunneling
                     DatagramChannels = DatagramChannels.Concat(new IDatagramChannel[] { datagramChannel }).ToArray();
                     var thread = new Thread(SendPacketThread, TunnelUtil.SocketStackSize_Datagram);
                     thread.Start(channel); // start sending after channel started
-                    Logger.LogInformation(GeneralEventId.DatagramChannel, $"A {channel.GetType().Name} has been added. ChannelCount: {DatagramChannels.Length}");
+                    VhLogger.Instance.LogInformation(GeneralEventId.DatagramChannel, $"A {channel.GetType().Name} has been added. ChannelCount: {DatagramChannels.Length}");
                 }
                 else
                 {
-                    StreamChannels = StreamChannels.Concat(new IChannel[] { channel }).ToArray();
-                    Logger.LogInformation(GeneralEventId.StreamChannel, $"A {channel.GetType().Name} has been added. ChannelCount: {StreamChannels.Length}");
+                    _streamChannels.Add(channel);
+                    VhLogger.Instance.LogInformation(GeneralEventId.StreamChannel, $"A {channel.GetType().Name} has been added. ChannelCount: {_streamChannels.Count}");
                 }
 
             }
@@ -125,7 +141,7 @@ namespace VpnHood.Tunneling
 
         public void RemoveChannel(IChannel channel, bool dispose = true)
         {
-            lock (_channelListObject)
+            lock (_channelListLock)
             {
                 if (!IsChannelExists(channel))
                     return; // channel already removed or does not exist
@@ -134,12 +150,12 @@ namespace VpnHood.Tunneling
                 {
                     datagramChannel.OnPacketReceived -= OnPacketReceived;
                     DatagramChannels = DatagramChannels.Where(x => x != channel).ToArray();
-                    Logger.LogInformation(GeneralEventId.DatagramChannel, $"A {channel.GetType().Name} has been removed. ChannelCount: {DatagramChannels.Length}");
+                    VhLogger.Instance.LogInformation(GeneralEventId.DatagramChannel, $"A {channel.GetType().Name} has been removed. ChannelCount: {DatagramChannels.Length}");
                 }
                 else
                 {
-                    StreamChannels = StreamChannels.Where(x => x != channel).ToArray();
-                    Logger.LogInformation(GeneralEventId.StreamChannel, $"A {channel.GetType().Name} has been removed. ChannelCount: {StreamChannels.Length}");
+                    _streamChannels.Where(x => x != channel).ToArray();
+                    VhLogger.Instance.LogInformation(GeneralEventId.StreamChannel, $"A {channel.GetType().Name} has been removed. ChannelCount: {_streamChannels.Count}");
                 }
 
                 // add stats of dead channel
@@ -232,7 +248,7 @@ namespace VpnHood.Tunneling
                             // drop packet if it is larger than _mtuWithFragment
                             if (packetSize > _mtuWithFragment)
                             {
-                                Logger.LogWarning($"Packet dropped! There is no channel to support this fragmented packet. Fragmented MTU: {_mtuWithFragment}, PacketLength: {ipPacket.TotalLength}, Packet: {ipPacket}");
+                                VhLogger.Instance.LogWarning($"Packet dropped! There is no channel to support this fragmented packet. Fragmented MTU: {_mtuWithFragment}, PacketLength: {ipPacket.TotalLength}, Packet: {ipPacket}");
                                 _packetQueue.TryDequeue(out ipPacket);
                                 continue;
                             }
@@ -240,7 +256,7 @@ namespace VpnHood.Tunneling
                             // drop packet if it is larger than _mtuNoFragment
                             if (packetSize > _mtuNoFragment && ipPacket is IPv4Packet ipV4packet && ipV4packet.FragmentFlags == 2)
                             {
-                                Logger.LogWarning($"Packet dropped! There is no channel to support this non fragmented packet. NoFragmented MTU: {_mtuNoFragment}, PacketLength: {ipPacket.TotalLength}, Packet: {ipPacket}");
+                                VhLogger.Instance.LogWarning($"Packet dropped! There is no channel to support this non fragmented packet. NoFragmented MTU: {_mtuNoFragment}, PacketLength: {ipPacket.TotalLength}, Packet: {ipPacket}");
                                 _packetQueue.TryDequeue(out ipPacket);
                                 var replyPacket = PacketUtil.CreateUnreachableReply(ipPacket, IcmpV4TypeCode.UnreachableFragmentationNeeded, (ushort)(_mtuNoFragment));
                                 OnPacketReceived?.Invoke(this, new ChannelPacketReceivedEventArgs(new[] { replyPacket }, channel));
@@ -284,7 +300,7 @@ namespace VpnHood.Tunneling
             }
             catch (Exception ex)
             {
-                Logger.LogWarning($"Could not send {packets.Count} packets via a channel! Message: {ex.Message}");
+                VhLogger.Instance.LogWarning($"Could not send {packets.Count} packets via a channel! Message: {ex.Message}");
             }
 
             // make sure channel has removed
@@ -297,9 +313,12 @@ namespace VpnHood.Tunneling
             if (_disposed) return;
             _disposed = true;
 
-            foreach (var channel in StreamChannels)
-                channel.Dispose();
-            StreamChannels = new IChannel[0]; //cleanup main consuming memory objects faster
+            lock (_channelListLock)
+            {
+                foreach (var channel in _streamChannels)
+                    channel.Dispose();
+                _streamChannels.Clear(); //cleanup main consuming memory objects faster
+            }
 
             foreach (var channel in DatagramChannels)
                 channel.Dispose();

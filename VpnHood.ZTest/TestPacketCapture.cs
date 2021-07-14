94e341d97e4d5f82331b07e916ebef2466169f4e
trudy <trudy@vpnhood.com>
2021-07-14 15:54:02 -0700

    Critial fix that cause casual packe loss due using sharing memoty of pakcet creation

--- VpnHood.ZTest/TestPacketCapture.cs
+++ VpnHood.ZTest/TestPacketCapture.cs
@@ -1,81 +1,50 @@
 ﻿using PacketDotNet;
 using System.Linq;
 using System.Net;
+using VpnHood.Client.Device;
 using VpnHood.Client.Device.WinDivert;
-using VpnHood.Tunneling;
 
 namespace VpnHood.Test
 {
-
     class TestPacketCapture : WinDivertPacketCapture
     {
-        public const int ServerPingTtl = 140;
-        public const int ServerMinPort = 33000;
-        public const int ServerMaxPort = 34000;
         private readonly TestDeviceOptions _deviceOptions;
+        private IPAddress[] dnsServers;
 
         public TestPacketCapture(TestDeviceOptions deviceOptions)
         {
             _deviceOptions = deviceOptions;
+            IncludeNetworks = deviceOptions.TestIpAddresses.Select(x => new IpNetwork(x)).ToArray();
         }
 
         protected override void ProcessPacket(IPPacket ipPacket)
         {
-            bool sendOut;
-
-            // ignore non test ips
-            if (!_deviceOptions.TestIpAddresses.Any(x => x.Equals(ipPacket.SourceAddress) || x.Equals(ipPacket.DestinationAddress)))
-            {
-                sendOut = true;
-            }
-
-            // let server outbound call, go out: Tcp
-            else if (ipPacket.Protocol == ProtocolType.Tcp)
-            {
-                var tcpPacket = PacketUtil.ExtractTcp(ipPacket);
-                sendOut = tcpPacket.SourcePort >= ServerMinPort && tcpPacket.SourcePort <= ServerMaxPort;
-            }
-
-            // let server outbound call, go out: Udp
-            else if (ipPacket.Protocol == ProtocolType.Udp)
-            {
-                var udpPacket = PacketUtil.ExtractUdp(ipPacket);
-                sendOut = udpPacket.SourcePort >= ServerMinPort && udpPacket.SourcePort <= ServerMaxPort;
-            }
-
-            // let server outbound call, go out: Icmp
-            else if (ipPacket.Protocol == ProtocolType.Icmp)
-            {
-                //var icmpPacket = PacketUtil.ExtractIcmp(ipPacket);
-                sendOut = ipPacket.TimeToLive == (ServerPingTtl - 1);
-            }
-
-            // drop direct packets for test addresses which client doesn't send to tunnel
-            else
-            {
-                return;
-            }
-
-            // let packet go out
-            if (sendOut)
-            {
+            if (TestNetProtector.IsProtectedPacket(ipPacket))
                 SendPacketToOutbound(ipPacket);
-            }
-            // Tunnel the packet
             else
-            {
                 base.ProcessPacket(ipPacket);
+        }
+
+        public override bool IsDnsServersSupported => _deviceOptions.IsDnsServerSupported;
+        public override IPAddress[] DnsServers
+        {
+            get => IsDnsServersSupported ? dnsServers : base.DnsServers;
+            set
+            {
+                if (IsDnsServersSupported)
+                    dnsServers = value;
+                else
+                    base.DnsServers = value;
             }
         }
-        
-        public override IPAddress[] DnsServers { get ; set ; } 
-        public override bool CanProtectSocket => !_deviceOptions.CanSendPacketToOutbound;
         public override bool CanSendPacketToOutbound => _deviceOptions.CanSendPacketToOutbound;
+        public override bool CanProtectSocket => !_deviceOptions.CanSendPacketToOutbound;
         public override void ProtectSocket(System.Net.Sockets.Socket socket)
         {
-            if (!CanProtectSocket)
+            if (CanProtectSocket)
+                TestNetProtector.ProtectSocket(socket);
+            else
                 base.ProtectSocket(socket);
         }
-        public override bool IsDnsServersSupported => _deviceOptions.IsDnsServerSupported;
     }
 }

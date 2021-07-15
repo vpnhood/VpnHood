7e0f42ba3ef317eb29d8f0a0386811e53ff8d229
trudy <trudy@vpnhood.com>
2021-07-15 01:22:08 -0700

    fix: No window open by pressing menu items

---
+++ VpnHood.Client.App.Android/MyWebChromeClient.cs
@@ -0,0 +1,20 @@
+﻿using Android.OS;
+using Android.Webkit;
+using System;
+using Xamarin.Essentials;
+
+namespace VpnHood.Client.App.Android
+{
+    class MyWebChromeClient : WebChromeClient
+    {
+        public override bool OnCreateWindow(WebView view, bool isDialog, bool isUserGesture, Message resultMsg)
+        {
+            var newWebView = new WebView(view.Context);
+            newWebView.SetWebViewClient(new MyWebViewClient());
+            var transport = (WebView.WebViewTransport)resultMsg.Obj;
+            transport.WebView = newWebView;
+            resultMsg.SendToTarget();
+            return true;
+        }
+    }
+}

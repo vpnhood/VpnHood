5dea9d10bcd9798d4d1250ae9c745dcf318e769a
trudy <trudy@vpnhood.com>
2021-07-16 10:00:15 -0700

    Fix: Android: System Notification remain connected after disconnet

--- VpnHood.Tunneling/TunnelUtil.cs
+++ VpnHood.Tunneling/TunnelUtil.cs
@@ -2,9 +2,7 @@
 {
     public static class TunnelUtil
     {
-        public const int SocketStackSize_Stream = 0xFFFF * 2;
-        public const int SocketStackSize_Datagram = 0xFFFF * 2;
-        public const int MaxDatagramChannelCount = 8;
+        public const int MaxDatagramChannelCount = 4;
         public const int TlsHandshakeLength = 5000;
         public const int MtuWithFragmentation = 0xFFFF - 70;
         public const int MtuWithoutFragmentation = 1500 - 70;

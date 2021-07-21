aee4029b50442d6ad65bdb7dcb374663de7ca674
trudy <trudy@vpnhood.com>
2021-07-21 01:45:03 -0700

    add PublicAddress to HelloResponse

--- VpnHood.Server.Access/ClientIdentity.cs
+++ VpnHood.Server.Access/ClientIdentity.cs
@@ -10,10 +10,12 @@ namespace VpnHood.Server
         public Guid TokenId { get; set; }
         public string UserToken { get; set; }
         public Guid ClientId { get; set; }
+        
         [JsonConverter(typeof(IPAddressConverter))]
         public IPAddress ClientIp { get; set; }
         public string ClientVersion { get; set; }
         public string UserAgent { get; set; }
+
         [JsonConverter(typeof(IPEndPointConverter))]
         public IPEndPoint ServerEndPoint { get; set; }
     }

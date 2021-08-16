db71143b440ce72b601565522531ab86347a367b
trudy <trudy@vpnhood.com>
2021-08-16 15:13:49 -0700

    improve filtering tests

--- VpnHood.ZTest/TestDeviceOptions.cs
+++ VpnHood.ZTest/TestDeviceOptions.cs
@@ -4,11 +4,7 @@ namespace VpnHood.Test
 {
     class TestDeviceOptions
     {
-        public TestDeviceOptions()
-        {
-            TestIpAddresses = TestHelper.GetTestIpAddresses();
-        }
-        public IPAddress[] TestIpAddresses { get; set; }
+        public IPAddress[] TestIpAddresses { get; set; } = TestHelper.GetTestIpAddresses();
         public bool CanSendPacketToOutbound { get; set; } = true;
         public bool IsDnsServerSupported { get; set; } = false;
     }

f9ced799ffaac541d617ae3a20ce843d5d9d00d0
trudy <trudy@vpnhood.com>
2021-08-15 19:05:02 -0700

    c#8 nullable respect for server app; add AppBaseNet

--- VpnHood.Server.App.Net/AppSettings.cs
+++ VpnHood.Server.App.Net/AppSettings.cs
@@ -1,19 +1,26 @@
 ﻿using System;
+using System.Net;
+using System.Text.Json.Serialization;
+using VpnHood.Common.Converters;
 
 namespace VpnHood.Server.App
 {
-    class AppSettings
+    public class AppSettings
     {
         public Guid? ServerId { get; set; }
         public Uri? RestBaseUrl { get; set; }
         public string? RestAuthorization { get; set; }
         public string? RestCertificateThumbprint { get; set; }
-        public ushort Port { get; set; } = 443;
+        [JsonConverter(typeof(IPEndPointConverter))]
+        public IPEndPoint EndPoint { get; set; } = new(IPAddress.Any, 443);
         public bool IsAnonymousTrackerEnabled { get; set; } = true;
         public string? SslCertificatesPassword { get; set; }
         public bool IsDiagnoseMode { get; set; }
         public int OrgStreamReadBufferSize { get; set; } = new ServerOptions().OrgStreamReadBufferSize;
         public int TunnelStreamReadBufferSize { get; set; } = new ServerOptions().TunnelStreamReadBufferSize;
         public int MaxDatagramChannelCount { get; set; } = new ServerOptions().MaxDatagramChannelCount;
+
+        [Obsolete("Deprecated from 1.4.2588. Use ListenerEndPoint")]
+        public int Port { get => EndPoint.Port; set => EndPoint.Port = value; }
     }
 }

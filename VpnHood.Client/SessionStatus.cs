b96ed497578e750d245c5e1553379caa3359afc4
trudy <trudy@vpnhood.com>
2021-08-28 23:14:26 -0700

    fix AppBase AppFolder finder

--- VpnHood.Client/SessionStatus.cs
+++ VpnHood.Client/SessionStatus.cs
@@ -1,11 +1,10 @@
 ﻿using VpnHood.Common.Messaging;
-using VpnHood.Tunneling.Messaging;
 
 namespace VpnHood.Client
 {
     public class SessionStatus
     {
-        public SessionErrorCode ErrorCode { get; internal set; } //todo change UI from response coce
+        public SessionErrorCode ErrorCode { get; internal set; }
         public AccessUsage? AccessUsage { get; internal set; } 
         public SessionSuppressType SuppressedTo { get; internal set; }
         public SessionSuppressType SuppressedBy { get; internal set; }

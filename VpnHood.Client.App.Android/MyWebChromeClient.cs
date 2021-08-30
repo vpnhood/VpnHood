59515d609b8947072f3315f290630aac06fa5248
trudy <trudy@vpnhood.com>
2021-08-30 15:22:35 -0700

    code clean up

--- VpnHood.Client.App.Android/MyWebChromeClient.cs
+++ VpnHood.Client.App.Android/MyWebChromeClient.cs
@@ -4,14 +4,14 @@ using Android.Webkit;
 
 namespace VpnHood.Client.App.Android
 {
-    class MyWebChromeClient : WebChromeClient
+    internal class MyWebChromeClient : WebChromeClient
     {
         public override bool OnCreateWindow(WebView? view, bool isDialog, bool isUserGesture, Message? resultMsg)
         {
             if (view?.Context == null) return false;
             var newWebView = new WebView(view.Context);
             newWebView.SetWebViewClient(new MyWebViewClient());
-            
+
             if (resultMsg?.Obj is not WebView.WebViewTransport transport) return false;
             transport.WebView = newWebView;
             resultMsg.SendToTarget();

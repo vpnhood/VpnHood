2780c9c9cb0f235da773e055afbbabc87122660f
Bob <bob@vpnhood.com>
2021-11-26 21:07:51 -0800

    Readable OS version info for server Create VpnHoodServer tar file for linux

--- VpnHood.Server.App.Net/_publish.ps1
+++ VpnHood.Server.App.Net/_publish.ps1
@@ -4,9 +4,9 @@ New-Item -Path $packagesServerDir -ItemType Directory -Force
 
 # server install-linux.sh
 echo "Make Server installation script for this release"
-$linuxScript = (Get-Content -Path "$PSScriptRoot/Install/install-linux.sh" -Raw).Replace('$installUrlParam', "https://github.com/vpnhood/VpnHood/releases/download/$versionTag/VpnHoodServer.zip");
+$linuxScript = (Get-Content -Path "$PSScriptRoot/Install/install-linux.sh" -Raw).Replace('$installUrlParam', "https://github.com/vpnhood/VpnHood/releases/download/$versionTag/VpnHoodServer.tar.gz");
 $linuxScript = $linuxScript -replace "`r`n", "`n";
-$linuxScript  | Out-File -FilePath "$packagesServerDir/install-linux.sh" -Encoding ASCII -Force -NoNewline ;
+$linuxScript  | Out-File -FilePath "$packagesServerDir/install-linux.sh" -Encoding ASCII -Force -NoNewline;
 
 . "$PSScriptRoot/../Pub/PublishApp.ps1" `
 	$PSScriptRoot `

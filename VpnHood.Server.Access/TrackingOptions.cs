9a5c05a52618194cc3a420835b350eba2f35214c
Trudy <trudy@vpnhood.com>
2021-12-07 22:36:04 -0800

    add trakingOptions

---
+++ VpnHood.Server.Access/TrackingOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace VpnHood.Server
+{
+    public class TrackingOptions
+    {
+        public bool ClientIp { get; set; }
+        public bool LocalPort { get; set; }
+
+        public bool IsEnabled() => ClientIp || LocalPort;
+    }
+}

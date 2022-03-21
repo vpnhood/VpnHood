cbe3944c27cb8216164211f55113ff380fa79d5b
Trudy <trudy@vpnhood.com>
2022-03-20 19:19:20 -0700

    change updater interval from minutes to timespan

--- VpnHood.App.Launcher/UpdaterOptions.cs
+++ VpnHood.App.Launcher/UpdaterOptions.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
 namespace VpnHood.App.Launcher
 {
     public class UpdaterOptions
     {
-        public int CheckIntervalMinutes { get; set; } = 1 * 24 * 60;
+        public TimeSpan CheckInterval { get; set; } = TimeSpan.FromHours(12);
         public ILogger Logger { get; set; } = NullLogger.Instance;
     }
 }

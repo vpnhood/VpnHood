4f9311e236e45d4bc353738d370983a09f98ab58
Trudy <trudy@vpnhood.com>
2022-05-17 20:14:38 -0700

    Change Reconfig Behaviour

--- VpnHood.Server.Access/AccessServers/FileAccessServerOptions.cs
+++ VpnHood.Server.Access/AccessServers/FileAccessServerOptions.cs
@@ -1,13 +1,15 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace VpnHood.Server.AccessServers
 {
     public class FileAccessServerOptions : ServerConfig
     {
         public FileAccessServerOptions()
-            : base(new IPEndPoint[] { new(IPAddress.Any, 443), new(IPAddress.IPv6Any, 443) } )
+            : base(new IPEndPoint[] { new(IPAddress.Any, 443), new(IPAddress.IPv6Any, 443) }, Guid.Empty.ToString())
         {
         }
+
         public string? SslCertificatesPassword { get; set; }
     }
 }

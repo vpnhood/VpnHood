12f4db997901f9b8609f2218009db0d934765002
Trudy <trudy@vpnhood.com>
2022-09-27 14:57:27 -0700

    fix CreateUdpClient in client

--- Samples/VpnHood.Samples.SimpleClient.Droid/Resources/Resource.designer.cs
+++ Samples/VpnHood.Samples.SimpleClient.Droid/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace VpnHood.Samples.SimpleClient.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.3.31")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
 	public partial class Resource
 	{
 		

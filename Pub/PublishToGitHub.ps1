566936d7062c6872bd4f18c5a1f458be594f9dfd
Trudy <trudy@vpnhood.com>
2022-11-20 17:53:35 -0800

    test

--- Pub/PublishToGitHub.ps1
+++ Pub/PublishToGitHub.ps1
@@ -18,11 +18,11 @@ if ($isLatest)
 
 # commit and push git
 $gitDir = "$solutionDir/.git";
-gh release delete "$versionTag" --cleanup-tag --yes;
 git tag -d "v$versionParam";
+gh release delete "$versionTag" --cleanup-tag --yes;
 git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish v$versionParam";
 git --git-dir=$gitDir --work-tree=$solutionDir pull;
-git --git-dir=$gitDir --work-tree=$solutionDir push --tags;
+git --git-dir=$gitDir --work-tree=$solutionDir push;
 
 # swtich to main branch
 if (!$prerelease)

46959fb1feb35b690a903843ed88b94a5b9cddd0
Trudy <trudy@vpnhood.com>
2022-11-24 01:00:13 -0800

    improve linux updater and installation

---
+++ VpnHood.App.Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VpnHood.App.Updater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

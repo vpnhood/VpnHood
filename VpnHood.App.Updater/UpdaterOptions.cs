d0501d86dac4fc9b64f4b3b078e7b6420ad76311
Trudy <trudy@vpnhood.com>
2022-11-24 01:00:13 -0800

    improve linux updater and installation

---
+++ VpnHood.App.Updater/UpdaterOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VpnHood.App.Updater;
+
+public class UpdaterOptions
+{
+    public TimeSpan CheckInterval { get; set; } = TimeSpan.FromHours(12);
+    public string PublishInfoFilePath { get; set; }
+    public string UpdateFolderPath { get; set; }
+
+    public UpdaterOptions(string publishInfoFilePath, string updateFolderPath)
+    {
+        PublishInfoFilePath = publishInfoFilePath;
+        UpdateFolderPath = updateFolderPath;
+    }
+}

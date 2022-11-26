c0f82d20b95d75c7bd4306ffb9055ab5922d0f5f
Trudy <trudy@vpnhood.com>
2022-11-26 03:08:07 -0800

    fix windows server installer

---
+++ VpnHood.Server.App.Net/_publish_win.ps1
@@ -0,0 +1,32 @@
+. "$PSScriptRoot\..\Pub\Common.ps1";
+$projectDir = $PSScriptRoot;
+$projectFile = (Get-ChildItem -path $projectDir -file -Filter "*.csproj").FullName;
+
+Write-Host;
+Write-Host "*** Creating Windows Server Module..." -BackgroundColor Blue -ForegroundColor White;
+
+# prepare module folders
+$moduleDir = "$packagesServerDir/win-x64";
+$moduleDirLatest = "$packagesServerDirLatest/win-x64";
+PrepareModuleFolder $moduleDir $moduleDirLatest;
+
+# Creating linux package
+$templateDir = "$PSScriptRoot/Install/Win";
+$template_installScriptFile = "$templateDir/install.ps1";
+$template_launcherFile = "$templateDir/vhserver.ps1";
+$template_updaterFile = "$templateDir/updater.ps1";
+
+$publishDir = "$projectDir/bin/release/publish-win";
+$publish_infoDir = "$publishDir/$versionTag/publish_info";
+$publish_updaterFile = "$publish_infoDir/update.ps1";
+$publish_launcherFile = "$publish_infoDir/vhserver.ps1";
+$publish_InfoFile = "$publish_infoDir/publish.json";
+
+$module_InfoFile = "$moduleDir/VpnHoodServer-win.json";
+$module_InstallerFile = "$moduleDir/VpnHoodServer-win.ps1";
+$module_PackageFile = "$moduleDir/VpnHoodServer-win.zip";
+
+$lineEnding = "`r`n";
+$runtime = "win-x64";
+$launcher_exeFile = "$versionTag/VpnHoodServer.exe";
+. "$PSScriptRoot\_publish_impl.ps1";

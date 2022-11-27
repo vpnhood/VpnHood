91e3002ac29e64e58aa8ecd32a8e36de2d164ed7
Trudy <trudy@vpnhood.com>
2022-11-26 18:38:53 -0800

    Publish v2.4.319

--- VpnHood.Server.App.Net/Install/Win/install.ps1
+++ VpnHood.Server.App.Net/Install/Win/install.ps1
@@ -1,69 +1,72 @@
-echo "VpnHood Server Installation for Windows";
+Write-Output "VpnHood Server Installation for Windows";
 $ErrorActionPreference = "Stop";
 
 # Default arguments
-$packageUrl="$packageUrlParam";
-$versionTag="$versionTagParam";
-$destinationPath="${env:ProgramFiles}/VpnHood/VpnHoodServer";
-$packageFile="";
+$packageUrl = "$packageUrlParam";
+$versionTag = "$versionTagParam";
+$destinationPath = "${env:ProgramFiles}/VpnHood/VpnHoodServer";
+$packageFile = "";
 
-for ($i=0; $i -lt $args.length; $i++)
-{
+for ($i = 0; $i -lt $args.length; $i++) {
 	$arg = $args[$i];
 
-    if ($arg -eq "-autostart") {
-		$autostart="y";
-		$lastArg=""; continue;
+	if ($arg -eq "-autostart") {
+		$autostart = "y";
+		$lastArg = ""; continue;
 	}
 	
 	elseif ($arg -eq "-q") {
-		$quiet="y";
-		$lastArg=""; continue;
+		$quiet = "y";
+		$lastArg = ""; continue;
 	}
 
 	elseif ($lastArg -eq "-restBaseUrl") {
-		$restBaseUrl=$arg;
-		$lastArg=""; continue;
+		$restBaseUrl = $arg;
+		$lastArg = ""; continue;
 	}
 	elseif ($lastArg -eq "-restAuthorization") {
-		$restAuthorization=$arg;
-		$lastArg=""; continue;
+		$restAuthorization = $arg;
+		$lastArg = ""; continue;
 	}
 	elseif ($lastArg -eq "-packageFile") {
-		$packageFile=$arg;
-		$lastArg=""; continue;
+		$packageFile = $arg;
+		$lastArg = ""; continue;
+	}
+	elseif ($lastArg -eq "-packageUrl") {
+		$packageUrl = $arg;
+		$lastArg = ""; continue;
 	}
 	elseif ($lastArg -eq "-versionTag") {
-		$versionTag=$arg;
-		$lastArg=""; continue;
+		$versionTag = $arg;
+		$lastArg = ""; continue;
 	}	
 	elseif ("$lastArg" -ne "") {
 		throw "Unknown argument! argument: $lastArg";
 	}
-	$lastArg=$arg;
+	$lastArg = $arg;
 }
 
 # validate $versionTag
-if ( "$versionTag" -eq "" ){
+if ( "$versionTag" -eq "" ) {
 	throw "Could not find versionTag!";
 }
 
 # User interaction
-if ( "$quiet" -ne "y" ){
+if ( "$quiet" -ne "y" ) {
 	if ("$autostart" -eq "") { $autostart = Read-Host -Prompt "Auto Start (y/n)?" ; }
 }
 
-$binDir="$destinationPath/$versionTag";
+$binDir = "$destinationPath/$versionTag";
 
 # point to latest version if $packageUrl is not set
-if ( "$packageUrl" -eq "" ){
-	$packageUrl="https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodServer.zip";
+if ( "$packageUrl" -eq "" ) {
+	$packageUrl = "https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodServer.zip";
 }
 
 # download & install VpnHoodServer
-if ( "$packageFile" -eq "" ){
-	echo "Downloading VpnHoodServer...";
-	$packageFile="VpnHoodServer-win.zip";
+if ( "$packageFile" -eq "" ) {
+	Write-Output "Downloading VpnHoodServer...";
+	$packageFile = "VpnHoodServer-win.zip";
 	[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12";
 	$oldProgressPreference = $ProgressPreference;
 	try {
@@ -75,34 +78,47 @@ if ( "$packageFile" -eq "" ){
 	}
 }
 
-# extract
-echo "Extracting to $destinationPath";
+# extracting
+Write-Output "Extracting to $destinationPath";
 New-Item -ItemType Directory -Path $destinationPath -Force | Out-Null;
-Expand-Archive "$packageFile" -DestinationPath "$destinationPath" -Force
+Expand-Archive "$packageFile" -DestinationPath "$destinationPath" -Force -ErrorAction Continue;
 
 # Updating shared files...
-echo "Updating shared files...";
-$infoDir="$binDir/publish_info";
+Write-Output "Updating shared files...";
+$infoDir = "$binDir/publish_info";
 Copy-Item -path "$infoDir/update.ps1" -Destination "$destinationPath/" -Force;
 Copy-Item -path "$infoDir/vhserver.ps1" -Destination "$destinationPath/" -Force;
 Copy-Item -path "$infoDir/publish.json" -Destination "$destinationPath/" -Force;
 
+# Write AppSettings
+if ("$restBaseUrl" -ne "") {
+	# publish info
+	$appSettings = @{
+		HttpAccessServer = @{
+			BaseUrl       = $restBaseUrl;
+			Authorization = $restAuthorization;
+		};
+	};
+	
+	# publish info
+	$appSettings | ConvertTo-Json | Out-File "$destinationPath/appsettings.json";
+}
+
 # AutoStart VpnHood Server
-if ($autostart -eq "y")
-{
-	echo "creating autostart service... Name: VpnHoodServer";
+if ($autostart -eq "y") {
+	Write-Output "creating autostart service... Name: VpnHoodServer";
 	$action = New-ScheduledTaskAction -Execute "$binDir/VpnHoodServer.exe";
-	$trigger = New-ScheduledTaskTrigger -AtStartup;
+	$trigger1 += New-ScheduledTaskTrigger -AtStartup;
+	$trigger2 += New-ScheduledTaskTrigger -once -RepetitionInterval "00:01:00" -At (Get-Date);
 	$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0);
-	$settings.CimInstanceProperties.Item('MultipleInstances').Value = 3; #StopExisting
-	$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings;
+	$task = New-ScheduledTask -Action $action -Trigger @($trigger1, $trigger2) -Settings $settings;
 	Register-ScheduledTask -User "System" -TaskName 'VpnHoodServer' -InputObject $task -Force -AsJob | Out-Null;
 
-	echo "creating auto update service... Name: VpnHoodUpdater";
+	Write-Output "creating auto update service... Name: VpnHoodUpdater";
 	$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NonInteractive -NoLogo -NoProfile -File `"$destinationPath/update.ps1`"";
 	$trigger = New-ScheduledTaskTrigger -Daily -At 3am;
 	$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings;
 	Register-ScheduledTask -User "System" -TaskName 'VpnHoodUpdater' -InputObject $task -Force | Out-Null;
 
-	 schtasks /run /tn "VpnHoodServer";
+	schtasks /run /tn "VpnHoodServer";
 }

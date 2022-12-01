2edffa9380cb26e433791ca1c2b50888c951eb82
Trudy <trudy@vpnhood.com>
2022-11-30 18:29:16 -0800

    Publish v2.4.320

--- Pub/PublishToGitHub.ps1
+++ Pub/PublishToGitHub.ps1
@@ -46,7 +46,6 @@ echo $releaseServerDir
 
 gh release create "$versionTag" `
 	--title "$versionTag" `
-	(&{if($prerelease) {"--prerelease"} else {""}}) `
 	-F $releaseRootDir/ReleaseNote.txt `
 	$releaseClientDir/android/VpnHoodClient-Android.apk `
 	$releaseClientDir/windows/VpnHoodClient-win.exe  `

ad2df548980970054dcd1eae7ff4a512a4363df8
trudy <trudy@vpnhood.com>
2022-12-12 12:59:25 -0800

    set free server limit to 3

--- VpnHood.AccessServer/QuotaConstants.cs
+++ VpnHood.AccessServer/QuotaConstants.cs
@@ -5,7 +5,7 @@ namespace VpnHood.AccessServer;
 public static class QuotaConstants
 {
     public static int ProjectCount { get; set; } = 2;
-    public static int ServerCount { get; set; } = 5;
+    public static int ServerCount { get; set; } = 3;
     public static int AccessTokenCount { get; set; } = 1000;
     public static int AccessPointGroupCount { get; set; } = 100;
     public static int AccessPointCount { get; set; } = 100;

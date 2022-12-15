5aee567e665ca5e5c978b6c88e4e145cc1f5c988
Trudy <trudy@vpnhood.com>
2022-12-14 19:52:54 -0800

    improve webview loading windows self contain WebView optional
    Former-commit-id: 7c6304ad02b2885094a1d1ceed1f07957c606110

--- VpnHood.Client.App.Win.Setup/_publish.ps1
+++ VpnHood.Client.App.Win.Setup/_publish.ps1
@@ -9,8 +9,11 @@ PrepareModuleFolder $moduleDir $moduleDirLatest;
 
 $advinstallerFile = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Caphyon\Advanced Installer" -Name "InstallRoot").InstallRoot;
 $advinstallerFile = Join-Path $advinstallerFile "bin\x86\AdvancedInstaller.com";
-$packageFile = Join-Path $PSScriptRoot "Release\VpnHoodClient-win.exe";
-$updaterConfigFile= Join-Path $PSScriptRoot "Release\VpnHoodClient-win.txt";
+
+$module_title = "VpnHoodClient-win-x64";
+$module_packageFileName = "$module_title.exe"
+$module_packageFile = Join-Path $PSScriptRoot "Release\$module_packageFileName";
+$module_updaterConfigFile= Join-Path $PSScriptRoot "Release\$module_title.txt";
 
 $aipFile= Join-Path $PSScriptRoot "VpnHood.Client.App.Win.Setup.aip";
 & $advinstallerFile /build $aipFile
@@ -21,26 +24,27 @@ $str=";aiu;
 [Update]
 Name = VpnHood $versionParam
 ProductVersion = $versionParam
-URL = https://github.com/vpnhood/VpnHood/releases/download/$versionTag/VpnHoodClient-win.exe
-Size = $((Get-Item $packageFile).length)
-SHA256 = $((Get-FileHash $packageFile -Algorithm SHA256).Hash)
-MD5 = $((Get-FileHash $packageFile -Algorithm MD5).Hash)
-ServerFileName = VpnHoodClient-win.exe
+URL = https://github.com/vpnhood/VpnHood/releases/download/$versionTag/$module_packageFileName
+Size = $((Get-Item $module_packageFile).length)
+SHA256 = $((Get-FileHash $module_packageFile -Algorithm SHA256).Hash)
+MD5 = $((Get-FileHash $module_packageFile -Algorithm MD5).Hash)
+ServerFileName = $moduleFileName.exe
 Flags = NoRedetect
 RegistryKey = HKUD\Software\VpnHood\VpnHood\Version
 Version = $versionParam
 UpdatedApplications = VpnHood(1.0-$versionParam)
 Description = <a href=""https://github.com/vpnhood/VpnHood/blob/main/CHANGELOG.md"">Release note</a>
 ";
-$str | Out-File -FilePath $updaterConfigFile
+$str | Out-File -FilePath $module_updaterConfigFile
 
 
 #####
 # copy to module
-Copy-Item -path $packageFile -Destination "$moduleDir/" -Force
-Copy-Item -path $updaterConfigFile -Destination "$moduleDir/" -Force
+Copy-Item -path "$module_packageFile" -Destination "$moduleDir/" -Force
+Copy-Item -path "$module_updaterConfigFile" -Destination "$moduleDir/" -Force
 
 if ($isLatest)
 {
 	Copy-Item -path "$moduleDir/*" -Destination "$moduleDirLatest/" -Force -Recurse
+	Copy-Item -path "$module_updaterConfigFile" -Destination "$moduleDirLatest/VpnHoodClient-win.txt" -Force -Recurse # legacy
 }

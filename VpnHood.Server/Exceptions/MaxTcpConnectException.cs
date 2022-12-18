d93fe8c50fb411fce73db2cc97af3b0acd828e70
Trudy <trudy@vpnhood.com>
2022-12-18 14:44:22 -0800

    add MaxTcpChannelCount control
    Former-commit-id: 6778b554121e11012b0952f671105140da54a592

---
+++ VpnHood.Server/Exceptions/MaxTcpConnectException.cs
@@ -0,0 +1,12 @@
+﻿using VpnHood.Common.Exceptions;
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Server.Exceptions;
+
+internal class MaxTcpConnectException : SessionException
+{
+    public MaxTcpConnectException(uint sessionId)
+        : base(SessionErrorCode.GeneralError, $"Maximum TcpConnectWait has been reached. SessionId: {sessionId}.")
+    {
+    }
+}

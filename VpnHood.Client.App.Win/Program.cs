08f4aee390fbec6e8ab9e3e8d19dfb6ea9f3030c
Trudy <trudy@vpnhood.com>
2022-12-28 14:45:32 -0800

    Use async Start for Server

--- VpnHood.Client.App.Win/Program.cs
+++ VpnHood.Client.App.Win/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
+using VpnHood.Common.Exceptions;
+using VpnHood.Common.Logging;
 
 namespace VpnHood.Client.App;
 
@@ -7,8 +10,14 @@ internal class Program
     [STAThread]
     private static void Main(string[] args)
     {
-        // run the app
-        using var app = new WinApp();
-        app.Start(args);
+        try
+        {
+            using var app = new WinApp();
+            app.Start(args);
+        }
+        catch (AnotherInstanceIsRunning ex)
+        {
+            VhLogger.Instance.LogError(ex.Message);
+        }
     }
 }

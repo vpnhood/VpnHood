59a0fe4283141d19a897c8335d19f0ea6faee630
Trudy <trudy@vpnhood.com>
2022-12-28 12:26:26 -0800

    mix max CompletionPortThreads

--- VpnHood.Server/VpnHoodServer.cs
+++ VpnHood.Server/VpnHoodServer.cs
@@ -53,8 +53,8 @@ public class VpnHoodServer : IDisposable
         ThreadPool.GetMinThreads(out var workerThreads, out var completionPortThreads);
         ThreadPool.SetMinThreads(workerThreads, options.MinCompletionPortThreads ?? completionPortThreads * 30);
 
-        ThreadPool.GetMaxThreads(out var workerThreadsMax, out var zzz);
-        ThreadPool.SetMaxThreads(workerThreadsMax, options.MaxCompletionPortThreads ?? zzz); // We prefer all IO operations get slow together than be queued
+        ThreadPool.GetMaxThreads(out var workerThreadsMax, out _);
+        ThreadPool.SetMaxThreads(workerThreadsMax, options.MaxCompletionPortThreads ?? 0xFFFF); // We prefer all IO operations get slow together than be queued
 
         // update timers
         _configureTimer = new Timer(options.ConfigureInterval.TotalMilliseconds) { AutoReset = false, Enabled = false };

f6520e7b698afad57ad140f9df0e949dff06e571
Trudy <trudy@vpnhood.com>
2023-01-13 01:25:33 -0800

    fix UdpProxyEx
    Former-commit-id: 1c9e68afe43754d378b563b8b5120d1fba086ad0

--- VpnHood.Tunneling/UdpProxyPoolEx.cs
+++ VpnHood.Tunneling/UdpProxyPoolEx.cs
@@ -43,6 +43,7 @@ public abstract class UdpProxyPoolEx : IDisposable, IJob
     {
         _socketFactory = socketFactory;
         _connectionMap = new TimeoutDictionary<string, UdpProxyEx>(UdpTimeout);
+        JobRunner.Default.Add(this);
     }
 
     public Task SendPacket(IPAddress sourceAddress, IPAddress destinationAddress, UdpPacket udpPacket, bool? noFragment)
@@ -53,24 +54,24 @@ public abstract class UdpProxyPoolEx : IDisposable, IJob
 
         // find the proxy for the connection (source-destination)
         var connectionKey = $"{sourceEndPoint}:{destinationEndPoint}";
-        var udpWorker = _connectionMap.GetOrAdd(connectionKey, _ =>
+        var udpProxy = _connectionMap.GetOrAdd(connectionKey, _ =>
         {
             // Find or create a worker that does not use the RemoteEndPoint
             lock (_udpProxies)
             {
-                var newUdpWorker = _udpProxies.FirstOrDefault(x =>
+                var newUdpProxy = _udpProxies.FirstOrDefault(x =>
                     x.AddressFamily == addressFamily &&
                     !x.DestinationEndPointMap.TryGetValue(destinationEndPoint, out var _));
 
                 var isNewLocalEndPoint = false;
-                if (newUdpWorker == null)
+                if (newUdpProxy == null)
                 {
                     // check WorkerMaxCount
                     if (_udpProxies.Count >= WorkerMaxCount)
                         throw new UdpClientQuotaException(_udpProxies.Count);
 
-                    newUdpWorker = new UdpProxyEx(this, _socketFactory.CreateUdpClient(addressFamily), addressFamily);
-                    _udpProxies.Add(newUdpWorker);
+                    newUdpProxy = new UdpProxyEx(this, _socketFactory.CreateUdpClient(addressFamily), addressFamily);
+                    _udpProxies.Add(newUdpProxy);
                     isNewLocalEndPoint = true;
                 }
 
@@ -84,16 +85,16 @@ public abstract class UdpProxyPoolEx : IDisposable, IJob
 
                 // Raise new endpoints
                 OnNewEndPoint?.Invoke(this, new EndPointEventArgs(ProtocolType.Udp,
-                    newUdpWorker.LocalEndPoint, destinationEndPoint, isNewLocalEndPoint, isNewRemoteEndPoint));
+                    newUdpProxy.LocalEndPoint, destinationEndPoint, isNewLocalEndPoint, isNewRemoteEndPoint));
 
                 // Add destinationEndPoint; each newUdpWorker can not amp a destinationEndPoint to more than one source port
-                newUdpWorker.DestinationEndPointMap.TryAdd(destinationEndPoint, new TimeoutItem<IPEndPoint>(sourceEndPoint));
-                return newUdpWorker;
+                newUdpProxy.DestinationEndPointMap.TryAdd(destinationEndPoint, new TimeoutItem<IPEndPoint>(sourceEndPoint));
+                return newUdpProxy;
             }
         });
 
         var dgram = udpPacket.PayloadData ?? Array.Empty<byte>();
-        return udpWorker.SendPacket(destinationEndPoint, dgram, noFragment);
+        return udpProxy.SendPacket(destinationEndPoint, dgram, noFragment);
     }
 
     public Task RunJob()
--- VpnHood.ZTest/Tests/UdpProxyTest.cs
+++ VpnHood.ZTest/Tests/UdpProxyTest.cs
@@ -11,7 +11,7 @@ namespace VpnHood.Test.Tests;
 [TestClass]
 public class UdpProxyTest
 {
-    private class TestUdpProxyPool : UdpProxyPool
+    private class TestUdpProxyPool : UdpProxyPoolEx
     {
         private IPPacket? LastReceivedPacket { get; set; }
         public TestUdpProxyPool() :
@@ -96,6 +96,50 @@ public class UdpProxyTest
         // test ip6
         await udpProxyClient.SendPacket(IPAddress.Parse("::1"),
             TestHelper.WebServer.UdpEndPoint2Ip6.Address, udpPacket, false);
+        await udpProxyClient.WaitForUdpPacket(p => p.DestinationAddress.Equals(IPAddress.Parse("::1")));
+    }
+
+    [TestMethod]
+    public async Task Multiple_EndPointEx()
+    {
+        var udpProxyClient = new TestUdpProxyPool();
+        var ep1 = TestHelper.WebServer.UdpEndPoint1;
+        var udp1 = new UdpPacket(2000, (ushort)ep1.Port) { PayloadData = Guid.NewGuid().ToByteArray() };
+
+        // Test
+        await udpProxyClient.SendPacket(IPAddress.Parse("127.0.0.2"), ep1.Address, udp1, false);
+        await udpProxyClient.SendPacket(IPAddress.Parse("127.0.0.2"), ep1.Address, udp1, false);
+        Assert.AreEqual(1, udpProxyClient.WorkerCount);
+        await udpProxyClient.WaitForUdpPacket(p => p.DestinationAddress.Equals(IPAddress.Parse("127.0.0.2")));
+
+        // -------------
+        // Test
+        // -------------
+        await udpProxyClient.SendPacket(IPAddress.Parse("127.0.0.3"), ep1.Address, udp1, false);
+        Assert.AreEqual(2, udpProxyClient.WorkerCount, "New source with a same destination should create a new worker.");
+        await udpProxyClient.WaitForUdpPacket(p => p.DestinationAddress.Equals(IPAddress.Parse("127.0.0.3")));
+
+        // -------------
+        // Test
+        // -------------
+        var ep2 = TestHelper.WebServer.UdpEndPoint2;
+        var udp2 = new UdpPacket(2000, (ushort)ep2.Port) { PayloadData = Guid.NewGuid().ToByteArray() };
+        await udpProxyClient.SendPacket(IPAddress.Parse("127.0.0.3"), ep2.Address, udp2, false);
+        Assert.AreEqual(2, udpProxyClient.WorkerCount, 
+            "new destination should not create a worker.");
+
+        // -------------
+        // Test: timeout
+        // -------------
+        udpProxyClient = new TestUdpProxyPool { UdpTimeout = TimeSpan.FromMicroseconds(1000) };
+        await udpProxyClient.SendPacket(IPAddress.Parse("127.0.0.2"), ep1.Address, udp1, false);
+        Assert.AreEqual(1, udpProxyClient.WorkerCount);
+        await Task.Delay(2000);
+        Assert.AreEqual(0, udpProxyClient.WorkerCount);
+
+        // test ip6
+        await udpProxyClient.SendPacket(IPAddress.Parse("::1"),
+            TestHelper.WebServer.UdpEndPoint2Ip6.Address, udp1, false);
         await udpProxyClient.WaitForUdpPacket(p => p.DestinationAddress.Equals(IPAddress.Parse("::1")));
     }
 

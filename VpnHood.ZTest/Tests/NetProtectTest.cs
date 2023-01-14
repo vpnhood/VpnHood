de3733756a78989d644abd7e97b366439780d9cb
Trudy <trudy@vpnhood.com>
2023-01-14 13:37:56 -0800

    add test for MaxTcpWaitConnect
    Former-commit-id: 6af34a4a4aa838b8a97321d5f54bbb315a3337e3

---
+++ VpnHood.ZTest/Tests/NetProtectTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VpnHood.Test.Tests;
+
+[TestClass]
+public class NetProtectTest
+{
+    [TestMethod]
+    public async Task MaxTcpWaitConnect_reject()
+    {
+        // create server
+        var fileAccessServerOptions = TestHelper.CreateFileAccessServerOptions();
+        fileAccessServerOptions.SessionOptions.TcpConnectTimeout = TimeSpan.FromSeconds(1);
+        fileAccessServerOptions.SessionOptions.MaxTcpConnectWaitCount = 0;
+        await using var server = TestHelper.CreateServer(fileAccessServerOptions);
+
+        // create client
+        var token = TestHelper.CreateAccessToken(server);
+        await using var client = TestHelper.CreateClient(token);
+
+        try
+        {
+            await TestHelper.Test_HttpsAsync();
+            Assert.Fail("Exception expected!");
+        }
+        catch (Exception ex)
+        {
+            Assert.AreEqual(ex.GetType().Name, nameof(HttpRequestException));
+        }
+    }
+
+    [TestMethod]
+    public async Task MaxTcpWaitConnect_accept()
+    {
+        // create server
+        var fileAccessServerOptions = TestHelper.CreateFileAccessServerOptions();
+        fileAccessServerOptions.SessionOptions.TcpConnectTimeout = TimeSpan.FromSeconds(1);
+        fileAccessServerOptions.SessionOptions.MaxTcpConnectWaitCount = 1;
+        await using var server = TestHelper.CreateServer(fileAccessServerOptions);
+
+        // create client
+        var token = TestHelper.CreateAccessToken(server);
+        await using var client = TestHelper.CreateClient(token);
+
+        await TestHelper.Test_HttpsAsync();
+        await TestHelper.Test_HttpsAsync();
+    }
+
+}

c823cfd7bbfaca7ba39e7d213a7c945f8cfce6c4
Trudy <trudy@vpnhood.com>
2023-01-14 21:20:43 -0800

    use IPacketProxy

---
+++ VpnHood.Tunneling/ProxyManagerOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using VpnHood.Tunneling.Factory;
+
+namespace VpnHood.Tunneling;
+
+public class ProxyManagerOptions
+{
+    public TimeSpan? UdpTimeout { get; set; }
+    public TimeSpan? IcmpTimeout { get; set; }
+    public int? MaxUdpWorkerCount { get; set; }
+    public int? MaxIcmpWorkerCount { get; set; }
+}

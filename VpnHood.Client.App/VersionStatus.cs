d05e19e1853208ab8c2e28e533ae8e40a1b751b9
Trudy <trudy@vpnhood.com>
2023-01-26 18:08:50 -0800

    Publish v2.6.341

--- VpnHood.Client.App/VersionStatus.cs
+++ VpnHood.Client.App/VersionStatus.cs
@@ -1,5 +1,8 @@
-﻿namespace VpnHood.Client.App;
+﻿using System.Text.Json.Serialization;
 
+namespace VpnHood.Client.App;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum VersionStatus
 {
     Unknown,

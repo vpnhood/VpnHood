6971b206051b3e75ff90dfc3ac1e117ebc81c388
Trudy <trudy@vpnhood.com>
2023-02-12 21:08:12 -0800

    ServerNetFilters tests

---
+++ VpnHood.Server.Access/Configurations/NetFilterOptions.cs
@@ -0,0 +1,12 @@
+﻿using VpnHood.Common.Net;
+
+namespace VpnHood.Server.Configurations;
+
+public class NetFilterOptions
+{
+    public bool ExcludeLocalNetwork { get; set; }
+    public IpRange[]? PacketCaptureIncludeIpRanges { get; set; }
+    public IpRange[]? PacketCaptureExcludeIpRanges { get; set; }
+    public IpRange[]? IncludeIpRanges { get; set; }
+    public IpRange[]? ExcludeIpRanges { get; set; }
+}

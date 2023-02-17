93875e3da4903357a4ce2305cdf22730946102e4
Trudy <trudy@vpnhood.com>
2023-02-16 17:49:04 -0800

    clean up
    Former-commit-id: 873659e6ddd147a83d18401b06389bc9305f1cad

--- VpnHood.Server.Access/Configurations/TrackingOptions.cs
+++ VpnHood.Server.Access/Configurations/TrackingOptions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace VpnHood.Server.Configurations;
 

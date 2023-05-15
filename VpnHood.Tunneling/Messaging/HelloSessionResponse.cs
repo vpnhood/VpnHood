4c3a35c328ff1db820b32766e59cea44985ae4bc
Trudy <trudy@vpnhood.com>
2023-05-15 00:37:07 -0700

    add EndPoints to Hello
    Former-commit-id: 87269a5eadd19c1658dda1a11daaa6831080b879

--- VpnHood.Tunneling/Messaging/HelloSessionResponse.cs
+++ VpnHood.Tunneling/Messaging/HelloSessionResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Text.Json.Serialization;
 using VpnHood.Common.Converters;
 using VpnHood.Common.Messaging;
@@ -22,8 +23,15 @@ public class HelloSessionResponse : SessionResponse
     [JsonConverter(typeof(IPAddressConverter))]
     public IPAddress ClientPublicAddress { get; set; } = null!;
 
+    [JsonConverter(typeof(ArrayConverter<IPEndPoint, IPEndPointConverter>))]
+    public IPEndPoint[] TcpEndPoints { get; set; } = Array.Empty<IPEndPoint>();
+
+    [JsonConverter(typeof(ArrayConverter<IPEndPoint, IPEndPointConverter>))]
+    public IPEndPoint[] UdpEndPoints { get; set; } = Array.Empty<IPEndPoint>();
+
     public string ServerVersion { get; set; } = null!;
     public int ServerProtocolVersion { get; set; }
+    
     public int UdpPort { get; set; }
     public byte[]? UdpKey { get; set; } = null!;
     public byte[]? ServerSecret { get; set; } //todo: must not null in version >= 2.9.362

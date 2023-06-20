2c617aef6eed056b6b2d490ac76f9bdde2b1c440
Trudy <trudy@vpnhood.com>
2023-06-20 02:15:36 -0700

    fix client UdpTransmitter disposal
    Former-commit-id: c5fbe4ab0f48d3b4b744156828f4072636bf19fe

--- VpnHood.Client/ClientUdpChannelTransmitter.cs
+++ VpnHood.Client/ClientUdpChannelTransmitter.cs
@@ -8,11 +8,10 @@ public class ClientUdpChannelTransmitter : UdpChannelTransmitter
 {
     private readonly UdpChannel2 _udpChannel;
 
-    public ClientUdpChannelTransmitter(UdpChannel2 udpChannel, UdpClient udpClient, IPEndPoint serverEndPoint, byte[] serverKey) 
+    public ClientUdpChannelTransmitter(UdpChannel2 udpChannel, UdpClient udpClient, byte[] serverKey) 
         : base(udpClient, serverKey)
     {
         _udpChannel = udpChannel;
-        udpChannel.SetRemote(this, serverEndPoint);
     }
 
     protected override void OnReceiveData(ulong sessionId, IPEndPoint remoteEndPoint, long channelCryptorPosition, byte[] buffer, int bufferIndex)

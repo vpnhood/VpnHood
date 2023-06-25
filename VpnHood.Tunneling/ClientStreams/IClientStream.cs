5a9ce35c7e54f5cfdda6c61edc0578ff389881df
Trudy <trudy@vpnhood.com>
2023-06-25 03:21:24 -0700

    add ClientStream cleanup to server

--- VpnHood.Tunneling/ClientStreams/IClientStream.cs
+++ VpnHood.Tunneling/ClientStreams/IClientStream.cs
@@ -7,6 +7,7 @@ namespace VpnHood.Tunneling.ClientStreams;
 
 public interface IClientStream : IAsyncDisposable
 {
+    bool Disposed { get; }
     string ClientStreamId { get; set; }
     IPEndPointPair IpEndPointPair { get; }
     Stream Stream { get; }
--- VpnHood.Tunneling/TunnelDefaults.cs
+++ VpnHood.Tunneling/TunnelDefaults.cs
@@ -8,7 +8,8 @@ public static class TunnelDefaults
     public const int TlsHandshakeLength = 5000;
     public const int MtuWithFragmentation = 0xFFFF - 70;
     public const int MtuWithoutFragmentation = 1500 - 70;
-    public static TimeSpan TcpTimeout { get; set; } = TimeSpan.FromMinutes(15);
-    public static TimeSpan TcpGracefulTimeout { get; set; } = TimeSpan.FromSeconds(10); //todo must be 60
-    public static TimeSpan TcpReuseTimeout { get; set; } = TimeSpan.FromSeconds(60); 
+    public static TimeSpan TcpCheckInterval { get; set; } = TimeSpan.FromMinutes(15);
+    public static TimeSpan TcpGracefulTimeout { get; set; } = TimeSpan.FromSeconds(30);
+    public static TimeSpan TcpReuseTimeout { get; set; } = TimeSpan.FromSeconds(60);
+    public static TimeSpan TcpRequestTimeout { get; set; } = TimeSpan.FromSeconds(60);
 }

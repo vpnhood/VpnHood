b0e6a0a83d49d98f01a2a1b665c0dc371759ef1e
Trudy <trudy@vpnhood.com>
2023-06-26 16:11:15 -0700

    add ga4
    Former-commit-id: 9f7e53e11551a8e41767c6e3315eb39f1bb70ced

---
+++ VpnHood.Common/Ga4Tracking/Ga4Payload.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+// ReSharper disable once CheckNamespace
+namespace Ga4.Ga4Tracking;
+
+internal class Ga4Payload
+{
+    public class UserProperty
+    {
+        [JsonPropertyName("value")]
+        public required object Value { get; init; }
+    }
+
+    [JsonPropertyName("client_id")] 
+    public required string ClientId { get; set; }
+
+    [JsonPropertyName("timestamp_micros")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public ulong? TimestampMicros { get; set; }
+
+    [JsonPropertyName("user_properties")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public Dictionary<string, UserProperty>? UserProperties { get; set; }
+    
+    [JsonPropertyName("events")]
+    public required IEnumerable<Ga4Event> Events { get; set; }
+}

6283f72f81004725c06051f919c80ee270ed57a6
Trudy <trudy@vpnhood.com>
2023-06-27 10:35:27 -0700

    add some prop to g4tag

--- VpnHood.Common/Ga4Tracking/Ga4TagParam.cs
+++ VpnHood.Common/Ga4Tracking/Ga4TagParam.cs
@@ -5,4 +5,6 @@ public class Ga4TagParam
 {
     public required string EventName { get; init; }
     public required string AppName { get; init; }
+    public string DocumentLocation { get; init; } = "home";
+    public string DocumentTitle { get; init; } = "Home Page";
 }

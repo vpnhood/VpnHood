aabdb9904a68ebc5b31b3521634b75248a48a3bf
Trudy <trudy@vpnhood.com>
2023-06-27 13:16:55 -0700

    Migrate to Ga4

--- VpnHood.Common/Trackers/ITracker.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace VpnHood.Common.Trackers;
-
-public interface ITracker
-{
-    public Task<bool> TrackEvent(string category, string action, string? label = null, int? value = null);
-}

09b03dfded713bce68d84770b505d4f9830567f5
Trudy <trudy@vpnhood.com>
2023-06-27 23:37:48 -0700

    rename server IsAnonymousTrackerEnabled tp AllowAnonymousTracker

--- VpnHood.Client.App/Settings/UserSettings.cs
+++ VpnHood.Client.App/Settings/UserSettings.cs
@@ -6,19 +6,22 @@ namespace VpnHood.Client.App.Settings;
 
 public class UserSettings
 {
+    private static readonly ClientOptions DefaultClientOptions = new(); 
+
     public AppLogSettings Logging { get; set; } = new();
     public string CultureName { get; set; } = "en";
     public Guid? DefaultClientProfileId { get; set; }
     public int MaxReconnectCount { get; set; } = 3;
-    public int MaxDatagramChannelCount { get; set; } = new ClientOptions().MaxDatagramChannelCount;
+    public int MaxDatagramChannelCount { get; set; } = DefaultClientOptions.MaxDatagramChannelCount;
     public bool TunnelClientCountry { get; set; } = true;
     public string[]? IpGroupFilters { get; set; }
     public FilterMode IpGroupFiltersMode { get; set; } = FilterMode.All;
     public IpRange[]? CustomIpRanges { get; set; }
     public string[]? AppFilters { get; set; }
     public FilterMode AppFiltersMode { get; set; } = FilterMode.All;
-    public bool UseUdpChannel { get; set; } = new ClientOptions().UseUdpChannel;
-    public bool ExcludeLocalNetwork { get; set; } = new ClientOptions().ExcludeLocalNetwork;
+    public bool UseUdpChannel { get; set; } = DefaultClientOptions.UseUdpChannel;
+    public bool ExcludeLocalNetwork { get; set; } = DefaultClientOptions.ExcludeLocalNetwork;
     public IpRange[] PacketCaptureIncludeIpRanges { get; set; } = IpNetwork.All.ToIpRanges().ToArray();
     public IpRange[]? PacketCaptureExcludeIpRanges { get; set; } = IpNetwork.None.ToIpRanges().ToArray();
+    public bool AllowAnonymousTracker { get; set; } = DefaultClientOptions.AllowAnonymousTracker;
 }

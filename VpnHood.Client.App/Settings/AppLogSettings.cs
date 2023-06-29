077f70c45fa746d8c1078bcdb148530fa832c623
Trudy <trudy@vpnhood.com>
2023-06-29 15:06:45 -0700

    fix android console logger

--- VpnHood.Client.App/Settings/AppLogSettings.cs
+++ VpnHood.Client.App/Settings/AppLogSettings.cs
@@ -2,6 +2,7 @@
 
 public class AppLogSettings
 {
+    public bool LogToConsole { get; set; } = true;
     public bool LogToFile { get; set; }
     public bool LogVerbose { get; set; }
     public bool LogAnonymous { get; set; }

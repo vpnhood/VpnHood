65b0b41bcb6e5d01c6ad11bd5ea2834e612def23
Trudy <trudy@vpnhood.com>
2023-08-23 17:04:28 -0700

    maui

--- VpnHood.Client.App.Maui/MauiProgram.cs
+++ VpnHood.Client.App.Maui/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using VpnHood.Client.App.UI;
 
 namespace VpnHood.Client.App.Maui;
 
@@ -18,24 +19,34 @@ public static class MauiProgram
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
-        
+
         // init app
         var appProvider = CreateAppProvider();
-        VpnHoodApp.Init(appProvider, new AppOptions
-        {
-            AppDataFolderPath = AppLocalDataPath,
-            UpdateInfoUrl = new Uri("https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodClient-win-x64.json")
-        });
-        VpnHoodAppUi.Init(new MemoryStream(Resource.SPA), url2: localWebUrl);
-
+        VpnHoodApp.Init(appProvider, new AppOptions { UpdateInfoUrl = GetUpdateInfoUrl() });
+        VpnHoodAppUi.Init(File.OpenRead(Path.Combine(AppContext.BaseDirectory, "SPA.zip")));
 
         return builder.Build();
     }
 
-    // Create vpnhood IAppProvider 
+    // Create VpnHood IAppProvider 
     private static IAppProvider CreateAppProvider()
     {
+#if WINDOWS
         return new WinAppProvider();
+#else
+        throw new NotSupportedException();
+#endif
+
     }
 
+    private static Uri GetUpdateInfoUrl()
+    {
+#if WINDOWS
+        return new Uri("https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodClient-win-x64.json");
+#elif ANDROID
+        return new Uri("https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodClient-android.json");
+#else
+        throw new NotSupportedException();
+#endif
+    }
 }

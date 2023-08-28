7841af0a83bbf867aa6eb5da29cd9d1556cab805
Trudy <trudy@vpnhood.com>
2023-08-28 13:24:00 -0700

    add swagger gen

---
+++ VpnHood.Client.App.UI/Api/IClientApi.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using VpnHood.Client.App.Settings;
+// ReSharper disable InconsistentNaming
+
+namespace VpnHood.Client.App.UI.Api;
+
+public interface IClientApi
+{
+    Task<LoadAppResponse> loadApp(LoadAppParam loadAppParam);
+    Task connect(ConnectParam connectParam);
+    Task diagnose(ConnectParam connectParam);
+    Task disconnect();
+    Task<ClientProfile> addAccessKey(AddClientProfileParam addClientProfileParam);
+    Task setClientProfile(SetClientProfileParam setClientProfileParam);
+    Task removeClientProfile(RemoveClientProfileParam removeClientProfileParam);
+    void clearLastError();
+    void addTestServer();
+    Task setUserSettings(UserSettings userSettings);
+    Task log();
+}

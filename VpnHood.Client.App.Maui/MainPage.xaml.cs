f74e3b15be79b3dcd417f0043402eeabc16a67a3
Trudy <trudy@vpnhood.com>
2023-08-30 13:23:58 -0700

    maui

--- VpnHood.Client.App.Maui/MainPage.xaml.cs
+++ VpnHood.Client.App.Maui/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using VpnHood.Client.App.UI;
+﻿using Microsoft.Maui.Handlers;
+using VpnHood.Client.App.UI;
 
 namespace VpnHood.Client.App.Maui;
 
@@ -9,8 +10,22 @@ public partial class MainPage
         InitializeComponent();
         MainWebView.Source = VpnHoodAppUi.Instance.Url.AbsoluteUri;
         MainWebView.Navigated += MainWebView_Navigated;
+        MainWebView.Navigating += MainWebView_Navigating;
+
+        //var z = (IWebViewHandler)MainWebView.Handler;
+        //var a = ((IWebViewHandler)MainWebView.Handler).PlatformView;
+        //a.SetWebChromeClient(new VpnHood.Client.App.Maui.MyWebChromeClient((IWebViewHandler)MainWebView.Handler));
     }
 
+
+    private void MainWebView_Navigating(object? sender, WebNavigatingEventArgs e)
+    {
+        if (new Uri(e.Url).Host != VpnHoodAppUi.Instance.Url.Host)
+        {
+            e.Cancel = true;
+            Launcher.OpenAsync("https://learn.microsoft.com/dotnet/maui");
+        }
+    }
     private void MainWebView_Navigated(object? sender, WebNavigatedEventArgs e)
     {
         _ = HideSplashScreen();
@@ -20,5 +35,15 @@ public partial class MainPage
     {
         await SplashScreen.FadeTo(0, 2000);
         MainLayout.Remove(SplashScreen);
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+
+        if (!MainWebView.CanGoBack)
+            return base.OnBackButtonPressed();
+
+        MainWebView.GoBack();
+        return true;
     }
 }

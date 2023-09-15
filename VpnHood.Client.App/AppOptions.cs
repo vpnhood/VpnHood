1e564c3181d20efa2ec808f62d4af9970f8afa7c
Trudy <trudy@vpnhood.com>
2023-09-15 13:30:46 -0700

    fix check new version

--- VpnHood.Client.App/AppOptions.cs
+++ VpnHood.Client.App/AppOptions.cs
@@ -6,17 +6,10 @@ namespace VpnHood.Client.App;
 
 public class AppOptions
 {
-    public AppOptions()
-    {
-        AppDataFolderPath = 
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VpnHood");
-    }
-
-    public Uri? UpdateInfoUrl { get; set; }
-    public string AppDataFolderPath { get; set; }
+    public string AppDataFolderPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VpnHood");
     public TimeSpan SessionTimeout { get; set; } = new ClientOptions().SessionTimeout;
     public SocketFactory? SocketFactory { get; set; } = null;
-    public TimeSpan UpdateCheckerInterval { get; set; } = TimeSpan.FromHours(1);
+    public TimeSpan UpdateCheckerInterval { get; set; } = TimeSpan.FromHours(6);
     public bool LoadCountryIpGroups { get; set; } = true;
     // ReSharper disable once StringLiteralTypo
     public string? AppGa4MeasurementId { get; init; } = "G-4LE99XKZYE";

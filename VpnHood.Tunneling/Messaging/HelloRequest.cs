a77f50de0775177fcd699473b984df36850a2748
Trudy <trudy@vpnhood.com>
2023-09-17 01:24:11 -0700

    fix UdpCompatibility

--- VpnHood.Tunneling/Messaging/HelloRequest.cs
+++ VpnHood.Tunneling/Messaging/HelloRequest.cs
@@ -9,4 +9,8 @@ public class HelloRequest : SessionRequest
         : base((byte)Messaging.RequestCode.Hello, requestId,  tokenId, clientInfo, encryptedClientId)
     {
     }
+
+    //todo remove
+    [Obsolete("Not needed for server >= 416")]
+    public bool UseUdpChannel2 { get; set; } = true; 
 }

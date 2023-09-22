ca69ee93fc59d890c50589e27f3d0b241f450bf0
Trudy <trudy@vpnhood.com>
2023-09-21 18:27:52 -0700

    prepare UdpPacket Request

--- VpnHood.Client.App.Swagger/_recreate-api.ps1
+++ VpnHood.Client.App.Swagger/_recreate-api.ps1
@@ -5,7 +5,7 @@ $projectFile="$curDir/VpnHood.Client.App.Swagger.csproj";
 $namespace = "VpnHood.Client.Api";
 $nswagFile = "$curDir/Api/Api.nswag";
 $outBaseFile = "VpnHood.Client.Api";
-$noBuild = $true;
+$noBuild = $false;
 
 # run
 $nswagExe = "${Env:ProgramFiles(x86)}/Rico Suter/NSwagStudio/Net70/dotnet-nswag.exe";
---
+++ VpnHood.Tunneling/Messaging/UdpPacketRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+
+namespace VpnHood.Tunneling.Messaging;
+
+public class UdpPacketRequest : RequestBase
+{
+    public required byte[][] PacketBuffers { get; init; }
+    public required IPAddress DestinationAddress { get; init; }
+
+    [JsonConstructor]
+    public UdpPacketRequest(string requestId, ulong sessionId, byte[] sessionKey)
+        : base(Messaging.RequestCode.TcpDatagramChannel, requestId, sessionId, sessionKey)
+    {
+    }
+}

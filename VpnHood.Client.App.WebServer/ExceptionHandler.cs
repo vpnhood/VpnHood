a7f8a0a1c53bd0b75da7b13617309c226a31197b
Trudy <trudy@vpnhood.com>
2023-09-22 12:23:18 -0700

    imrpove typescript api

---
+++ VpnHood.Client.App.WebServer/ExceptionHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EmbedIO;
+using VpnHood.Common.Client;
+using VpnHood.Common.Exceptions;
+
+namespace VpnHood.Client.App.WebServer;
+
+internal static class ExceptionHandler
+{
+    private static Type GetExceptionType(Exception ex)
+    {
+        if (AlreadyExistsException.Is(ex)) return typeof(AlreadyExistsException);
+        if (NotExistsException.Is(ex)) return typeof(NotExistsException);
+        return ex.GetType();
+    }
+
+    public static Task DataResponseForException(IHttpContext context, Exception ex)
+    {
+        // set correct https status code depends on exception
+        if (NotExistsException.Is(ex)) context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+        else if (AlreadyExistsException.Is(ex)) context.Response.StatusCode = (int)HttpStatusCode.Conflict;
+        else if (ex is UnauthorizedAccessException) context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+        else context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+        var typeFullName = GetExceptionType(ex).FullName;
+        var message = ex.Message;
+        if (!string.IsNullOrEmpty(ex.InnerException?.Message))
+            message += $" InnerMessage: {ex.InnerException?.Message}";
+
+        // set optional information
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        var error = new ApiException.ServerException
+        {
+            Data = new Dictionary<string, string?>(),
+            TypeName = GetExceptionType(ex).Name,
+            TypeFullName = typeFullName,
+            Message = message
+        };
+
+        foreach (DictionaryEntry item in ex.Data)
+        {
+            var key = item.Key.ToString();
+            if (key != null)
+                error.Data.Add(key, item.Value?.ToString());
+        }
+
+        throw new HttpException(HttpStatusCode.BadRequest, error.Message, error);
+    }
+
+    public static async Task DataResponseForHttpException(IHttpContext context, IHttpException httpException)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        await ResponseSerializer.Json(context, httpException.DataObject);
+    }
+}

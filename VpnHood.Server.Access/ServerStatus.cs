524b00a28dda4d74c20ad6cc03ca63a384c43dd8
Trudy <trudy@vpnhood.com>
2023-09-21 18:54:38 -0700

    foo

--- VpnHood.Server.Access/ServerStatus.cs
+++ VpnHood.Server.Access/ServerStatus.cs
@@ -13,4 +13,6 @@ public class ServerStatus
     public int ThreadCount { get; set; }
     public Traffic TunnelSpeed { get; set; } = new();
     public string? ConfigCode { get; set; }
+
+    // todo
 }

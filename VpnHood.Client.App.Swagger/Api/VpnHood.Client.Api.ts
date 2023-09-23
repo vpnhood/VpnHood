5e46637ee0e5c2964b74ee4bf80f8edd19100e2e
Trudy <trudy@vpnhood.com>
2023-09-22 23:58:15 -0700

    change android apk package

--- VpnHood.Client.App.Swagger/Api/VpnHood.Client.Api.ts
+++ VpnHood.Client.App.Swagger/Api/VpnHood.Client.Api.ts
@@ -1875,25 +1875,74 @@ function isAxiosError(obj: any | undefined): obj is AxiosError {
 }
 
 export class ApiException extends Error {
-    override message: string;
-    status: number;
-    response: string;
-    headers: { [key: string]: any; };
-    result: any;
+    statusCode: number;
+    response?: string;
+    exceptionTypeName?: string;
+    exceptionTypeFullName?: string;
+    headers: any;
+    data: any;
 
-    constructor(message: string, status: number, response: string, headers: { [key: string]: any; }, result: any) {
-        super();
+    constructor(
+        message: string,
+        statusCode: number,
+        response?: string,
+        headers?: any,
+        innerException?: Error
+    ) {
+        super(ApiException.buildMessage(message, statusCode, response));
+        Object.setPrototypeOf(this, ApiException.prototype);
 
-        this.message = message;
-        this.status = status;
+        this.statusCode = statusCode;
         this.response = response;
         this.headers = headers;
-        this.result = result;
+
+        let serverException: ServerException = ServerException.tryParse(response);
+        if (serverException != null) {
+            Object.keys(serverException.data).forEach((key) => {
+                this[key] = serverException.data[key];
+            });
+            this.response = JSON.stringify(serverException, null, 2);
+            this.exceptionTypeName = serverException.typeName;
+            this.exceptionTypeFullName = serverException.typeFullName;
+        }
+
+        if (innerException) {
+            this.stack = innerException.stack;
+        }
     }
 
-    protected isApiException = true;
+    private static buildMessage(
+        message: string,
+        statusCode: number,
+        response: string | null
+    ): string {
+        let serverException = ServerException.tryParse(response);
+        if (serverException != null)
+            return serverException.message || '';
 
-    static isApiException(obj: any): obj is ApiException {
-        return obj.isApiException === true;
+        return `${message}\n\nStatus: ${statusCode}\nResponse:\n${response?.substring(0, Math.min(512, response.length))}`;
+    }
+
+    toString(): string {
+        return `HTTP Response:\n\n${this.response}\n\n${super.toString()}`;
+    }
+}
+
+class ServerException {
+    data!: { [key: string]: string | null };
+    typeName?: string;
+    typeFullName?: string;
+    message?: string;
+
+    public static tryParse(value: string): ServerException | null {
+        if (!value)
+            return null;
+
+        try {
+            let outServerException: ServerException = JSON.parse(value);
+            return outServerException.typeName != null ? outServerException : null;
+        } catch {
+            return null;
+        }
     }
 }

99b84617ab9ea50d7377950ee16c1fee58256386
Trudy <trudy@vpnhood.com>
2023-10-16 20:14:04 -0700

    add Android Quick Title
    Former-commit-id: cbe4cce12b1b31f94c41683cfb7ad99c6bc747aa

--- VpnHood.Client.App.Android.Xamarin/App.cs
+++ VpnHood.Client.App.Android.Xamarin/App.cs
@@ -5,45 +5,44 @@ using Android.Graphics;
 using Android.Runtime;
 using VpnHood.Client.App.Resources;
 
-namespace VpnHood.Client.App.Droid
-{
+namespace VpnHood.Client.App.Droid;
+
 #if DEBUG
-    [Application(Debuggable = true, Banner = "@mipmap/banner")]
+[Application(Debuggable = true, Banner = "@mipmap/banner")]
 #else
     [Application(Debuggable = false, Banner = "@mipmap/banner")]
 #endif
-    internal class App : Application
+internal class App : Application
+{
+    public AppNotification Notification { get; private set; } = default!;
+    public IAppProvider AppProvider { get; } = new AppProvider();
+    public static App? Current { get; private set; }
+    public static Color BackgroundColor => new (UiDefaults.WindowBackgroundColor.R, UiDefaults.WindowBackgroundColor.G, UiDefaults.WindowBackgroundColor.B, UiDefaults.WindowBackgroundColor.A);
+
+    public App(IntPtr javaReference, JniHandleOwnership transfer)
+        : base(javaReference, transfer)
     {
-        public AppNotification Notification { get; private set; } = default!;
-        public IAppProvider AppProvider { get; } = new AppProvider();
-        public static App? Current { get; private set; }
-        public static Color BackgroundColor => new (UiDefaults.WindowBackgroundColor.R, UiDefaults.WindowBackgroundColor.G, UiDefaults.WindowBackgroundColor.B, UiDefaults.WindowBackgroundColor.A);
+    }
 
-        public App(IntPtr javaReference, JniHandleOwnership transfer)
-            : base(javaReference, transfer)
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        Current = this;
+
+        //app init
+        if (!VpnHoodApp.IsInit) VpnHoodApp.Init(AppProvider);
+        Notification = new AppNotification(this);
+        VpnHoodApp.Instance.ConnectionStateChanged += (_, _) => Notification.Update();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
         {
+            if (VpnHoodApp.IsInit) _ = VpnHoodApp.Instance.DisposeAsync();
+            Notification?.Dispose();
         }
 
-        public override void OnCreate()
-        {
-            base.OnCreate();
-            Current = this;
-
-            //app init
-            if (!VpnHoodApp.IsInit) VpnHoodApp.Init(AppProvider);
-            Notification = new AppNotification(this);
-            VpnHoodApp.Instance.ConnectionStateChanged += (_, _) => Notification.Update();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                if (VpnHoodApp.IsInit) _ = VpnHoodApp.Instance.DisposeAsync();
-                Notification?.Dispose();
-            }
-
-            base.Dispose(disposing);
-        }
+        base.Dispose(disposing);
     }
 }

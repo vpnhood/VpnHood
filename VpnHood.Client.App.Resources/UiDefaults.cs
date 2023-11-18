1124bcdbaa808ca5cf7b83fe0b80ea04bbd9be36
Trudy <trudy@vpnhood.com>
2023-11-18 12:44:14 -0800

    fix manifest

--- VpnHood.Client.App.Resources/UiDefaults.cs
+++ VpnHood.Client.App.Resources/UiDefaults.cs
@@ -5,5 +5,6 @@ namespace VpnHood.Client.App.Resources;
 public static class UiDefaults
 {
     public static Color WindowBackgroundColor = Color.FromArgb(0x19, 0x40, 0xb0);
+    public static Color WindowBackgroundBottomColor = Color.FromArgb(18, 34, 114);
     public static Size WindowSize = new (400, 700);
 }

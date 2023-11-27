f80174a80d7623e4f2a534d5bb03d4ff31efecfe
Trudy <trudy@vpnhood.com>
2023-11-27 03:59:35 -0800

    improve common; remove some extra permission

--- VpnHood.Client.App.Win/WinAppProvider.cs
+++ VpnHood.Client.App.Win/WinAppProvider.cs
@@ -10,5 +10,6 @@ internal class VpnHoodAppProvider : IAppProvider
     public IDevice Device { get; } = new WinDivertDevice();
     public bool IsLogToConsoleSupported => true;
     public Uri? AdditionalUiUrl => WinApp.Instance.RegisterLocalDomain();
+    //todo move
     public Uri UpdateInfoUrl => new ("https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodClient-win-x64.json");
 }

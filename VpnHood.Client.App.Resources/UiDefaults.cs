b196c355efd3aaae8c9afbe7e817cf46847053d5
Trudy <trudy@vpnhood.com>
2023-11-27 00:07:07 -0800

    before removing ads

--- VpnHood.Client.App.Resources/UiDefaults.cs
+++ VpnHood.Client.App.Resources/UiDefaults.cs
@@ -2,9 +2,27 @@
 
 namespace VpnHood.Client.App.Resources;
 
-public static class UiDefaults
+public static class UiDefaults 
 {
-    public static Color WindowBackgroundColor = Color.FromArgb(0x19, 0x40, 0xb0);
-    public static Color WindowBackgroundBottomColor = Color.FromArgb(18, 34, 114);
-    public static Size WindowSize = new (400, 700);
+    public static AppResources AppResources { get; } = new()
+    {
+        SpaZipData = UiResource.SPA,
+        Colors = new AppResources.AppColors
+        {
+            WindowBackgroundBottomColor = Color.FromArgb(18, 34, 114),
+            WindowBackgroundColor = Color.FromArgb(0x19, 0x40, 0xb0),
+        },
+        Icons = new AppResources.AppIcons
+        {
+            AppIcon = new AppResources.IconData(UiResource.VpnHoodIcon),
+            BadgeConnectedIcon = new AppResources.IconData(UiResource.BadgeVpnConnected),
+            BadgeConnectingIcon = new AppResources.IconData(UiResource.BadgeVpnConnecting),
+            ConnectedIcon = new AppResources.IconData(UiResource.VpnConnectedIcon),
+            ConnectingIcon = new AppResources.IconData(UiResource.VpnConnectingIcon),
+            DisconnectedIcon = new AppResources.IconData(UiResource.VpnDisconnectedIcon),
+            NotificationImage = new AppResources.ImageData(UiResource.NotificationImage),
+            QuickLaunchTileImage = new AppResources.ImageData(UiResource.QuickLaunchTileImage),
+        }
+    };
 }
+

37ce8407825efa9d3c2c717648c2018255e06b91
Trudy <trudy@vpnhood.com>
2023-12-21 02:45:57 -0800

    fix job updater

---
+++ VpnHood.Common/JobController/JobConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace VpnHood.Common.JobController;
+
+public class JobConfig
+{
+    public TimeSpan Interval { get; init; } = TimeSpan.FromSeconds(30);
+    public TimeSpan? DueTime { get; init; }
+    public string? Name { get; init; }
+}

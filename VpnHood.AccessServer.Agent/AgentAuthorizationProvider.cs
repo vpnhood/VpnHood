7bf3e793f4195ba7c2710d1c9bc10922d763b79d
trudy <trudy@vpnhood.com>
2024-01-10 00:40:59 -0800

    update nugets

--- VpnHood.AccessServer.Agent/AgentAuthorizationProvider.cs
+++ VpnHood.AccessServer.Agent/AgentAuthorizationProvider.cs
@@ -5,29 +5,22 @@ using VpnHood.AccessServer.Agent.Services;
 
 namespace VpnHood.AccessServer.Agent;
 
-public class AgentAuthorizationProvider : IAuthorizationProvider
+public class AgentAuthorizationProvider(
+    CacheService cacheService, 
+    IOptions<AgentOptions> agentOptions) : IAuthorizationProvider
 {
-    private readonly CacheService _cacheService;
-    private readonly AgentOptions _agentOptions;
-
-    public AgentAuthorizationProvider(CacheService cacheService, IOptions<AgentOptions> agentOptions)
-    {
-        _cacheService = cacheService;
-        _agentOptions = agentOptions.Value;
-    }
-
     public async Task<string?> GetAuthorizationCode(ClaimsPrincipal principal)
     {
         if (principal.HasClaim("usage_type", "agent"))
         {
             var id = await GetUserId(principal) ?? throw new Exception("Could not find server id.");
-            var server = await _cacheService.GetServer(Guid.Parse(id));
+            var server = await cacheService.GetServer(Guid.Parse(id));
             return server.AuthorizationCode.ToString();
         }
 
         if (principal.HasClaim("usage_type", "system"))
         {
-            return _agentOptions.SystemAuthorizationCode;
+            return agentOptions.Value.SystemAuthorizationCode;
         }
 
         throw new Exception("The access token has invalid usage_type.");
@@ -42,5 +35,10 @@ public class AgentAuthorizationProvider : IAuthorizationProvider
     public Task OnAuthenticated(ClaimsPrincipal principal)
     {
         return Task.CompletedTask;
+    }
+
+    public Task RestAuthorizationCode(ClaimsPrincipal principal)
+    {
+        throw new NotSupportedException();
     }
 }
--- VpnHood.AccessServer.Test/Helper/TestBotAuthenticationProvider.cs
+++ VpnHood.AccessServer.Test/Helper/TestBotAuthenticationProvider.cs
@@ -4,36 +4,34 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace VpnHood.AccessServer.Test.Helper;
 
-public class TestAuthorizationProvider : IAuthorizationProvider
+public class TestAuthorizationProvider(IServiceScopeFactory serviceScopeFactory) : IAuthorizationProvider
 {
-    private readonly IServiceScopeFactory _serviceScopeFactory;
-
-    public TestAuthorizationProvider(IServiceScopeFactory serviceScopeFactory)
-    {
-        _serviceScopeFactory = serviceScopeFactory;
-    }
-
     public async Task<string?> GetAuthorizationCode(ClaimsPrincipal principal)
     {
         if (principal.FindFirstValue("test_authenticated") == "1")
             return "test_1234";
 
-        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        await using var scope = serviceScopeFactory.CreateAsyncScope();
         var original = scope.ServiceProvider.GetServices<IAuthorizationProvider>();
         return await original.First(x => x != this).GetAuthorizationCode(principal);
     }
 
     public async Task<string?> GetUserId(ClaimsPrincipal principal)
     {
-        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        await using var scope = serviceScopeFactory.CreateAsyncScope();
         var original = scope.ServiceProvider.GetServices<IAuthorizationProvider>();
         return await original.First(x => x != this).GetUserId(principal);
     }
 
     public async Task OnAuthenticated(ClaimsPrincipal principal)
     {
-        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        await using var scope = serviceScopeFactory.CreateAsyncScope();
         var original = scope.ServiceProvider.GetServices<IAuthorizationProvider>();
         await original.First(x => x != this).OnAuthenticated(principal);
+    }
+
+    public Task RestAuthorizationCode(ClaimsPrincipal principal)
+    {
+        throw new NotSupportedException();
     }
 }

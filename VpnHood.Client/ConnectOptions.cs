e9769cba8b23bf298eeb26dbac0782743f24da61
Trudy <trudy@vpnhood.com>
2024-01-13 02:01:19 -0800

    fix reconnect issue in windows

--- VpnHood.Client/ConnectOptions.cs
+++ VpnHood.Client/ConnectOptions.cs
@@ -7,7 +7,7 @@ public class ConnectOptions
     /// <summary>
     ///     Time in millisecond
     /// </summary>
-    public TimeSpan ReconnectDelay { get; set; } = TimeSpan.FromSeconds(20);
+    public TimeSpan ReconnectDelay { get; set; } = TimeSpan.FromSeconds(10); //to do : should be 20 seconds
 
     public UdpChannelMode UdpChannelMode { get; set; } = UdpChannelMode.Off;
 }

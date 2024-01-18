922b9a8ac8f57c2d6d298cb317d1a4786c81d1e1
Peter <peter@vpnhood.com>
2024-01-19 03:02:05 +0330

    new
    Former-commit-id: 0feefcb9d7c5120eecc8edf11f06e8b0e203a89e

--- VpnHood.Client.App.Android.Connect/MainActivity2.cs
+++ VpnHood.Client.App.Android.Connect/MainActivity2.cs
@@ -27,35 +27,20 @@ namespace VpnHood.Client.App.Droid.Connect;
 public class MainActivity : AndroidAppWebViewMainActivity
 {
     private GoogleSignInOptions _googleSignInOptions = default!;
-    //private GoogleApiClient _googleApiClient = default!;
+    private GoogleSignInClient _googleSignInClient = default!;
     private BillingClient _billingClient = default!;
-    
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         MobileAds.Initialize(this);
 
-        // Set our simple view
-        /*var linearLayout = new LinearLayout(this);
-
-        _connectButton = new Button(this);
-        _connectButton.Click += ConnectButton_Click;
-        _connectButton.Text += "Test";
-        linearLayout.AddView(_connectButton);
-
-        _statusTextView = new TextView(this);
-        linearLayout.AddView(_statusTextView);
-        SetContentView(linearLayout);*/
-
         // Signin with Google
-        /*_googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+        _googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
             .RequestIdToken("147087744118-asq131v41mqrt777frtghbv66u5u4d2d.apps.googleusercontent.com")
             .RequestEmail()
             .Build();
-        _googleApiClient = new GoogleApiClient.Builder(this)
-            .AddApi(Auth.GOOGLE_SIGN_IN_API, _googleSignInOptions).Build();
-        _googleApiClient.Connect();*/
-
+        _googleSignInClient = GoogleSignIn.GetClient(this, _googleSignInOptions);
 
         // Google play billing
         var billingBuilder = BillingClient.NewBuilder(this);
@@ -65,9 +50,82 @@ public class MainActivity : AndroidAppWebViewMainActivity
         StartConnection();
     }
 
-    private void StartConnection()
+    private async void StartConnection()
     {
-        _billingClient.StartConnection(OnBillingSetupFinished, OnBillingServiceDisconnected);
+        //_billingClient.StartConnection(OnBillingSetupFinished, OnBillingServiceDisconnected);
+        var billingResult = await _billingClient.StartConnectionAsync();
+        switch (billingResult.ResponseCode)
+        {
+            case BillingResponseCode.ServiceDisconnected:
+                throw new NotImplementedException();
+                break;
+            case BillingResponseCode.Ok:
+                OnBillingSetupFinished();
+                break;
+            case BillingResponseCode.BillingUnavailable:
+                throw new NotImplementedException();
+            case BillingResponseCode.DeveloperError:
+                throw new NotImplementedException();
+            case BillingResponseCode.Error:
+                throw new NotImplementedException();
+            case BillingResponseCode.FeatureNotSupported:
+                throw new NotImplementedException();
+            case BillingResponseCode.ItemAlreadyOwned:
+                throw new NotImplementedException();
+            case BillingResponseCode.ItemNotOwned:
+                throw new NotImplementedException();
+            case BillingResponseCode.ItemUnavailable:
+                throw new NotImplementedException();
+            case BillingResponseCode.NetworkError:
+                throw new NotImplementedException();
+            case BillingResponseCode.ServiceTimeout:
+                throw new NotImplementedException();
+            case BillingResponseCode.ServiceUnavailable:
+                throw new NotImplementedException();
+            case BillingResponseCode.UserCancelled:
+                throw new NotImplementedException();
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private async void OnBillingSetupFinished(BillingResult? billingResult = null)
+    {
+        var isDeviceSupportSubscription = _billingClient.IsFeatureSupported("subscriptions");
+        if (isDeviceSupportSubscription.ResponseCode == BillingResponseCode.FeatureNotSupported)
+            throw new NotImplementedException();
+
+        var productDetailsParams = QueryProductDetailsParams.NewBuilder()
+            .SetProductList([
+                QueryProductDetailsParams.Product.NewBuilder()
+                    .SetProductId("hidden_farm")
+                    .SetProductType(BillingClient.ProductType.Subs)
+                    .Build()
+            ]).Build();
+
+        try
+        {
+            var productsDetails = await _billingClient.QueryProductDetailsAsync(productDetailsParams);
+            if (productsDetails.ProductDetails.Count > 0)
+            {
+                OnProductDetailsResponse(productsDetails.Result, productsDetails.ProductDetails);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    private void OnProductDetailsResponse(BillingResult billingResult, IList<ProductDetails> productDetailsList)
+    {
+
+        var isDeviceSupportSubscription = _billingClient.IsFeatureSupported("subscriptions");
+        if (isDeviceSupportSubscription.ResponseCode == BillingResponseCode.FeatureNotSupported)
+            throw new NotImplementedException();
+
+        Console.WriteLine(productDetailsList);
+        Console.WriteLine(billingResult);
     }
 
     private void PurchasesUpdatedListener(BillingResult billingResult, IList<Purchase> purchases)
@@ -107,76 +165,37 @@ public class MainActivity : AndroidAppWebViewMainActivity
         }
     }
 
-    private async void OnBillingSetupFinished(BillingResult billingResult)
-    {
-        if (billingResult.ResponseCode == BillingResponseCode.Ok)
-        {
-            var productDetailsParams = QueryProductDetailsParams.NewBuilder()
-                .SetProductList([
-                    QueryProductDetailsParams.Product.NewBuilder()
-                        .SetProductId("hh")
-                        .SetProductType(BillingClient.ProductType.Subs)
-                        .Build()
-                ]).Build();
-
-            try
-            {
-                var productsDetails = await _billingClient.QueryProductDetailsAsync(productDetailsParams);
-                OnProductDetailsResponse(productsDetails.Result, productsDetails.ProductDetails);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
-
-        }
-    }
 
     private void OnBillingServiceDisconnected()
     {
         StartConnection();
     }
 
-    private void OnProductDetailsResponse(BillingResult billingResult, IList<ProductDetails> productDetailsList)
-    {
-        if (productDetailsList.Count == 0)
-            throw new NotImplementedException();
-
-        var isDeviceSupportSubscription = _billingClient.IsFeatureSupported("subscriptions");
-        if (isDeviceSupportSubscription.ResponseCode == BillingResponseCode.FeatureNotSupported)
-            throw new NotImplementedException();
-
-        Console.WriteLine(productDetailsList);
-        Console.WriteLine(billingResult);
-    }
-
 
 
     // Google signin button
-    /*[Obsolete("Obsolete")]
+
     private void ConnectButton_Click(object? sender, EventArgs e)
     {
         _statusTextView.Text = "Signin with Google";
-        var intent = Auth.GoogleSignInApi.GetSignInIntent(_googleApiClient);
+        var intent = _googleSignInClient.SignInIntent;
         StartActivityForResult(intent, 1);
-    }*/
+    }
 
     // Google signin result
-    /*protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent? data)
+    protected override async void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent? data)
     {
         base.OnActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 1)
         {
-            GoogleSignInResult result = Auth.GoogleSignInApi.GetSignInResultFromIntent(data);
+            var account = await GoogleSignIn.GetSignedInAccountFromIntentAsync(data);
 
-            if (result.IsSuccess)
+            if (account.IdToken != null)
             {
-                _statusTextView.Text = "Id Token: " + result.SignInAccount.IdToken;
+                _statusTextView.Text = "Id Token: " + account.IdToken;
             }
         }
-    }*/
+    }
 
 }

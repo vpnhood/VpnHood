b077b9d91f045ce419e0f3ed1d5e7861c4d65067
Trudy <trudy@vpnhood.com>
2024-01-20 21:49:28 -0800

    remove IAppService

--- Tests/VpnHood.Test/TestAppService.cs
+++
@@ -1,9 +0,0 @@
-﻿using VpnHood.Client.App.Abstractions;
-using VpnHood.Client.Device;
-
-namespace VpnHood.Test;
-
-internal class TestAppService(TestDeviceOptions? testDeviceOptions) : IAppService
-{
-    public IDevice Device { get; } = TestHelper.CreateDevice(testDeviceOptions);
-}

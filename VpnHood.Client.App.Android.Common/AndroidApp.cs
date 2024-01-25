8168f5c1b2bbf5257b92489d5141140a9cb32873
Trudy <trudy@vpnhood.com>
2024-01-24 23:33:12 -0800

    switch for listen to all ip in internal webserver

--- VpnHood.Client.App.Android.Common/AndroidApp.cs
+++ VpnHood.Client.App.Android.Common/AndroidApp.cs
@@ -3,15 +3,11 @@ using VpnHood.Client.Device.Droid;
 
 namespace VpnHood.Client.App.Droid.Common;
 
-public class AndroidApp : Application
+public class AndroidApp(IntPtr javaReference, JniHandleOwnership transfer) 
+    : Application(javaReference, transfer)
 {
     private AndroidAppNotification? _appNotification;
     protected virtual AppOptions AppOptions { get; } = new();
-
-    public AndroidApp(IntPtr javaReference, JniHandleOwnership transfer)
-        : base(javaReference, transfer)
-    {
-    }
 
     public override void OnCreate()
     {

d3e5a2a9f4d7e674ea5715f49bc10169165e91ed
Trudy <trudy@vpnhood.com>
2024-01-24 21:05:18 -0800

    clean up

--- Tests/VpnHood.Test/Tests/AccessTest.cs
+++ Tests/VpnHood.Test/Tests/AccessTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VpnHood.Client;
 using VpnHood.Common.Logging;

2fd8c369b51a240b822d6efd7bdba9b4458506a9
Trudy <trudy@vpnhood.com>
2024-01-28 20:50:23 -0800

    remove some exceptions from authenticaton

--- VpnHood.Client.App/Abstractions/IAppAuthenticationExternalService.cs
+++ VpnHood.Client.App/Abstractions/IAppAuthenticationExternalService.cs
@@ -2,7 +2,7 @@ namespace VpnHood.Client.App.Abstractions;
 
 public interface IAppAuthenticationExternalService: IDisposable
 {
-    public Task<string?> TrySilentSignIn();
+    public Task<string> SilentSignIn();
     public Task<string> SignIn();
     public Task SignOut();
 

21abefaa1ec53c946b885fdf4b4ee96aec8f74b2
Trudy <trudy@vpnhood.com>
2024-01-29 15:06:24 -0800

    fix DataResponseForException

--- VpnHood.Client.App.WebServer/ExceptionHandler.cs
+++ VpnHood.Client.App.WebServer/ExceptionHandler.cs
@@ -10,17 +10,39 @@ internal static class ExceptionHandler
 {
     public static Task DataResponseForException(IHttpContext context, Exception ex)
     {
-        // set correct https status code depends on exception
-        if (NotExistsException.Is(ex)) context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-        else if (AlreadyExistsException.Is(ex)) context.Response.StatusCode = (int)HttpStatusCode.Conflict;
-        else if (ex is UnauthorizedAccessException) context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-        else context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        context.Response.ContentType = MediaTypeNames.Application.Json;
+        if (ex is ApiException apiException)
+        {
+            var apiError = new ApiError(apiException.ExceptionTypeName ?? nameof(ApiException), ex.Message)
+            {
+                TypeFullName = apiException.ExceptionTypeFullName,
+                InnerMessage = apiException.InnerException?.Message,
+            };
 
+            foreach (var key in apiException.Data)
+            {
+                if (key is string keyStr)
+                    apiError.Data.TryAdd(keyStr, ex.Data[keyStr]?.ToString());
+                apiError.Data.TryAdd("InnerStatusCode", apiException.StatusCode.ToString());
+            }
 
-        // create portable exception
-        var apiError = new ApiError(ex);
-        throw new HttpException(HttpStatusCode.BadRequest, apiError.Message, apiError);
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            context.Response.StatusCode = apiException.StatusCode;
+            throw new HttpException(HttpStatusCode.BadRequest, apiError.Message, apiError);
+        }
+        else
+        {
+
+            // set correct https status code depends on exception
+            if (NotExistsException.Is(ex)) context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            else if (AlreadyExistsException.Is(ex)) context.Response.StatusCode = (int)HttpStatusCode.Conflict;
+            else if (ex is UnauthorizedAccessException) context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            else context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+
+            // create portable exception
+            var apiError = new ApiError(ex);
+            throw new HttpException(HttpStatusCode.BadRequest, apiError.Message, apiError);
+        }
     }
 
     public static Task DataResponseForHttpException(IHttpContext context, IHttpException httpException)

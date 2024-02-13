18f6c6081736fe0d8169b6eceb21a7826415b6bc
Trudy <trudy@vpnhood.com>
2024-02-13 14:14:10 -0800

    decouple SessionRequestEx

--- VpnHood.Server/SessionManager.cs
+++ VpnHood.Server/SessionManager.cs
@@ -89,7 +89,7 @@ public class SessionManager : IAsyncDisposable, IJob
             ? VhUtil.JsonDeserialize<SessionExtraData>(sessionResponseEx.ExtraData)
             : new SessionExtraData { ProtocolVersion = 3 };
 
-        var session = new Session(_accessManager, sessionResponseEx, NetFilter, _socketFactory, 
+        var session = new Session(_accessManager, sessionResponseEx, NetFilter, _socketFactory,
             SessionOptions, TrackingOptions, extraData);
 
         // add to sessions
@@ -109,13 +109,15 @@ public class SessionManager : IAsyncDisposable, IJob
         // validate the token
         VhLogger.Instance.Log(LogLevel.Trace, "Validating the request by the access server. TokenId: {TokenId}", VhLogger.FormatId(helloRequest.TokenId));
         var extraData = JsonSerializer.Serialize(new SessionExtraData { ProtocolVersion = helloRequest.ClientInfo.ProtocolVersion });
-        var sessionResponseEx = await _accessManager.Session_Create(new SessionRequestEx(helloRequest, ipEndPointPair.LocalEndPoint)
+        var sessionResponseEx = await _accessManager.Session_Create(new SessionRequestEx
         {
             HostEndPoint = ipEndPointPair.LocalEndPoint,
             ClientIp = ipEndPointPair.RemoteEndPoint.Address,
-            ExtraData = extraData
+            ExtraData = extraData,
+            ClientInfo = helloRequest.ClientInfo,
+            EncryptedClientId = helloRequest.EncryptedClientId,
+            TokenId = helloRequest.TokenId,
         });
-        sessionResponseEx.ExtraData = extraData; //extraData may not return by session creation
 
         // Access Error should not pass to the client in create session
         if (sessionResponseEx.ErrorCode is SessionErrorCode.AccessError)

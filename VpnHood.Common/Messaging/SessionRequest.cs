84cd6015dce169e5b6da92830ca7e43ef086a2c5
Trudy <trudy@vpnhood.com>
2024-02-13 14:53:58 -0800

    remove SessionRequest

--- VpnHood.Common/Messaging/SessionRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VpnHood.Common.Messaging;
-
-public abstract class SessionRequest(byte requestCode) : ClientRequest(requestCode)
-{
-    public required string TokenId { get; init; }
-    public required ClientInfo ClientInfo { get; init; }
-    public required byte[] EncryptedClientId { get; init; }
-}

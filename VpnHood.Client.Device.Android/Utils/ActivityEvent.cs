bd61dfb76dc3d2b67dbf2d7db2ec99ace6f67510
Trudy <trudy@vpnhood.com>
2024-03-01 13:43:54 -0800

    use IActivityEvent instead Activity

--- VpnHood.Client.Device.Android/Utils/ActivityEvent.cs
+++ VpnHood.Client.Device.Android/Utils/ActivityEvent.cs
@@ -1,13 +1,60 @@
 ﻿using Android.Content;
+using Android.Content.PM;
 using Android.Runtime;
+using Android.Views;
 
 namespace VpnHood.Client.Device.Droid.Utils;
 
 public class ActivityEvent : Activity, IActivityEvent
 {
     public event EventHandler<ActivityResultEventArgs>? OnActivityResultEvent;
+    public event EventHandler<CreateEventArgs>? OnCreateEvent;
+    public event EventHandler<NewIntentEventArgs>? OnNewIntentEvent;
+    public event EventHandler<RequestPermissionsResultArgs>? OnRequestPermissionsResultEvent;
+    public event EventHandler<KeyDownArgs>? OnKeyDownEvent;
     public event EventHandler? OnDestroyEvent;
     public Activity Activity => this;
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        OnCreateEvent?.Invoke(this, new CreateEventArgs
+        {
+            SavedInstanceState = savedInstanceState
+        });
+    }
+
+    protected override void OnNewIntent(Intent? intent)
+    {
+        OnNewIntentEvent?.Invoke(this, new NewIntentEventArgs
+        {
+            Intent = intent
+        });
+    }
+
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+    {
+        OnRequestPermissionsResultEvent?.Invoke(this, new RequestPermissionsResultArgs
+        {
+            RequestCode = requestCode,
+            Permissions = permissions,
+            GrantResults = grantResults
+        });
+
+        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent? e)
+    {
+        var args = new KeyDownArgs
+        {
+            KeyCode = keyCode,
+            KeyEvent = e
+        };
+
+        OnKeyDownEvent?.Invoke(this, args);
+        return args.IsHandled ? args.IsHandled : base.OnKeyDown(keyCode, e);
+    }
 
     protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent? data)
     {
@@ -20,6 +67,7 @@ public class ActivityEvent : Activity, IActivityEvent
 
         base.OnActivityResult(requestCode, resultCode, data);
     }
+
     protected override void OnDestroy()
     {
         OnDestroyEvent?.Invoke(this, EventArgs.Empty);
--- VpnHood.Client.Device.Android/Utils/IActivityEvent.cs
+++ VpnHood.Client.Device.Android/Utils/IActivityEvent.cs
@@ -1,8 +1,41 @@
-﻿namespace VpnHood.Client.Device.Droid.Utils;
+﻿using Android.Content;
+using Android.Content.PM;
+using Android.Views;
+
+namespace VpnHood.Client.Device.Droid.Utils;
 
 public interface IActivityEvent
 {
     event EventHandler<ActivityResultEventArgs> OnActivityResultEvent;
+    event EventHandler<CreateEventArgs> OnCreateEvent;
+    event EventHandler<NewIntentEventArgs> OnNewIntentEvent;
+    event EventHandler<RequestPermissionsResultArgs> OnRequestPermissionsResultEvent;
+    event EventHandler<KeyDownArgs> OnKeyDownEvent;
     event EventHandler OnDestroyEvent;
     Activity Activity { get; }
 }
+
+public class KeyDownArgs
+{
+    public required Keycode KeyCode { get; init; }
+    public required KeyEvent? KeyEvent { get; init; }
+    public bool IsHandled { get; set; }
+}
+
+public class RequestPermissionsResultArgs
+{
+    public required int RequestCode { get; init; }
+    public required string[] Permissions { get; init; }
+    public required Permission[] GrantResults { get; init; }
+}
+
+public class NewIntentEventArgs
+{
+    public required Intent? Intent { get; init; }
+}
+
+public class CreateEventArgs
+{
+    public required Bundle? SavedInstanceState { get; init; }
+}
+

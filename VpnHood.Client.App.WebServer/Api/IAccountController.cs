0845a9bf109234ae73b9c371e5eef6ab13195795
Peter <peter@vpnhood.com>
2024-03-14 02:11:52 +0330

    Add new icon, Change account handling (Incomplete)

--- VpnHood.Client.App.WebServer/Api/IAccountController.cs
+++ VpnHood.Client.App.WebServer/Api/IAccountController.cs
@@ -9,6 +9,7 @@ public interface IAccountController
     bool IsSigninWithGoogleSupported();
     Task SignInWithGoogle();
     Task SignOut();
+    Task Refresh();
     Task<AppAccount?> Get();
     Task<bool> IsSubscriptionOrderProcessed(string providerOrderId);
     Task<List<string>> GetAccessKeys(string subscriptionId);

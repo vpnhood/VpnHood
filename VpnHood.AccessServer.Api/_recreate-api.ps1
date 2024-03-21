0c2ac8d6513ed06a3eb6923e50489e7baf8d07a5
trudy <trudy@vpnhood.com>
2024-03-21 01:38:56 -0700

    add ts to API

--- VpnHood.AccessServer.Api/_recreate-api.ps1
+++ VpnHood.AccessServer.Api/_recreate-api.ps1
@@ -3,10 +3,11 @@ $solutionDir = (Split-Path $PSScriptRoot -Parent);
 
 # variables
 $projectFile="$solutionDir/VpnHood.AccessServer/VpnHood.AccessServer.csproj";
-$namespace = "VpnHood.AccessServer.Api";
 $nswagFile = "$curDir/Api/Api.nswag";
+$namespace = "VpnHood.AccessServer.Api";
+$outBaseFile = "VpnHood.AccessServer.Api";
 
 # run
 $nswagExe = "${Env:ProgramFiles(x86)}/Rico Suter/NSwagStudio/Net80/dotnet-nswag.exe";
-$variables="/variables:namespace=$namespace,apiBaseFile=$namespace,projectFile=$projectFile";
+$variables="/variables:namespace=$namespace,apiBaseFile=$outBaseFile,projectFile=$projectFile";
 & "$nswagExe" run $nswagFile $variables;

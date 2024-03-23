e300b2c6ad83dbda2d1699e83fadf92c6e172e62
Peter <peter@vpnhood.com>
2024-03-23 04:33:39 +0330

    New SPA (fix connect button double click issue)
    Former-commit-id: 221788cbcd2020ce15b6df7cafd011346090af9b

--- VpnHood.Client.App.Android.GooglePlay/Ads/InterstitialAdLoadCallback.cs
+++ VpnHood.Client.App.Android.GooglePlay/Ads/InterstitialAdLoadCallback.cs
@@ -6,10 +6,9 @@ using Android.Runtime;
 namespace VpnHood.Client.App.Droid.GooglePlay.Ads;
 
 // fix google ad library problem
-public class InterstitialAdLoadCallback2(Activity activity) : InterstitialAdLoadCallback
+public class InterstitialAdLoadCallback(Activity activity) : global::Android.Gms.Ads.Interstitial.InterstitialAdLoadCallback
 {
     private static Delegate? _cbOnAdLoaded;
-    private InterstitialAd? _mInterstitialAd;
 
     // ReSharper disable once UnusedMember.Local
     private static Delegate GetOnAdLoadedHandler()
@@ -19,23 +18,15 @@ public class InterstitialAdLoadCallback2(Activity activity) : InterstitialAdLoad
 
     private static void OnAdLoadedNative(IntPtr env, IntPtr nativeThis, IntPtr nativeP0)
     {
-        var interstitialAdLoadCallback = GetObject<InterstitialAdLoadCallback2>(env, nativeThis, JniHandleOwnership.DoNotTransfer);
-        var interstitialAd = GetObject<InterstitialAd>(nativeP0, JniHandleOwnership.DoNotTransfer);
+        var interstitialAdLoadCallback = GetObject<InterstitialAdLoadCallback>(env, nativeThis, JniHandleOwnership.DoNotTransfer);
+        var interstitialAd = GetObject<global::Android.Gms.Ads.Interstitial.InterstitialAd>(nativeP0, JniHandleOwnership.DoNotTransfer);
         if (interstitialAd != null)
             interstitialAdLoadCallback?.OnAdLoaded(interstitialAd);
     }
 
     // ReSharper disable once StringLiteralTypo
     [Register("onAdLoaded", "(Lcom/google/android/gms/ads/interstitial/InterstitialAd;)V", "GetOnAdLoadedHandler")]
-    public virtual void OnAdLoaded(InterstitialAd interstitialAd)
+    public virtual void OnAdLoaded(global::Android.Gms.Ads.Interstitial.InterstitialAd interstitialAd)
     {
-        _mInterstitialAd = interstitialAd;
-        base.OnAdLoaded(interstitialAd);
-    }
-
-    public override void OnAdFailedToLoad(LoadAdError loadAdError)
-    {
-        _mInterstitialAd = null;
-        base.OnAdFailedToLoad(loadAdError);
     }
 }

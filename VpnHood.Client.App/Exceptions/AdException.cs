72cda1c314f787febd15009061c2be6111fdc7ed
Trudy <trudy@vpnhood.com>
2024-03-30 16:15:29 -0700

    fix webview open page on  browser

--- VpnHood.Client.App/Exceptions/AdException.cs
+++ VpnHood.Client.App/Exceptions/AdException.cs
@@ -1,3 +1,14 @@
 ﻿namespace VpnHood.Client.App.Exceptions;
 
-public class AdException(string message) : Exception(message);
+
+public class AdException :Exception
+{
+    public AdException(string message, Exception innerException) : base(message, innerException)
+    {
+        
+    }
+
+    public AdException(string message) : base(message)
+    {
+    }
+}

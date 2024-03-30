8bc99e938b0e9723bddb12b2ee0147f64c34f44e
Trudy <trudy@vpnhood.com>
2024-03-30 16:15:29 -0700

    fix webview open page on  browser

---
+++ VpnHood.Tunneling/Messaging/AdRewardRequest.cs
@@ -0,0 +1,9 @@
+﻿using VpnHood.Common.Messaging;
+
+namespace VpnHood.Tunneling.Messaging;
+
+public class AdRewardRequest()
+    : ClientRequest((byte)Messaging.RequestCode.RewardAd)
+{
+    public string? AdData { get; init; }
+}

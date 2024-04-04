e6bd3f79a09bbd5560b60c79280a16522d39bf40
Trudy <trudy@vpnhood.com>
2024-04-04 11:14:48 -0700

    Update UiCultureInfo

--- VpnHood.Client.App.WebServer/Api/UiCultureInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VpnHood.Client.App.WebServer.Api;
-
-public class UiCultureInfo
-{
-    public required string Code { get; init; }
-    public required string NativeName { get; init; }
-}

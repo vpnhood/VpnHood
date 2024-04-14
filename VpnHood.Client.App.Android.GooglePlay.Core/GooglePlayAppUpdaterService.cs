d9f77b72c3a0285e229632b1078c170e8a20090e
Peter <peter@vpnhood.com>
2024-04-15 02:51:35 +0330

    Complete GooglePlayAppUpdaterService.

--- VpnHood.Client.App.Android.GooglePlay.Core/GooglePlayAppUpdaterService.cs
+++ VpnHood.Client.App.Android.GooglePlay.Core/GooglePlayAppUpdaterService.cs
@@ -2,40 +2,56 @@
 using VpnHood.Client.App.Abstractions;
 using VpnHood.Common.Logging;
 using Xamarin.Google.Android.Play.Core.AppUpdate;
-using Xamarin.Google.Android.Play.Core.AppUpdate.Testing;
+using Xamarin.Google.Android.Play.Core.Install;
 using Xamarin.Google.Android.Play.Core.Install.Model;
-using static Android.Icu.Text.IDNA;
 
 namespace VpnHood.Client.App.Droid.GooglePlay;
 
-public class GooglePlayAppUpdaterService(Activity activity) 
-    : IAppUpdaterService
+public class GooglePlayAppUpdaterService(Activity activity) : IAppUpdaterService
 {
+    private readonly TaskCompletionSource<int> _taskCompletionSource = new();
     public async Task<bool> Update()
     {
-        var appUpdateManager = AppUpdateManagerFactory.Create(activity);
+        var appUpdateManager =  AppUpdateManagerFactory.Create(activity);
         try
         {
             var appUpdateInfo = await new GooglePlayTaskCompleteListener<AppUpdateInfo>(appUpdateManager.AppUpdateInfo).Task;
             var updateAvailability = appUpdateInfo.UpdateAvailability();
 
-            // postpone check if check succeeded
-            if (updateAvailability == UpdateAvailability.UpdateAvailable && appUpdateInfo.IsUpdateTypeAllowed(AppUpdateType.Flexible))
-            {
-                appUpdateManager.StartUpdateFlow(appUpdateInfo, activity, AppUpdateOptions.NewBuilder(AppUpdateType.Flexible).Build());
-
-                var appUpdateStatus = appUpdateInfo.InstallStatus();
-                if (appUpdateStatus == InstallStatus.Downloaded)
-                {
-                    var installUpdateStatus = appUpdateManager.CompleteUpdate();
-                    appUpdateManager.Dispose();
-                    return installUpdateStatus.IsComplete;
-                }
-            }
-
             // play set UpdateAvailability.UpdateNotAvailable even when there is no connection to google
             // So we return false if there is UpdateNotAvailable to let the alternative way works
-            return false;
+            if (updateAvailability != UpdateAvailability.UpdateAvailable || !appUpdateInfo.IsUpdateTypeAllowed(AppUpdateType.Flexible))
+                return false;
+
+            // Show Google Play update dialog
+            var updateFlowPlayTask = appUpdateManager.StartUpdateFlow(appUpdateInfo, activity, AppUpdateOptions.NewBuilder(AppUpdateType.Flexible).Build());
+            var updateFlowResult = await new GooglePlayTaskCompleteListener<Java.Lang.Integer>(updateFlowPlayTask).Task;
+            if (updateFlowResult.IntValue() != -1)
+                throw new Exception("Could not start update flow.");
+
+            // Set listener to check download state
+            appUpdateManager.RegisterListener(new GooglePlayDownloadStateListener(_taskCompletionSource));
+            var downloadState = await _taskCompletionSource.Task;
+
+            // Download failed
+            if (downloadState != InstallStatus.Downloaded)
+                return false;
+
+            // Start install downloaded update
+            var installUpdateTask = appUpdateManager.CompleteUpdate();
+            var installUpdateStatus = await new GooglePlayTaskCompleteListener<Java.Lang.Integer>(installUpdateTask).Task;
+
+            // Could not start install
+            if (installUpdateStatus.IntValue() != -1)
+                throw new Exception("Could not complete update.");
+
+            // Set listener to check installation state
+            var installState = await _taskCompletionSource.Task;
+            // TODO Check for unregister
+            //appUpdateManager.UnregisterListener(GooglePlayDownloadStateListener);
+            appUpdateManager.Dispose();
+
+            return installState == InstallStatus.Installed;
         }
         catch (Exception ex)
         {
@@ -44,5 +60,29 @@ public class GooglePlayAppUpdaterService(Activity activity)
         }
     }
 
-
+    public class GooglePlayDownloadStateListener(TaskCompletionSource<int> taskCompletionSource) : Java.Lang.Object, IInstallStateUpdatedListener
+    {
+        public void OnStateUpdate(InstallState state)
+        {
+            var status = state.InstallStatus();
+            switch (status)
+            {
+                case InstallStatus.Downloaded:
+                    taskCompletionSource.TrySetResult(status);
+                    break;
+                case InstallStatus.Installed:
+                    taskCompletionSource.TrySetResult(status);
+                    break;
+                case InstallStatus.Canceled:
+                    taskCompletionSource.SetCanceled();
+                    break;
+                case InstallStatus.Failed:
+                    taskCompletionSource.TrySetException(new Exception("Download Failed."));
+                    break;
+                case InstallStatus.Unknown:
+                    taskCompletionSource.TrySetException(new Exception("Unknown status."));
+                    break;
+            }
+        }
+    }
 }

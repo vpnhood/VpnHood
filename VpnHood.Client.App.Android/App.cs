4785817b22e28413304f044d586bb9f987419143
Trudy <trudy@vpnhood.com>
2024-04-23 20:49:32 -0700

    follow ad requirement by access manager
    Former-commit-id: 91c1ece805c537b7daa2a742ecea7d80b7a4acc1

--- VpnHood.Client.App.Android/App.cs
+++ VpnHood.Client.App.Android/App.cs
@@ -18,6 +18,6 @@ public class App(IntPtr javaReference, JniHandleOwnership transfer)
     {
         Resource = DefaultAppResource.Resource,
         UpdateInfoUrl = AssemblyInfo.UpdateInfoUrl,
-        IsAddServerSupported = true
+        IsAddAccessKeySupported = true
     };
 }

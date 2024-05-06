b1becfaa63e01382dc55ae2b1dde974287cd936e
Trudy <trudy@vpnhood.com>
2024-05-06 03:45:01 -0700

    add quicklaunch, notification and open alwayson on UiService and controller

--- Tests/VpnHood.Test/TestDevice.cs
+++ Tests/VpnHood.Test/TestDevice.cs
@@ -7,14 +7,13 @@ internal class TestDevice(TestDeviceOptions? options = default) : IDevice
 {
     private readonly TestDeviceOptions _options = options ?? new TestDeviceOptions();
 
-#pragma warning disable 0067
     public event EventHandler? StartedAsService;
-#pragma warning restore 0067
     public IAppCultureService? CultureService => null;
     public string OsInfo => Environment.OSVersion + ", " + (Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit");
     public bool IsExcludeAppsSupported => false;
     public bool IsIncludeAppsSupported => false;
     public bool IsLogToConsoleSupported => true;
+    public bool IsAlwaysOnSupported => false;
 
     public DeviceAppInfo[] InstalledApps => throw new NotSupportedException();
 
---
+++ VpnHood.Client.App/AppUiServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using VpnHood.Client.App.Abstractions;
+
+namespace VpnHood.Client.App;
+
+internal class AppUiServiceBase : IAppUiService
+{
+    public bool IsQuickLaunchSupported => false;
+    public Task<bool> RequestQuickLaunch(IAppUiContext uiContext, CancellationToken cancellationToken) => throw new NotSupportedException();
+
+    public bool IsNotificationSupported => false;
+    public Task<bool> RequestNotification(IAppUiContext uiContext, CancellationToken cancellationToken) => throw new NotSupportedException();
+
+    public bool IsOpenAlwaysOnPageSupported => false;
+    public void OpenAlwaysOnPage(IAppUiContext uiContext) => throw new NotSupportedException();
+}

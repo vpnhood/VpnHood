1711dbb4bb10394d69e0ba4ce49598d9852ad174
Trudy <trudy@vpnhood.com>
2024-05-06 02:44:59 -0700

    add storage folder to auth

--- VpnHood.Client.App.Android.Connect/Properties/AssemblyInfo.cs
+++ VpnHood.Client.App.Android.Connect/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using VpnHood.Common.Utils;
 namespace VpnHood.Client.App.Droid.Connect.Properties;
 public static class AssemblyInfo
 {
-    public static string GetMetadata(string key, string defaultValue) => VhUtil.GetAssemblyMetadata(typeof(AssemblyInfo).Assembly, key, defaultValue);
+    public const string AdMonApplicationId = "ca-app-pub-8662231806304184~1740102860";
     public static Uri UpdateInfoUrl => new("https://github.com/vpnhood/VpnHood.Client.Connect/releases/latest/download/VpnHoodConnect-android.json");
     public static bool ListenToAllIps => IsDebugMode;
     public static int? DefaultSpaPort => IsDebugMode ? 9571 : 9570;
@@ -24,6 +24,7 @@ public static class AssemblyInfo
     public static string RewardedAdUnitId => GetMetadata("RewardedAdUnitId", "ca-app-pub-8662231806304184/1656979755");
     public static string GlobalServersAccessKey => GetMetadata("GlobalServersAccessKey", "vh://eyJ2Ijo0LCJuYW1lIjoiR2xvYmFsIFNlcnZlcnMgKEFkKSIsInNpZCI6IjEyNjUiLCJ0aWQiOiI3N2Q1ODYwMy1jZGNiLTRlZmMtOTkyZi1jMTMyYmUxZGUwZTMiLCJpYXQiOiIyMDI0LTA0LTI5VDIwOjAxOjIxLjY1OTgyODRaIiwic2VjIjoicExBbGYrVUhaVzJsTmhUQUJGT2x0QT09IiwiYWQiOnRydWUsInNlciI6eyJjdCI6IjIwMjQtMDQtMTVUMTk6NDQ6MzlaIiwiaG5hbWUiOiJtby5naXdvd3l2eS5uZXQiLCJocG9ydCI6MCwiaXN2IjpmYWxzZSwic2VjIjoidmFCcVU5UkMzUUhhVzR4RjVpYllGdz09IiwiY2giOiIzZ1hPSGU1ZWN1aUM5cStzYk83aGxMb2tRYkE9IiwidXJsIjoiaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3Zwbmhvb2QvVnBuSG9vZC5GYXJtS2V5cy9tYWluL0ZyZWVfZW5jcnlwdGVkX3Rva2VuLnR4dCIsImVwIjpbIjUxLjgxLjIxMC4xNjQ6NDQzIiwiWzI2MDQ6MmRjMDoyMDI6MzAwOjo1Y2VdOjQ0MyJdfX0=");
 
+    public static string GetMetadata(string key, string defaultValue) => VhUtil.GetAssemblyMetadata(typeof(AssemblyInfo).Assembly, key, defaultValue);
     public static bool IsDebugMode
     {
         get

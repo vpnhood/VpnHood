71704c1a53717e8a7d4f6fddab09c611c891b479
Peter <peter@vpnhood.com>
2024-05-09 03:07:35 +0330

    New SPA. Add BillingResultState. Add DebugData in the user setting. Remove cached account file on refresh account.

---
+++ VpnHood.Client.App/Abstractions/BillingPurchaseState.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VpnHood.Client.App.Abstractions;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum BillingPurchaseState
+{
+    None = 0,
+    Started = 1,
+    Processing = 2,
+}

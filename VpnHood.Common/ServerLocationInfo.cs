3ab023ccc6e107329d0b73f18a828b19b372c6bf
Trudy <trudy@vpnhood.com>
2024-05-17 15:20:48 -0700

    ad IsNested to ServerLocationInfo

--- VpnHood.Common/ServerLocationInfo.cs
+++ VpnHood.Common/ServerLocationInfo.cs
@@ -74,48 +74,5 @@ public class ServerLocationInfo : IComparable<ServerLocationInfo>
             return countryCode;
         }
     }
-
-    public static ServerLocationInfo[] AddCategoryGaps(string[]? serverLocations)
-    {
-        serverLocations ??= [];
-        var locationInfos = serverLocations.Select(Parse).ToArray();
-
-        var results = new List<ServerLocationInfo>();
-        var countryCount = new Dictionary<string, int>();
-
-        // Count occurrences of each country and region
-        foreach (var locationInfo in locationInfos)
-        {
-            if (!countryCount.TryAdd(locationInfo.CountryCode, 1))
-                countryCount[locationInfo.CountryCode]++;
-        }
-
-        // Add wildcard serverLocations for countries multiple occurrences
-        var seenCountries = new HashSet<string>();
-        foreach (var locationInfo in locationInfos)
-        {
-            var country = locationInfo.CountryCode;
-
-            // Add wildcard selector for country if it has multiple occurrences
-            if (!seenCountries.Contains(country))
-            {
-                if (countryCount[country] > 1)
-                    results.Add(new ServerLocationInfo
-                    {
-                        CountryCode = locationInfo.CountryCode,
-                        RegionName = "*",
-                    });
-                seenCountries.Add(country);
-            }
-
-            results.Add(locationInfo);
-        }
-
-        if (seenCountries.Count > 1)
-            results.Insert(0, Auto);
-
-        results.Sort();
-        var distinctResults = results.Distinct().ToArray();
-        return distinctResults;
-    }
+   
 }

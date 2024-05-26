08aec90765ff8a39a2c7cab5e621d5898c71f719
Trudy <trudy@vpnhood.com>
2024-05-26 00:50:47 -0700

    server finder
    Former-commit-id: 8db68363e3f981b38bd85e299e4f037f301e85df

--- VpnHood.Client/ServerFinder.cs
+++ VpnHood.Client/ServerFinder.cs
@@ -1,7 +1,10 @@
-﻿using System.Net;
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Threading;
 using Microsoft.Extensions.Logging;
 using VpnHood.Client.ConnectorServices;
 using VpnHood.Common;
+using VpnHood.Common.Exceptions;
 using VpnHood.Common.Logging;
 using VpnHood.Common.Messaging;
 using VpnHood.Common.Utils;
@@ -12,7 +15,7 @@ namespace VpnHood.Client;
 
 public class ServerFinder(int maxDegreeOfParallelism = 10)
 {
-    private readonly Dictionary<IPEndPoint, bool> _hostEndPointStatus = new();
+    private IDictionary<IPEndPoint, bool>? _hostEndPointStatus;
 
     // There are much work to be done here
     public async Task<IPAddress?> FindBestServerAsync(ServerToken serverToken, ISocketFactory socketFactory, CancellationToken cancellationToken)
@@ -32,31 +35,68 @@ public class ServerFinder(int maxDegreeOfParallelism = 10)
             return connector;
         });
 
-        await VhUtil.ParallelForEachAsync(connectors, async connector =>
-        {
-            _hostEndPointStatus[connector.EndPointInfo.TcpEndPoint] = await CheckServerStatus(connector);
-        }, maxDegreeOfParallelism, cancellationToken);
+        _hostEndPointStatus = await VerifyServersStatus(connectors, cancellationToken);
 
         throw new NotImplementedException();
     }
 
-    private static async Task<bool> CheckServerStatus(ConnectorService connectorService)
+    private async Task<IDictionary<IPEndPoint, bool>> VerifyServersStatus(IEnumerable<ConnectorService> connectors,
+        CancellationToken cancellationToken)
+    {
+        var hostEndPointStatus = new ConcurrentDictionary<IPEndPoint, bool>();
+
+        try
+        {
+            // check all servers
+            using var cancellationTokenSource = new CancellationTokenSource();
+            using var linkedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationTokenSource.Token, cancellationToken);
+            await VhUtil.ParallelForEachAsync(connectors, async connector =>
+            {
+                var serverStatus = await VerifyServerStatus(connector, linkedCancellationTokenSource.Token);
+                hostEndPointStatus[connector.EndPointInfo.TcpEndPoint] = serverStatus;
+                if (serverStatus)
+                    linkedCancellationTokenSource.Cancel(); // no need to continue, we find a server
+
+            }, maxDegreeOfParallelism, linkedCancellationTokenSource.Token);
+
+        }
+        catch (OperationCanceledException)
+        {
+        }
+
+        return hostEndPointStatus;
+    }
+
+
+    private static async Task<bool> VerifyServerStatus(ConnectorService connector, CancellationToken cancellationToken)
     {
         try
         {
-            var requestResult = await connectorService.SendRequest<ServerStatusResponse>(
-                new ServerStatusRequest
-                {
-                    RequestId = Guid.NewGuid().ToString(),
-                    Message = "Hi, How are you?"
-                }, CancellationToken.None);
+            var requestResult = await connector.SendRequest<ServerStatusResponse>(
+            new ServerStatusRequest
+            {
+                RequestId = Guid.NewGuid().ToString(),
+                Message = "Hi, How are you?"
+            }, cancellationToken);
 
-            return requestResult.Response.ErrorCode == SessionErrorCode.Ok;
+            // this should be already handled by the connector and never happen
+            if (requestResult.Response.ErrorCode != SessionErrorCode.Ok)
+                throw new SessionException(requestResult.Response.ErrorCode);
+
+            return true;
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            throw;
+        }
+        catch (SessionException ex)
+        {
+            throw;
         }
         catch (Exception ex)
         {
             VhLogger.Instance.LogError(ex, "Could not get server status. EndPoint: {EndPoint}",
-                VhLogger.Format(connectorService.EndPointInfo.TcpEndPoint));
+                VhLogger.Format(connector.EndPointInfo.TcpEndPoint));
 
             return false;
         }

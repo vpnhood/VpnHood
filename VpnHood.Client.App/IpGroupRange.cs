49b16e1875392dafff6455266642549bcba6990c
Trudy <trudy@vpnhood.com>
2024-06-02 02:52:17 -0700

    IpGroupManager

---
+++ VpnHood.Client.App/IpGroupRange.cs
@@ -0,0 +1,10 @@
+﻿using VpnHood.Common.Net;
+
+namespace VpnHood.Client.App;
+
+public class IpGroupRange
+{
+    public required string IpGroupId { get; init; }
+    public required IpRangeOrderedList IpRanges { get; init; }
+
+}

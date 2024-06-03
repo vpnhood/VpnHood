9299ead9b6960685c0ac83a86c8d870ef241dde4
Trudy <trudy@vpnhood.com>
2024-06-02 21:42:57 -0700

    IpRangeOrderedList

--- VpnHood.Common/Net/IpNetworkExtension.cs
+++ VpnHood.Common/Net/IpNetworkExtension.cs
@@ -6,11 +6,7 @@ public static class IpNetworkExtension
 {
     public static IOrderedEnumerable<IpNetwork> Sort(this IEnumerable<IpNetwork> ipNetworks)
     {
-        var ipNetworkList = new List<IpNetwork>();
-        foreach (var ipRange in ipNetworkList.ToIpRangesNew())
-            ipNetworkList.AddRange(ipRange.ToIpNetworks());
-
-        return ipNetworkList.OrderBy(x => x.FirstIpAddress, new IPAddressComparer());
+        return ipNetworks.ToIpRanges().ToIpNetworks();
     }
 
     public static IpRangeOrderedList ToIpRanges(this IEnumerable<IpNetwork> ipNetworks)
@@ -23,50 +19,8 @@ public static class IpNetworkExtension
         return ipNetworks.Select(x => x.ToIpRange()).ToOrderedList();
     }
 
-
-    public static IOrderedEnumerable<IpNetwork> ToIpNetworks(this IEnumerable<IpRange> ipRanges)
-    {
-        return IpNetwork.FromIpRange(ipRanges);
-    }
-
-    public static IEnumerable<IpNetwork> ToIpNetworks(this IpRange ipRange)
-    {
-        return IpNetwork.FromIpRange(ipRange.FirstIpAddress, ipRange.LastIpAddress);
-    }
-
     public static bool IsAll(this IOrderedEnumerable<IpNetwork> ipNetworks)
     {
         return IpNetwork.IsAll(ipNetworks);
-    }
-
-    public static IOrderedEnumerable<IpRange> Exclude(this IEnumerable<IpRange> ipRanges, IEnumerable<IpRange> excludeIpRanges)
-    {
-        return IpRange.Exclude(ipRanges, excludeIpRanges);
-    }
-
-    public static IOrderedEnumerable<IpRange> Invert(this IEnumerable<IpRange> ipRanges,
-        bool includeIPv4 = true, bool includeIPv6 = true)
-    {
-        return IpRange.Invert(ipRanges, includeIPv4, includeIPv6);
-    }
-
-    public static bool IsInSortedRanges(this IpRange[] sortedIpRanges, IPAddress ipAddress)
-    {
-        return IpRange.IsInSortedRanges(sortedIpRanges, ipAddress);
-    }
-
-    public static IpRange? FindInSortedRanges(this IpRange[] sortedIpRanges, IPAddress ipAddress)
-    {
-        return IpRange.FindInSortedRanges(sortedIpRanges, ipAddress);
-    }
-
-    public static IOrderedEnumerable<IpRange> Intersect(this IEnumerable<IpRange> ipRanges1, IEnumerable<IpRange> ipRanges2)
-    {
-        return IpRange.Intersect(ipRanges1, ipRanges2);
-    }
-
-    public static IOrderedEnumerable<IpRange> Union(this IEnumerable<IpRange> ipRanges1, IEnumerable<IpRange> ipRanges2)
-    {
-        return IpRange.Union(ipRanges1, ipRanges2);
     }
 }
--- VpnHood.Common/Net/IpRangeExtension.cs
+++ VpnHood.Common/Net/IpRangeExtension.cs
@@ -6,4 +6,23 @@ public static class IpRangeExtension
     {
         return new IpRangeOrderedList(ipRanges);
     }
+
+    public static IOrderedEnumerable<IpNetwork> ToIpNetworks(this IEnumerable<IpRange> ipRanges)
+    {
+        var ipNetworkList = new List<IpNetwork>();
+        foreach (var ipRange in ipRanges)
+            ipNetworkList.AddRange(ipRange.ToIpNetworks());
+
+        return ipNetworkList.OrderBy(x => x.FirstIpAddress, new IPAddressComparer());
+    }
+
+    public static IEnumerable<IpNetwork> ToIpNetworks(this IpRange ipRange)
+    {
+        return IpNetwork.FromRange(ipRange.FirstIpAddress, ipRange.LastIpAddress);
+    }
+
+    public static IEnumerable<IpRange> Intersect(this IEnumerable<IpRange> first, IEnumerable<IpRange> second)
+    {
+        throw new NotSupportedException($"Use {nameof(IpRangeOrderedList)}.Intersect.");
+    }
 }

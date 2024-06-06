5aef573b39a11e0b6ad2c62eb6fca74375be3a59
Peter <peter@vpnhood.com>
2024-06-07 01:59:10 +0330

    Add UnityAds project
    Former-commit-id: c1fdf8ed0891813ac714b78b92247065c079cdcd

---
+++ VpnHood.Client.App.Android.Unity.Ads/Additions/LoadModuleDecorator.cs
@@ -0,0 +1,20 @@
+﻿using Com.Unity3d.Services.Core.Webview.Bridge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Unity3d.Services.Ads.Operation.Load
+{
+    public partial class LoadModuleDecorator
+    {
+        private void Set(ILoadOperation sharedObject)
+        {
+
+        }
+
+        private void ExecuteAdOperation(IWebViewBridgeInvoker p0, global::Com.Unity3d.Services.Ads.Operation.Load.LoadOperationState p1)
+        {
+
+        }
+    }
+}

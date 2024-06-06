340ad063facd0a6310f99f26d175bb6f4827be5d
Peter <peter@vpnhood.com>
2024-06-07 01:59:10 +0330

    Add UnityAds project

---
+++ VpnHood.Client.App.Android.Unity.Ads/Additions/IPurchasesResponseListenerInvoker.cs
@@ -0,0 +1,16 @@
+﻿using Com.Unity3d.Services.Store.Gpbl.Bridges;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Unity3d.Services.Store.Listeners
+{
+    internal partial class IPurchasesResponseListenerInvoker
+    {
+        public void OnBillingResponse(BillingResultBridge p0, IList p1)
+        {
+
+        }
+    }
+}
---
+++ VpnHood.Client.App.Android.Unity.Ads/Additions/PurchasesResponseListener.cs
@@ -0,0 +1,16 @@
+﻿using Com.Unity3d.Services.Store.Gpbl.Bridges;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Unity3d.Services.Store.Listeners
+{
+    public partial class PurchasesResponseListener
+    {
+        public void OnBillingResponse(BillingResultBridge billingResult, IList purchases)
+        {
+
+        }
+    }
+}

900a952e03038925569b0af420e7bf031a3a672f
Trudy <trudy@vpnhood.com>
2024-06-07 23:18:58 -0700

    Refactor AdTest

--- Tests/VpnHood.Test/AccessManagers/TestAccessManager.cs
+++ Tests/VpnHood.Test/AccessManagers/TestAccessManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using VpnHood.Common.Collections;
 using VpnHood.Common.Messaging;
 using VpnHood.Server.Access;
 using VpnHood.Server.Access.Configurations;
@@ -10,15 +11,27 @@ namespace VpnHood.Test.AccessManagers;
 public class TestAccessManager(string storagePath, FileAccessManagerOptions options)
     : FileAccessManager(storagePath, options)
 {
+    private readonly TimeoutDictionary<string, TimeoutItem> _adsData = new(TimeSpan.FromMinutes(10));
     private readonly object _lockeObject = new();
+
     public int SessionGetCounter { get; private set; }
     public DateTime? LastConfigureTime { get; private set; }
     public ServerInfo? LastServerInfo { get; private set; }
     public ServerStatus? LastServerStatus { get; private set; }
     public IPEndPoint? RedirectHostEndPoint { get; set; }
     public Dictionary<string, IPEndPoint?> ServerLocations { get; set; } = new();
+    public bool RejectAllAds { get; set; }
 
+    public void AddAdData(string adData)
+    {
+        if (!RejectAllAds)
+            _adsData.TryAdd(adData, new TimeoutItem());
+    }
 
+    protected override bool IsValidAd(string? adData)
+    {
+        return adData != null && _adsData.TryRemove(adData, out _);
+    }
     public override async Task<ServerCommand> Server_UpdateStatus(ServerStatus serverStatus)
     {
         var ret = await base.Server_UpdateStatus(serverStatus);

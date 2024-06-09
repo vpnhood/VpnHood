5a8fd23c4c796826de472871971079bd0c9beedd
Trudy <trudy@vpnhood.com>
2024-06-08 21:18:26 -0700

    move ad exceptions to common

--- VpnHood.Common/Exceptions/AdException.cs
+++ VpnHood.Common/Exceptions/AdException.cs
@@ -1,10 +1,9 @@
-﻿namespace VpnHood.Client.Exceptions;
+﻿namespace VpnHood.Common.Exceptions;
 
 public class AdException : Exception
 {
     public AdException(string message, Exception innerException) : base(message, innerException)
     {
-
     }
 
     public AdException(string message) : base(message)

19258e2344721156b6a8e8090b95ce13c861e2e4
Trudy <trudy@vpnhood.com>
2024-06-09 21:47:56 -0700

    fix IsSupportedCountry

--- VpnHood.Server.Access/Messaging/SessionResponseEx.cs
+++ VpnHood.Server.Access/Messaging/SessionResponseEx.cs
@@ -11,7 +11,6 @@ public class SessionResponseEx : SessionResponse
     public SessionSuppressType SuppressedTo { get; set; }
     public ulong SessionId { get; set; }
     public byte[] SessionKey { get; set; } = [];
-    public bool IsAdRequired { get; set; } //todo: deprecated in version 504 or later
     public AdRequirement AdRequirement { get; set; } = AdRequirement.None;
     public string? ServerLocation { get; set; }
 }

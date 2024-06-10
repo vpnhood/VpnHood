62ed48f85754f2e63a0277568f00c2111c08d2be
Trudy <trudy@vpnhood.com>
2024-06-09 22:12:37 -0700

    support NullPacketCapture

--- Tests/VpnHood.Test/Device/TestNullPacketCapture.cs
+++ Tests/VpnHood.Test/Device/TestNullPacketCapture.cs
@@ -1,70 +1,7 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using PacketDotNet;
-using VpnHood.Client.Device;
-using VpnHood.Common.Net;
+﻿using VpnHood.Client.Device;
 
 namespace VpnHood.Test.Device;
 
-internal class TestNullPacketCapture : IPacketCapture
+internal class TestNullPacketCapture : NullPacketCapture
 {
-    public event EventHandler<PacketReceivedEventArgs>? PacketReceivedFromInbound;
-    public event EventHandler? Stopped;
-    public bool Started { get; set; }
-    public bool IsDnsServersSupported { get; set; }
-    public IPAddress[]? DnsServers { get; set; }
-    public bool CanExcludeApps { get; set; } = true;
-    public bool CanIncludeApps { get; set; } = true;
-    public string[]? ExcludeApps { get; set; }
-    public string[]? IncludeApps { get; set; }
-    public IpNetwork[]? IncludeNetworks { get; set; }
-    public bool IsMtuSupported { get; set; } = true;
-    public int Mtu { get; set; }
-    public bool IsAddIpV6AddressSupported { get; set; } = true;
-    public bool AddIpV6Address { get; set; } = true;
-    public bool CanProtectSocket { get; set; } = true;
-    public bool CanSendPacketToOutbound { get; set; } = false;
-    public void StartCapture()
-    {
-        Started = true;
-        _ = PacketReceivedFromInbound; //prevent not used warning
-    }
-
-    public void StopCapture()
-    {
-        Started = false;
-        Stopped?.Invoke(this, EventArgs.Empty);
-    }
-
-    public void ProtectSocket(Socket socket)
-    {
-        // nothing
-
-    }
-
-    public void SendPacketToInbound(IPPacket ipPacket)
-    {
-        // nothing
-    }
-
-    public void SendPacketToInbound(IList<IPPacket> packets)
-    {
-        // nothing
-    }
-
-    public void SendPacketToOutbound(IPPacket ipPacket)
-    {
-        // nothing
-    }
-
-    public void SendPacketToOutbound(IList<IPPacket> ipPackets)
-    {
-        // nothing
-    }
-
-    public void Dispose()
-    {
-        StopCapture();
-    }
-
 }

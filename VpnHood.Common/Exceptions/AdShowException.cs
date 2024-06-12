a477c9b29e81a653c1a658942b60e3eda66222b3
Trudy <trudy@vpnhood.com>
2024-06-11 20:18:55 -0700

    use AdShowException

---
+++ VpnHood.Common/Exceptions/AdShowException.cs
@@ -0,0 +1,12 @@
+﻿namespace VpnHood.Common.Exceptions;
+
+public class AdShowException : AdException
+{
+    public AdShowException(string message) : base(message)
+    {
+    }
+
+    public AdShowException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}

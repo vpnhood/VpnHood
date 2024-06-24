ed33c7830e36bcd5030a400708cd10fb2daeda05
Trudy <trudy@vpnhood.com>
2024-06-23 22:02:43 -0700

    fix IpFilters tests

--- VpnHood.Client.App/Settings/UserSettings.cs
+++ VpnHood.Client.App/Settings/UserSettings.cs
@@ -21,18 +21,18 @@ public class UserSettings
     public bool UseUdpChannel { get; set; } = DefaultClientOptions.UseUdpChannel;
     public bool DropUdpPackets { get; set; } = DefaultClientOptions.DropUdpPackets;
     public bool IncludeLocalNetwork { get; set; } = DefaultClientOptions.IncludeLocalNetwork;
-    
+
     [JsonConverter(typeof(NullToEmptyArrayConverter<IpRange>))] //todo: remove nullable after migration 4.5.533
-    public IpRange[] IncludeIpRanges { get; set; } = IpNetwork.All.ToIpRanges().ToArray(); 
-    
+    public IpRange[] IncludeIpRanges { get; set; } = IpNetwork.All.ToIpRanges().ToArray();
+
     [JsonConverter(typeof(NullToEmptyArrayConverter<IpRange>))] //todo: remove nullable after migration 4.5.533
-    public IpRange[] ExcludeIpRanges { get; set; } = IpNetwork.None.ToIpRanges().ToArray(); 
-    
+    public IpRange[] ExcludeIpRanges { get; set; } = IpNetwork.None.ToIpRanges().ToArray();
+
     [JsonConverter(typeof(NullToEmptyArrayConverter<IpRange>))] //todo: remove nullable after migration 4.5.533
-    public IpRange[] PacketCaptureIncludeIpRanges { get; set; } = IpNetwork.All.ToIpRanges().ToArray(); 
-    
+    public IpRange[] PacketCaptureIncludeIpRanges { get; set; } = IpNetwork.All.ToIpRanges().ToArray();
+
     [JsonConverter(typeof(NullToEmptyArrayConverter<IpRange>))] //todo: remove nullable after migration 4.5.533
-    public IpRange[] PacketCaptureExcludeIpRanges { get; set; } = IpNetwork.None.ToIpRanges().ToArray(); 
+    public IpRange[] PacketCaptureExcludeIpRanges { get; set; } = IpNetwork.None.ToIpRanges().ToArray();
     public bool AllowAnonymousTracker { get; set; } = DefaultClientOptions.AllowAnonymousTracker;
     public IPAddress[]? DnsServers { get; set; }
     public string? DebugData1 { get; set; }

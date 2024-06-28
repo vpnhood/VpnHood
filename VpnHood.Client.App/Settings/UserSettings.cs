76738a23bdfaa47b366f99d8edb7414b8518e508
Trudy <trudy@vpnhood.com>
2024-06-28 16:09:19 -0700

    * Fix: Exclude the ad tracker from tunnel * Fix: Exclude the ad tracker from tunnel * Fix: Exclude the ad tracker from tunnel

--- VpnHood.Client.App/Settings/UserSettings.cs
+++ VpnHood.Client.App/Settings/UserSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Text.Json.Serialization;
+using VpnHood.Client.DomainFiltering;
 using VpnHood.Common.Converters;
 using VpnHood.Common.Net;
 
@@ -35,8 +36,7 @@ public class UserSettings
     public IpRange[] PacketCaptureExcludeIpRanges { get; set; } = IpNetwork.None.ToIpRanges().ToArray();
     public bool AllowAnonymousTracker { get; set; } = DefaultClientOptions.AllowAnonymousTracker;
     public IPAddress[]? DnsServers { get; set; }
-    public string[] IncludeDomains { get; set; } = [];
-    public string[] ExcludeDomains { get; set; } = [];
+    public DomainFilter DomainFilter { get; set; } = new();
     public string? DebugData1 { get; set; }
     public string? DebugData2 { get; set; }
 }
---
+++ VpnHood.Common/Logging/VhConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace VpnHood.Common.Logging;
+
+public class VhConsoleLogger(bool includeScopes = true) : TextLogger(includeScopes)
+{
+    public override void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        var text = FormatLog(logLevel, eventId, state, exception, formatter);
+        Console.WriteLine(text);
+    }
+}

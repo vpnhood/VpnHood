413abf9753077896d11d8d910a0634cf440d3ddf
Trudy <trudy@vpnhood.com>
2024-07-07 02:55:57 -0700

    uodate trackers

--- VpnHood.Common/Ga4Tracking/IGa4MeasurementTracker.cs
+++ VpnHood.Common/Ga4Tracking/IGa4MeasurementTracker.cs
@@ -4,5 +4,5 @@ namespace Ga4.Ga4Tracking;
 public interface IGa4MeasurementTracker
 {
     public bool IsEnabled { get; set; }
-    public Task Track(Ga4TagEvent ga4Event, Dictionary<string, object>? userProperties = null);
+    public Task Track(IEnumerable<Ga4MeasurementEvent> ga4Events, Dictionary<string, object>? userProperties = null);
 }

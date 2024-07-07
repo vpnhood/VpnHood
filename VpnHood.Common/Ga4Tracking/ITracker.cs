f32feba8461144c1333c83550b8b80f2cd0c1f32
Trudy <trudy@vpnhood.com>
2024-07-07 02:55:57 -0700

    uodate trackers

---
+++ VpnHood.Common/Ga4Tracking/ITracker.cs
@@ -0,0 +1,8 @@
+﻿namespace Ga4.Ga4Tracking
+{
+    public interface ITracker
+    {
+        public bool IsEnabled { get; set; }
+        public Task Track(TrackEvent trackEvent, Dictionary<string, object>? userProperties = null);
+    }
+}

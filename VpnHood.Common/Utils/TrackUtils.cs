89ce0e384bfcb62489e6fc0780f484f661198fcd
Trudy <trudy@vpnhood.com>
2024-07-07 21:44:48 -0700

    remove track error from track library

---
+++ VpnHood.Common/Utils/TrackUtils.cs
@@ -0,0 +1,21 @@
+﻿using Ga4.Trackers;
+
+namespace VpnHood.Common.Utils;
+
+public static class TrackUtils
+{
+    public static Task TrackError(ITracker tracker, string action, Exception ex)
+    {
+        var trackEvent = new TrackEvent
+        {
+            EventName = "exception",
+            Parameters = new Dictionary<string, object>
+            {
+                { "page_location", "ex/" + action },
+                { "page_title", ex.Message }
+            }
+        };
+
+        return tracker.Track([trackEvent]);
+    }
+}

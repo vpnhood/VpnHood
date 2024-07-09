37da88aca853b38218ffc80f95a2627300d3248d
Trudy <trudy@vpnhood.com>
2024-07-09 11:21:23 -0700

    add IpV6 support to HelloRequest

--- VpnHood.Server.Access/Messaging/SessionRequestEx.cs
+++ VpnHood.Server.Access/Messaging/SessionRequestEx.cs
@@ -20,4 +20,5 @@ public class SessionRequestEx
     public required string? ExtraData { get; set; }
     public string? ServerLocation { get; set; }
     public bool AllowRedirect { get; set; } = true;
+    public bool? IsIpV6Supported { get; set; }
 }

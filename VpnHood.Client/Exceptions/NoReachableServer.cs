dd215b026a49fe6978dfc95536be9d7271b9a1df
Trudy <trudy@vpnhood.com>
2024-07-11 17:15:50 -0700

    move trackers to TrackerBuilder

--- VpnHood.Client/Exceptions/NoReachableServer.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace VpnHood.Client.Exceptions;
-
-public class NoReachableServer()
-    : Exception("There is no reachable server at this moment. Please try again later.");
---
+++ VpnHood.Client/Exceptions/UnreachableServer.cs
@@ -0,0 +1,9 @@
+﻿using VpnHood.Common;
+
+namespace VpnHood.Client.Exceptions;
+
+public class UnreachableServer(string? serverLocation = null)
+    : Exception(
+        ServerLocationInfo.IsAuto(serverLocation)
+            ? "There is no reachable server at this moment. Please try again later."
+            : $"There is no reachable server at this moment. Please try again later. Location: {serverLocation}");

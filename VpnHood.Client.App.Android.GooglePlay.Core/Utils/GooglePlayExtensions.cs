2df198263d23c5c5c867be405a935bf608cfa61d
Trudy <trudy@vpnhood.com>
2024-07-13 23:33:06 -0700

    imrove GooglePlayInAppReviewService
    Former-commit-id: 56c0b1c1b34fae2001baec3486ce25bfb621ebb9

---
+++ VpnHood.Client.App.Android.GooglePlay.Core/Utils/GooglePlayExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace VpnHood.Client.App.Droid.GooglePlay.Utils;
+
+public static class GooglePlayExtensions
+{
+    public static Task<T?> AsTask<T>(this Xamarin.Google.Android.Play.Core.Tasks.Task googlePlayTask) where T : class
+    {
+        return GooglePlayTaskCompleteListener<T>.Create(googlePlayTask);
+    }
+
+    public static Task AsTask(this Xamarin.Google.Android.Play.Core.Tasks.Task googlePlayTask)
+    {
+        return GooglePlayTaskCompleteListener<object>.Create(googlePlayTask);
+    }
+
+}

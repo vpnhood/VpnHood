543099b71a5a52a1694f8040bc3866ea5ed821b9
Trudy <trudy@vpnhood.com>
2024-07-25 03:38:03 -0700

    NetConfigurationProvider for IpAddress
    Former-commit-id: 5f6b217d2a23f72fbe9be5d364d87740f2eb8a76

--- VpnHood.Common/Net/IPAddressExtensions.cs
+++ VpnHood.Common/Net/IPAddressExtensions.cs
@@ -6,13 +6,35 @@ namespace VpnHood.Common.Net;
 // ReSharper disable once InconsistentNaming
 public static class IPAddressExtensions
 {
+    public static bool IsV4(this IPAddress ipAddress)
+    {
+        return ipAddress.AddressFamily == AddressFamily.InterNetwork;
+    }
+
     public static bool IsV6(this IPAddress ipAddress)
     {
         return ipAddress.AddressFamily == AddressFamily.InterNetworkV6;
     }
 
-    public static bool IsV4(this IPAddress ipAddress)
+    public static bool IsV4(this IPEndPoint ipEndPoint)
     {
-        return ipAddress.AddressFamily == AddressFamily.InterNetwork;
+        return ipEndPoint.AddressFamily == AddressFamily.InterNetwork;
     }
+
+    public static bool IsV6(this IPEndPoint ipEndPoint)
+    {
+        return ipEndPoint.AddressFamily == AddressFamily.InterNetworkV6;
+    }
+
+    public static bool IsV4(this AddressFamily addressFamily)
+    {
+        return addressFamily == AddressFamily.InterNetwork;
+    }
+
+
+    public static bool IsV6(this AddressFamily addressFamily)
+    {
+        return addressFamily == AddressFamily.InterNetworkV6;
+    }
+
 }

9c556625eef19d44411be481516e96e6d65ed961
Trudy <trudy@vpnhood.com>
2024-07-26 22:46:12 -0700

    update readme

--- VpnHood.Server.Access/Configurations/ServerConfig.cs
+++ VpnHood.Server.Access/Configurations/ServerConfig.cs
@@ -30,7 +30,7 @@ public class ServerConfig
     public string ConfigCode { get; set; } = string.Empty;
     public int? MinCompletionPortThreads { get; set; }
     public int? MaxCompletionPortThreads { get; set; }
-    public string? AddListenerIpToNetwork { get; set; }
+    public string? AddListenerIpsToNetwork { get; set; }
     public DnsChallenge? DnsChallenge { get; set; }
     public CertificateData[] Certificates { get; set; } = [];
 
@@ -62,7 +62,7 @@ public class ServerConfig
         if (obj.ServerSecret != null) ServerSecret = obj.ServerSecret;
         if (obj.MinCompletionPortThreads != null) MinCompletionPortThreads = obj.MinCompletionPortThreads;
         if (obj.MaxCompletionPortThreads != null) MaxCompletionPortThreads = obj.MaxCompletionPortThreads;
-        if (obj.AddListenerIpToNetwork != null) AddListenerIpToNetwork = obj.AddListenerIpToNetwork;
+        if (obj.AddListenerIpsToNetwork != null) AddListenerIpsToNetwork = obj.AddListenerIpsToNetwork;
         if (obj.DnsChallenge != null) DnsChallenge = obj.DnsChallenge;
     }
 

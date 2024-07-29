18021ea5bebc12d65be788cf6c7aa669d72246df
Trudy <trudy@vpnhood.com>
2024-07-28 21:55:24 -0700

    move CompletableFutureTask AsTask to Android Device

---
+++ VpnHood.Client.Device.Android/Utils/CompletableFutureExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Java.Util.Concurrent;
+
+namespace VpnHood.Client.Device.Droid.Utils;
+
+internal static class CompletableFutureExtensions
+{
+    public static Task<Java.Lang.Object?> AsTask(this CompletableFuture completableFuture)
+    {
+        return new CompletableFutureTask(completableFuture).Task;
+    }
+}

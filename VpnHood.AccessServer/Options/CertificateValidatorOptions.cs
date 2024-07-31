fa77943a86e48a223970228aa422bfbe9e5466a6
trudy <trudy@vpnhood.com>
2024-07-31 01:10:08 -0700

    recover CertificateValidatorOptions

--- VpnHood.AccessServer/Options/CertificateValidatorOptions.cs
+++ VpnHood.AccessServer/Options/CertificateValidatorOptions.cs
@@ -2,8 +2,8 @@
 
 public class CertificateValidatorOptions
 {
-    public TimeSpan Due { get; set; } = TimeSpan.FromMinutes(1);
+    public TimeSpan Due { get; set; } = TimeSpan.FromMinutes(10);
     public TimeSpan Interval { get; set; } = TimeSpan.FromHours(24);
-    public TimeSpan ExpirationThreshold { get; set; } = TimeSpan.FromDays(60);
+    public TimeSpan ExpirationThreshold { get; set; } = TimeSpan.FromDays(30);
     public int MaxRetry { get; set; } = 5;
 }

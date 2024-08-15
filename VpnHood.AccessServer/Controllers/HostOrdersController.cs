c0e514d919703e65da35928805fd8b12a4ca96c7
trudy <trudy@vpnhood.com>
2024-08-14 22:21:58 -0700

    nuget

--- VpnHood.AccessServer/Controllers/HostOrdersController.cs
+++ VpnHood.AccessServer/Controllers/HostOrdersController.cs
@@ -14,7 +14,7 @@ public class HostOrdersController(HostOrdersService hostOrdersService)
 {
     [HttpGet("ips")]
     [AuthorizeProjectPermission(Permissions.ProjectRead)]
-    public Task<HostIp[]> ListIps(Guid projectId, string search, int recordIndex = 0, int recordCount = 200)
+    public Task<HostIp[]> ListIps(Guid projectId, string? search = null, int recordIndex = 0, int recordCount = 200)
     {
         return hostOrdersService.ListIps(projectId, search: search, recordIndex: recordIndex, recordCount: recordIndex);
     }

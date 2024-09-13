d0df99bf4f7286c790e5f504b35d22b89c3a3a8e
Trudy <trudy@vpnhood.com>
2024-09-13 01:11:29 -0700

    add NetTester.

---
+++ Tests/VpnHood.NetTester/Speedometer.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+using VpnHood.Common.Jobs;
+using VpnHood.Common.Utils;
+
+namespace VpnHood.NetTester;
+
+public class Speedometer : IJob, IDisposable
+{
+    private readonly string _name;
+    private readonly bool _packetCounter;
+    private readonly Stopwatch _stopwatch = new();
+    private readonly object _lockObject = new();
+
+    private long _succeededCount;
+    private long _failedCount;
+    private long _transferSize;
+    private long _lastTransferSize;
+    private long _lastSucceededCount;
+
+    public JobSection JobSection { get; }
+
+    public Speedometer(string name,
+        TimeSpan? interval = null,
+        bool packetCounter = false)
+    {
+        _name = name;
+        _packetCounter = packetCounter;
+        _stopwatch.Start();
+        JobSection = new JobSection(interval ?? TimeSpan.FromSeconds(2));
+        JobRunner.Default.Add(this);
+    }
+
+    public void AddSucceeded(int bytes)
+    {
+        lock (_lockObject) {
+            _transferSize += bytes;
+            _succeededCount++;
+        }
+    }
+
+    public void AddRead(int bytes)
+    {
+        lock (_lockObject) {
+            _transferSize += bytes;
+            _succeededCount++;
+        }
+    }
+
+    public void AddWrite(int bytes)
+    {
+        lock (_lockObject) {
+            _transferSize += bytes;
+            _succeededCount++;
+        }
+    }
+
+
+    public void AddFailed()
+    {
+        lock (_lockObject)
+            _failedCount++;
+    }
+
+    public void Report()
+    {
+        lock (_lockObject) {
+            if (_stopwatch.ElapsedMilliseconds == 0) return;
+
+            var curTransferSize = _transferSize - _lastTransferSize;
+            var curSucceededCount = _succeededCount - _lastSucceededCount;
+            if (_packetCounter) {
+                Console.WriteLine(_name + ": " +
+                                  $"Speed: {VhUtil.FormatBits(1000 * curTransferSize / _stopwatch.ElapsedMilliseconds)}, " +
+                                  $"Success: {curSucceededCount}, TotalSucceeded: {_succeededCount}, TotalFailed: {_failedCount}, TotalBytes: {VhUtil.FormatBytes(_transferSize)}");
+            }
+            else {
+                Console.WriteLine(_name + ": " +
+                                  $"Speed: {VhUtil.FormatBits(1000 * curTransferSize / _stopwatch.ElapsedMilliseconds)}, " +
+                                  $"TotalBytes: {VhUtil.FormatBytes(_transferSize)}");
+            }
+
+            _lastTransferSize = _transferSize;
+            _lastSucceededCount = _succeededCount;
+            _stopwatch.Restart();
+        }
+    }
+
+    public Task RunJob()
+    {
+        Report();
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        JobRunner.Default.Remove(this);
+    }
+}
---
+++ Tests/VpnHood.NetTester/TcpTesters/TcpTesterClient.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Logging;
+using VpnHood.Common.Utils;
+
+namespace VpnHood.NetTester.TcpTesters;
+
+public class TcpTesterClient
+{
+    public static async Task StartFull(IPEndPoint serverEp, long writeBytes, long readBytes, int concurrentCount,
+        ILogger? logger, CancellationToken cancellationToken)
+    {
+        // start simple tester
+        using (var speedometer = new Speedometer("SingleTcp")) {
+            await Start(serverEp, writeBytes: writeBytes, readBytes: readBytes,
+                speedometer: speedometer, logger: logger, cancellationToken: cancellationToken);
+        }
+
+        // start concurrent testers
+        using (var speedometer = new Speedometer("ConcurrentTcp")) {
+            await StartConcurrent(serverEp, writeBytes: writeBytes / concurrentCount,
+                readBytes: readBytes / concurrentCount,
+                concurrentCount: concurrentCount,
+                speedometer: speedometer, logger: logger, cancellationToken: cancellationToken);
+        }
+    }
+
+    public static async Task StartConcurrent(IPEndPoint serverEp, long writeBytes, long readBytes, int concurrentCount,
+        Speedometer speedometer, ILogger? logger, CancellationToken cancellationToken)
+    {
+        logger?.LogInformation("Concurrent TCP tester has been started. Write: {Write}, Read:{Read}",
+            VhUtil.FormatBytes(writeBytes), VhUtil.FormatBytes(readBytes));
+
+        var tasks = new Task[concurrentCount];
+        for (var i = 0; i < concurrentCount; i++)
+            tasks[i] = Start(serverEp, writeBytes, readBytes, speedometer: speedometer, logger: null, cancellationToken: cancellationToken);
+
+        await Task.WhenAll(tasks);
+
+        logger?.LogInformation("Concurrent TCP tester has been completed.");
+    }
+
+    public static async Task Start(IPEndPoint serverEp, long writeBytes, long readBytes,
+        Speedometer speedometer, ILogger? logger, CancellationToken cancellationToken)
+    {
+        logger?.LogInformation("Single TCP tester has been started. Write: {Write}, Read:{Read}",
+            VhUtil.FormatBytes(writeBytes), VhUtil.FormatBytes(readBytes));
+
+        // connect to server
+        var client = new TcpClient();
+        await client.ConnectAsync(serverEp.Address, serverEp.Port, cancellationToken);
+
+        // write writeBytes to server
+        await using var stream = client.GetStream();
+        var buffer = BitConverter.GetBytes(writeBytes);
+        await stream.WriteAsync(buffer, 0, buffer.Length, cancellationToken);
+        await stream.FlushAsync(cancellationToken);
+
+        await TcpTesterUtil.WriteRandomData(stream, writeBytes, speedometer: speedometer, cancellationToken: cancellationToken);
+        await TcpTesterUtil.ReadData(stream, readBytes, speedometer: speedometer, cancellationToken: cancellationToken);
+
+        logger?.LogInformation("Single TCP tester has been completed.");
+    }
+
+}

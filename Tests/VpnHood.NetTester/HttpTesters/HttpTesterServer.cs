b1e79269d92628f7d08bcf944c7f8d2a2880056c
Trudy <trudy@vpnhood.com>
2024-09-14 11:57:20 -0700

    Add HTTP testing support and various enhancements

---
+++ Tests/VpnHood.NetTester/HttpTesters/HttpTesterServer.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text;
+using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using VpnHood.NetTester.Streams;
+
+namespace VpnHood.NetTester.HttpTesters;
+
+internal class HttpTesterServer : IDisposable
+{
+    private readonly WebServer _webServer;
+
+    public HttpTesterServer(IPEndPoint ipEndPoint, CancellationToken cancellationToken)
+    {
+        // create web server
+        var webServerOptions = new WebServerOptions();
+        webServerOptions.AddUrlPrefix($"http://{ipEndPoint}");
+
+        _webServer = new WebServer(webServerOptions)
+            .WithWebApi("/", c => c.WithController(() => new TestController()));
+
+
+        _webServer.Start(cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        _webServer.Dispose();
+    }
+
+    internal class TestController : WebApiController
+    {
+        [Route(HttpVerbs.Post, "/upload")]
+        public async Task Upload()
+        {
+            await using var requestStream = HttpContext.OpenRequestStream();
+            await using var streamDiscarder = new StreamDiscarder(null);
+            await requestStream.CopyToAsync(streamDiscarder);
+            await HttpContext.SendStringAsync("File uploaded successfully", "text/plain", Encoding.UTF8);
+        }
+
+        [Route(HttpVerbs.Get, "/download")]
+        public async Task Download([QueryField] int length)
+        {
+            // read length from query string parameter
+            await using var responseStream = HttpContext.OpenResponseStream();
+            await using var randomReader = new StreamRandomReader(length, null);
+            await randomReader.CopyToAsync(responseStream);
+        }
+    }
+}
---
+++ Tests/VpnHood.NetTester/Streams/StreamDiscarder.cs
@@ -0,0 +1,29 @@
+﻿namespace VpnHood.NetTester.Streams;
+
+public class StreamDiscarder(Speedometer? speedometer) : Stream
+{
+    // Override the required members of Stream
+
+    public override bool CanRead => false;
+    public override bool CanSeek => false;
+    public override bool CanWrite => true; // We want to allow writing, but discard the data
+
+    public override long Length => throw new NotSupportedException();
+    public override long Position {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
+
+    // The main functionality: discard data by doing nothing
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        // Do nothing, effectively discarding the data
+        speedometer?.AddWrite(count);
+    }
+
+    public override void Flush() { }
+
+    public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+    public override void SetLength(long value) => throw new NotSupportedException();
+}

0d68dfe4d30a0014ef73775306d08da306d4e223
Trudy <trudy@vpnhood.com>
2024-09-16 02:13:46 -0700

    Refactor length to size and update domain validation

--- Tests/VpnHood.NetTester/Clients/ClientOptions.cs
+++ Tests/VpnHood.NetTester/Clients/ClientOptions.cs
@@ -7,8 +7,8 @@ namespace VpnHood.NetTester.Clients;
 
 public class ClientOptions(string[] args)
 {
-    public int UpLength { get; } = ArgumentUtils.Get(args, "/up", 60) * 1000000; // 6MB
-    public int DownLength { get; } = ArgumentUtils.Get(args, "/down", 60) * 1000000; // 60MB
+    public int UpSize { get; } = ArgumentUtils.Get(args, "/up", 60) * 1000000; // 6MB
+    public int DownSize { get; } = ArgumentUtils.Get(args, "/down", 60) * 1000000; // 60MB
     public int TcpPort { get; } = ArgumentUtils.Get(args, "/tcp", 33700);
     public int HttpPort { get; } = ArgumentUtils.Get(args, "/http", 8080);
     public int HttpsPort { get; } = ArgumentUtils.Get(args, "/https", 443);
@@ -16,7 +16,7 @@ public class ClientOptions(string[] args)
     public int Multi { get; } = ArgumentUtils.Get(args, "/multi", 10);
     public Uri? Url { get; } = ArgumentUtils.Get<Uri?>(args, "/url", null);
     public string? Domain { get; } = ArgumentUtils.Get<string?>(args, "/domain", null);
-    public bool IsValidDomain { get;} = ArgumentUtils.Get(args, "/valid-domain", false);
+    public bool IsValidDomain { get;} = args.Contains("/valid-domain", StringComparer.OrdinalIgnoreCase);
     public bool IsDebug { get; } = args.Contains("/debug", StringComparer.OrdinalIgnoreCase);
 
     [JsonConverter(typeof(IPEndPointConverter))]

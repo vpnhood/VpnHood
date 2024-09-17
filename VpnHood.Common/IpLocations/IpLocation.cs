2cec3ec06f7858b7b0fd3aca547018f76f9b19c7
Trudy <trudy@vpnhood.com>
2024-09-17 14:58:22 -0700

    add more location provider

--- VpnHood.Common/IpLocations/IpLocation.cs
+++ VpnHood.Common/IpLocations/IpLocation.cs
@@ -12,8 +12,5 @@ public class IpLocation
     public required string CountryName { get; init; }
     public required string CountryCode { get; init; }
     public required string? RegionName { get; init; }
-    public required string? RegionCode { get; init; }
     public required string? CityName { get; init; }
-    public required string? CityCode { get; init; }
-    public required string? ContinentCode { get; init; }
 }

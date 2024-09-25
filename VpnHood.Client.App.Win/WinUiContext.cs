eb39617ba81f07bacbf391dff561077eacbe4765
Trudy <trudy@vpnhood.com>
2024-09-25 16:48:12 -0700

    add IsActive to UiContext

---
+++ VpnHood.Client.App.Win/WinUiContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+using VpnHood.Client.Device;
+
+namespace VpnHood.Client.App.Win;
+
+public class WinUiContext(Window window) : IUiContext
+{
+    public bool IsActive => window.IsActive;
+}

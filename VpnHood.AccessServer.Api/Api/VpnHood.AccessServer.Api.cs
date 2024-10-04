010292a6d4fcbcab967076aa4cef1aebd5660f82
trudy <trudy@vpnhood.com>
2024-10-03 21:55:20 -0700

    publish
    Former-commit-id: 8deedad19f9198f0938a7fb8a05301605c5c1e23

--- VpnHood.AccessServer.Api/Api/VpnHood.AccessServer.Api.cs
+++ VpnHood.AccessServer.Api/Api/VpnHood.AccessServer.Api.cs
@@ -10442,6 +10442,7 @@ namespace VpnHood.AccessServer.Api
 
     }
 
+
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.1.0.0 (NJsonSchema v11.0.2.0 (Newtonsoft.Json v13.0.0.0))")]
     public partial class Location
     {

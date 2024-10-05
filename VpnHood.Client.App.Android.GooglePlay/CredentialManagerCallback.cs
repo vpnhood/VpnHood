258b4ca0f335d1f44699a191863623084e3bcd2f
Peter <peter@vpnhood.com>
2024-10-06 02:54:38 +0330

    Update google authentication.
    Former-commit-id: 8dababcee7043e83bc3b5e53ca346fe2a7ce2f71

---
+++ VpnHood.Client.App.Android.GooglePlay/CredentialManagerCallback.cs
@@ -0,0 +1,22 @@
+﻿using AndroidX.Credentials;
+using GetCredentialResponse = AndroidX.Credentials.GetCredentialResponse;
+namespace VpnHood.Client.App.Droid.GooglePlay;
+
+public class CredentialManagerCallback : Java.Lang.Object, ICredentialManagerCallback {
+    private readonly TaskCompletionSource<GetCredentialResponse> _taskCompletionSource = new();
+    public void OnError(Java.Lang.Object e)
+    {
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        _taskCompletionSource.TrySetException((Exception)(object)e);
+    }
+
+    public void OnResult(Java.Lang.Object? result)
+    {
+        _taskCompletionSource.TrySetResult((GetCredentialResponse)result!);
+    }
+
+    public Task<GetCredentialResponse> GetResultAsync()
+    {
+        return _taskCompletionSource.Task;
+    }
+}

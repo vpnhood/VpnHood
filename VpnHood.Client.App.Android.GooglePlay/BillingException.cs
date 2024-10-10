1241428d00e657ffaebb5e4824dee2793114a537
Peter <peter@vpnhood.com>
2024-10-10 22:33:02 +0330

    Add BillingException.cs
    Former-commit-id: 735e6609027b62adff01dd08baec2843a88f4976

---
+++ VpnHood.Client.App.Android.GooglePlay/BillingException.cs
@@ -0,0 +1,27 @@
+﻿using Android.BillingClient.Api;
+
+namespace VpnHood.Client.App.Droid.GooglePlay;
+
+public class BillingException : Exception
+{
+    public BillingException(string message) : base(message)
+    {
+    }
+
+    public BillingException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+    
+    public static Exception Create(BillingResult billingResult, PurchaseState? purchaseState = null)
+    {
+        if (billingResult.ResponseCode == BillingResponseCode.Ok)
+            throw new InvalidOperationException("Response code should not be OK.");
+
+        return new BillingException(billingResult.DebugMessage) {
+            Data = {
+                { "ResponseCode", billingResult.ResponseCode.ToString() },
+                { "PurchaseState", purchaseState.ToString() }
+            }
+        };
+    }
+}

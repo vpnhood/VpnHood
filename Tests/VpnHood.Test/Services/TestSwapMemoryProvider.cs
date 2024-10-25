9add128b811d1b2eeeb92e830a2a6de83dec5a31
Trudy <trudy@vpnhood.com>
2024-10-25 00:16:04 -0700

    Add TestSwapMemoryProvider for ISwapMemoryProvider testing

---
+++ Tests/VpnHood.Test/Services/TestSwapMemoryProvider.cs
@@ -0,0 +1,29 @@
+﻿using VpnHood.Server.Abstractions;
+
+namespace VpnHood.Test.Services;
+
+public class TestSwapMemoryProvider : ISwapMemoryProvider
+{
+    public long AppSize { get; set; } 
+    public long AppUsed { get; set; }
+    public long OtherSize { get; set; }
+    public long OtherUsed { get; set; }
+
+    public SwapMemoryInfo Info => new() {
+        AppSize = AppSize,
+        AppUsed = AppUsed,
+        TotalSize = OtherSize + AppSize,
+        TotalUsed = OtherUsed + AppUsed
+    };
+
+    public Task<SwapMemoryInfo> GetInfo()
+    {
+        return Task.FromResult(Info);
+    }
+
+    public Task SetAppSwapMemorySize(long size)
+    {
+        AppSize = size;
+        return Task.CompletedTask;
+    }
+}

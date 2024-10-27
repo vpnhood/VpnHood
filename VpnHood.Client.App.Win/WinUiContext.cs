18b2ba55b47511da55c6ad4ef99473645bb3fe60
Trudy <trudy@vpnhood.com>
2024-10-26 21:17:31 -0700

    Publish v4.6.578
    Former-commit-id: af50badc4c9a1dda74e8d527830110697063df10

--- VpnHood.Client.App.Win/WinUiContext.cs
+++ VpnHood.Client.App.Win/WinUiContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Principal;
-using System.Windows;
+﻿using System.Windows;
 using VpnHood.Client.Device;
 
 namespace VpnHood.Client.App.Win;
@@ -7,9 +6,4 @@ namespace VpnHood.Client.App.Win;
 public class WinUiContext(Window window) : IUiContext
 {
     public bool IsActive => window.IsActive;
-}
-
-class UserIdProvider
-{
-    public string DeviceId => WindowsIdentity.GetCurrent().User?.Value ?? throw new Exception("Current user does not exist.");
 }

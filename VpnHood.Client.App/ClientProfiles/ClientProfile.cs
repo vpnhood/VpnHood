730563f1a73a1e4e3dd49cc71ee818a0631c69b1
Trudy <trudy@vpnhood.com>
2024-10-27 17:45:00 -0700

    add CustomData property to ClientProfile
    Former-commit-id: 1efd29885e165561d538ad96ebf6ec55a49f7c7d

--- VpnHood.Client.App/ClientProfiles/ClientProfile.cs
+++ VpnHood.Client.App/ClientProfiles/ClientProfile.cs
@@ -8,7 +8,7 @@ public class ClientProfile
     public required string? ClientProfileName { get; set; }
     public required Token Token { get; set; }
     public bool IsFavorite { get; set; }
+    public string? CustomData { get; set; }
     public bool IsForAccount { get; set; }
     public bool IsBuiltIn { get; set; }
-
 }

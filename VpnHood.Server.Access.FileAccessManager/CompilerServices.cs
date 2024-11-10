5f797fc131e7a7a426c9188e00766c34ebdfaa47
Trudy <trudy@vpnhood.com>
2024-11-09 18:53:46 -0800

    Refactor session handling and update client profile
    decuple File Access Manager

---
+++ VpnHood.Server.Access.FileAccessManager/CompilerServices.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable CS9113 // Parameter is unread.
+// ReSharper disable UnusedMember.Global
+// ReSharper disable RedundantTypeDeclarationBody
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+public static class IsExternalInit
+{
+}
+
+public class RequiredMemberAttribute : Attribute
+{
+}
+
+public class CompilerFeatureRequiredAttribute : Attribute
+{
+    public CompilerFeatureRequiredAttribute(string name)
+    {
+    }
+}
--- VpnHood.Server.Access/Managers/IAccessManager.cs
+++ VpnHood.Server.Access/Managers/IAccessManager.cs
@@ -10,6 +10,7 @@ public interface IAccessManager : IDisposable
     bool IsMaintenanceMode { get; }
     Task<SessionResponseEx> Session_Create(SessionRequestEx sessionRequestEx);
     Task<SessionResponseEx> Session_Get(ulong sessionId, IPEndPoint hostEndPoint, IPAddress? clientIp);
+    Task<SessionResponseEx[]> Session_GetAll();
     Task<SessionResponse> Session_AddUsage(ulong sessionId, Traffic traffic, string? adData);
     Task<SessionResponse> Session_Close(ulong sessionId, Traffic traffic);
     Task<ServerCommand> Server_UpdateStatus(ServerStatus serverStatus);

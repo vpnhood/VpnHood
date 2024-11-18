bb08b4941ead84ec68047690af7698c9947624c6
Trudy <trudy@vpnhood.com>
2024-11-18 14:05:29 -0800

    Refactor and update project structure and build scripts
    Refactored `AppOptions` to use a new constructor with `appId` parameter and replaced `DefaultStorageFolderPath` with `BuildStorageFolderPath` method. Updated `TestHelper` and

--- Tests/VpnHood.Test/Tests/AccessTest.cs
+++ Tests/VpnHood.Test/Tests/AccessTest.cs
@@ -14,12 +14,6 @@ namespace VpnHood.Test.Tests;
 public class AccessTest : TestBase
 {
     [TestMethod]
-    public async Task AFoo()
-    {
-        await Task.Delay(0);
-    }
-
-    [TestMethod]
     public async Task Server_reject_invalid_requests()
     {
         await using var server = await TestHelper.CreateServer();

06254133a235e109b1a0fc1c3f1f8a95d4381834
Trudy <trudy@vpnhood.com>
2024-11-20 03:26:42 -0800

    reset location for no server available error
    Former-commit-id: 3d3315d2aa0de5a037fda2b3ef4e27277d72e50b

--- VpnHood.Client.App/ClientProfiles/ClientProfile.cs
+++ VpnHood.Client.App/ClientProfiles/ClientProfile.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using VpnHood.Common.Tokens;
+﻿using VpnHood.Common.Tokens;
 
 namespace VpnHood.Client.App.ClientProfiles;
 
@@ -8,19 +7,10 @@ public class ClientProfile
     public required Guid ClientProfileId { get; init; }
     public required string? ClientProfileName { get; set; }
     public required Token Token { get; set; }
-    
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public bool IsFavorite { get; set; }
-    
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public string? CustomData { get; set; }
-    
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public bool IsPremiumLocationSelected { get; set; }
-    
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? SelectedLocation{ get; set; }
     public bool IsForAccount { get; set; }
-    
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public bool IsBuiltIn { get; set; }
 }

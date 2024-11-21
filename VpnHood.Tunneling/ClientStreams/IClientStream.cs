6854c8f19a3e0b612a4c401994ba35d2425ac711
Trudy <trudy@vpnhood.com>
2024-11-20 16:02:08 -0800

    Refactored response writing and client stream disposal by introducing new extension methods `WriteResponse` and `WriteFinalResponse`. Added `RequireHttpResponse` property to `IClientStream` interface and its implementation.
    Former-commit-id: 55a05e04ef1d393f7aa8f741bbd29d36e9766b77

--- VpnHood.Tunneling/ClientStreams/IClientStream.cs
+++ VpnHood.Tunneling/ClientStreams/IClientStream.cs
@@ -6,6 +6,7 @@ public interface IClientStream : IAsyncDisposable
 {
     bool Disposed { get; }
     string ClientStreamId { get; set; }
+    bool RequireHttpResponse { get; set; }
     IPEndPointPair IpEndPointPair { get; }
     Stream Stream { get; }
     bool CheckIsAlive();

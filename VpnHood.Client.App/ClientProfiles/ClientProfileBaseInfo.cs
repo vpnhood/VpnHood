ea8dc525fb198e6933a61051b04ae3bda214d4c5
Trudy <trudy@vpnhood.com>
2024-11-21 11:46:58 -0800

    Update JSON serialization in AccessTest and ClientProfile
    Former-commit-id: c4459cf97b89d9f0529f8f9c1d8aab36460d5c3e

--- VpnHood.Client.App/ClientProfiles/ClientProfileBaseInfo.cs
+++ VpnHood.Client.App/ClientProfiles/ClientProfileBaseInfo.cs
@@ -8,16 +8,12 @@ public class ClientProfileBaseInfo(ClientProfile clientProfile)
     public Guid ClientProfileId { get; private set; } = clientProfile.ClientProfileId;
     public string ClientProfileName { get; private set; } = GetTitle(clientProfile);
     
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public string? SupportId { get; private set; } = clientProfile.Token.SupportId;
     
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public string? CustomData { get; private set; } = clientProfile.CustomData;
     
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public bool IsPremiumLocationSelected { get; private set; } = clientProfile.IsPremiumLocationSelected;
     
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public bool IsPremiumAccount { get; private set; } = !clientProfile.Token.IsPublic;
 
     private static string GetTitle(ClientProfile clientProfile)

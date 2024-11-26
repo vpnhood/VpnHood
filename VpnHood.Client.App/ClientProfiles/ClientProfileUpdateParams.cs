eab408ab2e1b503abb584dfd43cf2f5b986836af
Trudy <trudy@vpnhood.com>
2024-11-26 12:16:00 -0800

    add SelectedLocation to ClientProfile
    Former-commit-id: e75b1390c1a8b1dc4e512ffcd82cc53821e3ca1b

--- VpnHood.Client.App/ClientProfiles/ClientProfileUpdateParams.cs
+++ VpnHood.Client.App/ClientProfiles/ClientProfileUpdateParams.cs
@@ -7,5 +7,6 @@ public class ClientProfileUpdateParams
     public Patch<string?>? ClientProfileName { get; set; }
     public Patch<bool>? IsFavorite { get; set; }
     public Patch<bool>? IsPremiumLocationSelected { get; set; }
+    public Patch<string>? SelectedLocation { get; set; }
     public Patch<string?>? CustomData { get; set; }
 }

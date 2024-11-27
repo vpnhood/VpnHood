4c2732458e78621fae9f7d6ec7a39ea8aa4f581e
Trudy <trudy@vpnhood.com>
2024-11-26 23:48:04 -0800

    make DefaultAppResource lazy

--- Tests/VpnHood.Test/Tests/AccessTest.cs
+++ Tests/VpnHood.Test/Tests/AccessTest.cs
@@ -67,6 +67,7 @@ public class AccessTest : TestBase
     public async Task Server_reject_expired_access_at_runtime()
     {
         var fileAccessManagerOptions = TestHelper.CreateFileAccessManagerOptions();
+        fileAccessManagerOptions.SessionOptions.SyncInterval = TimeSpan.FromMinutes(10); // make sure the disconnect is not due to sync
         await using var server = await TestHelper.CreateServer(fileAccessManagerOptions);
 
         // create a short expiring token

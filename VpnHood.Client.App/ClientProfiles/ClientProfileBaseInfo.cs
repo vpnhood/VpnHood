f57295fc226b7d32bb65b341db959230a3472bd7
Trudy <trudy@vpnhood.com>
2024-11-28 10:02:28 -0800

    Changed IsPremiumLocationSelected property type to nullable boolean in several files
    Former-commit-id: 5c366d401dfff83134a8ab5bdaeadff12c93843c

--- VpnHood.Client.App/ClientProfiles/ClientProfileBaseInfo.cs
+++ VpnHood.Client.App/ClientProfiles/ClientProfileBaseInfo.cs
@@ -6,7 +6,7 @@ public class ClientProfileBaseInfo
     public required string ClientProfileName { get; init; }
     public required string? SupportId { get; init; }
     public required string? CustomData { get; init; }
-    public required bool IsPremiumLocationSelected { get; init; }
+    public required bool? IsPremiumLocationSelected { get; init; }
     public required bool IsPremiumAccount { get; init; }
     public required ClientServerLocationInfo? SelectedLocationInfo { get; init; }
 }

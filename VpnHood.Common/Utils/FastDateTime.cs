801e03d2d16d62dd9cf6ce4b1605cf4da83eb775
Trudy <trudy@vpnhood.com>
2024-11-29 00:23:34 -0800

    Add UtcNow to FastDateTime and Synch DisposeAsyncEx for VpnHoodClient
    Former-commit-id: fbb45a384e36ebc1dcbcc4f18b6033b3558eab0f

--- VpnHood.Common/Utils/FastDateTime.cs
+++ VpnHood.Common/Utils/FastDateTime.cs
@@ -5,14 +5,16 @@ public static class FastDateTime
     private static readonly object Locker = new();
     private static DateTime _lastTime = DateTime.Now;
     private static int _lastTickCount = Environment.TickCount;
+    private static DateTime _lastTimeUtc = DateTime.UtcNow;
+    private static int _lastTickCountUtc = Environment.TickCount;
+
     public static TimeSpan Precision { get; set; } = TimeSpan.FromSeconds(1);
 
     public static DateTime Now {
         get {
             lock (Locker) {
                 var tickCount = Environment.TickCount;
-                if (tickCount - _lastTickCount >= Precision.Milliseconds ||
-                    tickCount < _lastTickCount) {
+                if (tickCount - _lastTickCount >= Precision.Milliseconds || tickCount < _lastTickCount) {
                     _lastTime = DateTime.Now;
                     _lastTickCount = tickCount;
                 }
@@ -21,4 +23,19 @@ public static class FastDateTime
             }
         }
     }
+
+    public static DateTime UtcNow {
+        get {
+            lock (Locker) {
+                var tickCount = Environment.TickCount;
+                if (tickCount - _lastTickCountUtc >= Precision.Milliseconds || tickCount < _lastTickCountUtc) {
+                    _lastTimeUtc = DateTime.UtcNow;
+                    _lastTickCountUtc = tickCount;
+                }
+
+                return _lastTimeUtc;
+            }
+        }
+    }
+
 }

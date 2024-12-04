3b682b838054e173631a8fba1265fd0910783472
Trudy <trudy@vpnhood.com>
2024-12-03 21:50:05 -0800

    Add DnsResolver class and update DNS resolution logic
    Former-commit-id: 609470e0d353d8cd318ef2d6d3c732fc835a977e

--- VpnHood.Client/Diagnosing/DiagnoseUtil.cs
+++ VpnHood.Client/Diagnosing/DiagnoseUtil.cs
@@ -1,7 +1,6 @@
 ﻿using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Text;
 using Microsoft.Extensions.Logging;
 using VpnHood.Common.Logging;
 using VpnHood.Common.Utils;
@@ -78,7 +77,7 @@ public class DiagnoseUtil
                 "UdpTest: {UdpTestStatus}, DnsName: {DnsName}, NsServer: {NsServer}, Timeout: {Timeout}...",
                 "Started", dnsName, nsIpEndPoint, timeout);
 
-            var res = await GetHostEntry(dnsName, nsIpEndPoint, udpClient, timeout).VhConfigureAwait();
+            var res = await DnsResolver.GetHostEntry(dnsName, nsIpEndPoint, udpClient, timeout).VhConfigureAwait();
             if (res.AddressList.Length == 0)
                 throw new Exception("Could not find any host!");
 
@@ -122,86 +121,5 @@ public class DiagnoseUtil
                 "Failed", logIpAddress, ex.Message);
             return ex;
         }
-    }
-
-    public static async Task<IPHostEntry> GetHostEntry(string host, IPEndPoint dnsEndPoint,
-        UdpClient? udpClient = null, int timeout = 5000)
-    {
-        // prepare  udpClient
-        using var udpClientTemp = new UdpClient();
-        udpClient ??= udpClientTemp;
-
-        await using var ms = new MemoryStream();
-        var rnd = new Random();
-        //About the dns message:http://www.ietf.org/rfc/rfc1035.txt
-
-        //Write message header.
-        ms.Write([
-            (byte)rnd.Next(0, 0xFF), (byte)rnd.Next(0, 0xFF),
-            0x01,
-            0x00,
-            0x00, 0x01,
-            0x00, 0x00,
-            0x00, 0x00,
-            0x00, 0x00
-        ], 0, 12);
-
-        //Write the host to query.
-        foreach (var block in host.Split('.')) {
-            var data = Encoding.UTF8.GetBytes(block);
-            ms.WriteByte((byte)data.Length);
-            ms.Write(data, 0, data.Length);
-        }
-
-        ms.WriteByte(0); //The end of query, must be 0 (null string)
-
-        //Query type:A
-        ms.WriteByte(0x00);
-        ms.WriteByte(0x01);
-
-        //Query class:IN
-        ms.WriteByte(0x00);
-        ms.WriteByte(0x01);
-
-        //send to dns server
-        var buffer = ms.ToArray();
-        udpClient.Client.SendTimeout = timeout;
-        udpClient.Client.ReceiveTimeout = timeout;
-        await udpClient.SendAsync(buffer, buffer.Length, dnsEndPoint).VhConfigureAwait();
-        var receiveTask = await VhUtil.RunTask(udpClient.ReceiveAsync(), TimeSpan.FromMilliseconds(timeout))
-            .VhConfigureAwait();
-        buffer = receiveTask.Buffer;
-
-        //The response message has the same header and question structure, so we move index to the answer part directly.
-        var index = (int)ms.Length;
-
-        //Parse response records.
-        void SkipName()
-        {
-            while (index < buffer.Length) {
-                int length = buffer[index++];
-                if (length == 0)
-                    return;
-                if (length > 191) return;
-                index += length;
-            }
-        }
-
-        var addresses = new List<IPAddress>();
-        while (index < buffer.Length) {
-            SkipName(); //Seems the name of record is useless in this case, so we just need to get the next index after name.
-            var type = buffer[index += 2];
-            index += 7; //Skip class and ttl
-
-            var length = (buffer[index++] << 8) | buffer[index++]; //Get record data length
-
-            if (type == 0x01) //A record
-                if (length == 4) //Parse record data to ip v4, this is what we need.
-                    addresses.Add(new IPAddress(new[]
-                        { buffer[index], buffer[index + 1], buffer[index + 2], buffer[index + 3] }));
-            index += length;
-        }
-
-        return new IPHostEntry { AddressList = addresses.ToArray() };
     }
 }

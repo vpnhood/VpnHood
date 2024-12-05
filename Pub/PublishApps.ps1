a06be29230a6cc47e082b31a9e9f2edd76407439
Trudy <trudy@vpnhood.com>
2024-12-05 00:21:52 -0800

    fix wind setup for connect
    Former-commit-id: 06d1d4cb6c7b23557185ee570091c0b49d8336fb

--- Pub/PublishApps.ps1
+++ Pub/PublishApps.ps1
@@ -1,8 +1,10 @@
 param( 
 	[Parameter(Mandatory=$true)][object]$bump,
 	[Parameter(Mandatory=$true)][object]$nugets,
-	[Parameter(Mandatory=$true)][object]$winClient,
-	[Parameter(Mandatory=$true)][object]$android, 
+	[Parameter(Mandatory=$true)][object]$clientWin,
+	[Parameter(Mandatory=$true)][object]$clientAndroid,
+	[Parameter(Mandatory=$true)][object]$connectWin,
+	[Parameter(Mandatory=$true)][object]$connectAndroid,
 	[Parameter(Mandatory=$true)][object]$maui,
 	[Parameter(Mandatory=$true)][object]$server,
 	[Parameter(Mandatory=$true)][object]$distribute,
@@ -10,10 +12,11 @@ param(
 	);
 
 $nugets = $nugets -eq "1";
-$android = $android -eq "1";
-$androidConnect = $androidConnect -eq "1";
+$connectWin = $connectWin -eq "1";
+$connectAndroid = $connectAndroid -eq "1";
+$clientWin = $clientWin -eq "1";
+$clientAndroid = $clientAndroid -eq "1";
 $distribute = $distribute -eq "1";
-$winClient = $winClient -eq "1";
 $server = $server -eq "1";
 $samples = $samples -eq "1";
 $maui = $maui -eq "1";
@@ -28,41 +31,45 @@ $noPushNuget = !$nugets
 Remove-Item "$packagesRootDir/ReleaseNote.txt" -ErrorAction Ignore;
 
 # rebuild libraries
-& "$solutionDir/VpnHood.Common/_publish.ps1";
-& "$solutionDir/VpnHood.Tunneling/_publish.ps1";
+if ($nugets) {
+	& "$solutionDir/VpnHood.Common/_publish.ps1";
+	& "$solutionDir/VpnHood.Tunneling/_publish.ps1";
+	& "$solutionDir/VpnHood.Client/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.Device.Android/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.Device/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.Device.WinDivert/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Abstractions/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Resources/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.WebServer/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Store/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Android.Common/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Android.GooglePlay/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Android.GooglePlay.Core/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Android.Ads.AdMob/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Win.Common/_publish.ps1";
+	& "$solutionDir/VpnHood.Client.App.Win.Common.WpfSpa/_publish.ps1";
+	& "$solutionDir/VpnHood.Server/_publish.ps1";
+	& "$solutionDir/VpnHood.Server.Access/_publish.ps1";
+}
 
-& "$solutionDir/VpnHood.Client/_publish.ps1";
-& "$solutionDir/VpnHood.Client.Device.Android/_publish.ps1";
-& "$solutionDir/VpnHood.Client.Device/_publish.ps1";
-& "$solutionDir/VpnHood.Client.Device.WinDivert/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Abstractions/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Resources/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.WebServer/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Store/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Android.Common/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Android.GooglePlay/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Android.GooglePlay.Core/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Android.Ads.AdMob/_publish.ps1";
-& "$solutionDir/VpnHood.Client.App.Win.Common/_publish.ps1";
-& "$solutionDir/VpnHood.Server/_publish.ps1";
-& "$solutionDir/VpnHood.Server.Access/_publish.ps1";
-
-# publish client
-if ($maui)
-{
+# publish MAUI nugets
+if ($maui) {
 	& "$solutionDir/VpnHood.Client.App.Maui.Common/_publish.ps1";
 }
 
-# publish client
-if ($winClient)
-{
+# publish win client
+if ($clientWin) {
 	& "$solutionDir/VpnHood.Client.App.Win/_publish.ps1";
 }
 
+# publish win client
+if ($connectWin) {
+	& "$solutionDir/VpnHood.Client.App.Win.Connect/_publish.ps1";
+}
+
 # publish server
-if ($server)
-{	
+if ($server) {	
 	& "$solutionDir/VpnHood.Server.App.Net/Pub/publish_win.ps1";
 	& "$solutionDir/VpnHood.Server.App.Net/Pub/publish_linux_x64.ps1";
 	& "$solutionDir/VpnHood.Server.App.Net/Pub/publish_linux_arm64.ps1";
@@ -70,28 +77,24 @@ if ($server)
 }
 
 # publish android
-if ($android)
-{	
+if ($clientAndroid) {	
 	& "$solutionDir/VpnHood.Client.App.Android/_publish_aab.ps1";
 	& "$solutionDir/VpnHood.Client.App.Android/_publish_apk.ps1";
 }
 
 # publish android
-if ($androidConnect)
-{	
+if ($connectAndroid) {	
 	& "$solutionDir/VpnHood.Client.App.Android.connect/_publish_aab.ps1";
 }
 
 
 # distribute
-if ($distribute)
-{
+if ($distribute) {
 	& "$PSScriptRoot/PublishToGitHub.ps1";
 }
 
 
 # update and push samples nugets
-if ($samples)
-{
+if ($samples) {
 	& "$solutionDir/../VpnHood.Client.Samples/UpdateAndPush.ps1";
 }

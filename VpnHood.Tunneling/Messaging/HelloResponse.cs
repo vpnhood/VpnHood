054b1f01dc4baec366ad0275ac0861f0bac6e6cb
Trudy <trudy@vpnhood.com>
2024-12-05 13:13:49 -0800

    move AccessKey to SessionResponse
    Former-commit-id: f2a7a0e39d59790e2aefa668b533dcf9d22fd052

--- VpnHood.Tunneling/Messaging/HelloResponse.cs
+++ VpnHood.Tunneling/Messaging/HelloResponse.cs
@@ -32,7 +32,6 @@ public class HelloResponse : SessionResponse
     public string? GaMeasurementId { get; init; }
     public TimeSpan RequestTimeout { get; init; } = TimeSpan.FromSeconds(60);
     public TimeSpan TcpReuseTimeout { get; init; } = TimeSpan.FromSeconds(60);
-    public string? AccessKey { get; set; }
     public AdRequirement AdRequirement { get; set; } = AdRequirement.None;
     public string? ServerLocation { get; set; }
     public string[] ServerTags { get; set; } = [];

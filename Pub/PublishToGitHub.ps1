f28c66ee9b9e823f78ac3ee1583605497c267900
Trudy <trudy@vpnhood.com>
2024-12-13 01:29:15 -0800

    fix github publish
    Former-commit-id: f75ab49347baa4b902b07d276e288659315e85ee

--- Pub/PublishToGitHub.ps1
+++ Pub/PublishToGitHub.ps1
@@ -3,6 +3,9 @@ param(
 	[Parameter(Mandatory=$true)][object]$connectRepo
 	);
 
+$mainRepo = $mainRepo -eq "1";
+$connectRepo = $connectRepo -eq "1";
+
 . "$PSScriptRoot/Core/Common.ps1"
 
 # update CHANGELOG
@@ -22,56 +25,89 @@ $releaseNote = $changeLog;
 $releaseNote = $releaseNote.SubString($releaseNote.IndexOf("`n")); # remove version tag
 $releaseNote = $releaseNote.SubString(0, $releaseNote.IndexOf("`n# ")); # remove other version
 $releaseNote | Out-File -FilePath "$packagesRootDir/ReleaseNote.txt" -Encoding utf8 -Force -NoNewline;
-if ($isLatest)
-{
+if ($isLatest) {
 	$releaseNote | Out-File -FilePath "$packagesRootDirLatest/ReleaseNote.txt" -Encoding utf8 -Force -NoNewline;
 }
 
-# Publishing to GitHub
-Push-Location -Path "$solutionDir";
+#-----------
+# CLIENT REPO
+#-----------
+if ($mainRepo -and $false) {
+	Write-Host "*** Publish MainRepo releases" -BackgroundColor Blue
 
-# commit and push git
-$gitDir = "$solutionDir/.git";
-gh release delete "$versionTag" --cleanup-tag --yes;
-git --git-dir=$gitDir --work-tree=$solutionDir tag --delete "$versionTag";
-git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish v$versionParam";
-git --git-dir=$gitDir --work-tree=$solutionDir pull;
-git --git-dir=$gitDir --work-tree=$solutionDir push;
+	# Publishing to GitHub
+	Push-Location -Path "$solutionDir";
 
-# swtich to main branch
-if (!$prerelease)
-{
-	git --git-dir=$gitDir --work-tree=$solutionDir checkout main
+	# commit and push git
+	$gitDir = "$solutionDir/.git";
+	gh release delete "$versionTag" --cleanup-tag --yes;
+	git --git-dir=$gitDir --work-tree=$solutionDir tag --delete "$versionTag";
+	git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish v$versionParam";
 	git --git-dir=$gitDir --work-tree=$solutionDir pull;
-	git --git-dir=$gitDir --work-tree=$solutionDir merge development;
 	git --git-dir=$gitDir --work-tree=$solutionDir push;
-	git --git-dir=$gitDir --work-tree=$solutionDir checkout development
+
+	# swtich to main branch
+	if (!$prerelease) {
+		git --git-dir=$gitDir --work-tree=$solutionDir checkout main
+		git --git-dir=$gitDir --work-tree=$solutionDir pull;
+		git --git-dir=$gitDir --work-tree=$solutionDir merge development;
+		git --git-dir=$gitDir --work-tree=$solutionDir push;
+		git --git-dir=$gitDir --work-tree=$solutionDir checkout development
+	}
+
+	# publish using github CLI: https://github.com/github/hub
+	$androidGoogleLatestDir = Join-Path $pubDir "Android.GooglePlay/apk/latest";
+
+	gh release create "$versionTag" `
+		--title "$versionTag" `
+		(&{if($prerelease) {"--prerelease"} else {"--latest"}}) `
+		-F $releaseRootDir/ReleaseNote.txt `
+		$androidGoogleLatestDir/VpnHoodClient-android.apk `
+		$androidGoogleLatestDir/VpnHoodClient-android.json `
+		$releaseRootDir/$packageClientDirName/android-web/VpnHoodClient-android-web.apk `
+		$releaseRootDir/$packageClientDirName/android-web/VpnHoodClient-android-web.json `
+		$releaseRootDir/$packageClientDirName/windows-web/VpnHoodClient-win-x64.msi  `
+		$releaseRootDir/$packageClientDirName/windows-web/VpnHoodClient-win-x64.txt  `
+		$releaseRootDir/$packageClientDirName/windows-web/VpnHoodClient-win-x64.json `
+		$releaseRootDir/$packageServerDirName/linux-x64/VpnHoodServer-linux-x64.json `
+		$releaseRootDir/$packageServerDirName/linux-x64/VpnHoodServer-linux-x64.sh `
+		$releaseRootDir/$packageServerDirName/linux-x64/VpnHoodServer-linux-x64.tar.gz `
+		$releaseRootDir/$packageServerDirName/linux-arm64/VpnHoodServer-linux-arm64.json `
+		$releaseRootDir/$packageServerDirName/linux-arm64/VpnHoodServer-linux-arm64.sh `
+		$releaseRootDir/$packageServerDirName/linux-arm64/VpnHoodServer-linux-arm64.tar.gz `
+		$releaseRootDir/$packageServerDirName/win-x64/VpnHoodServer-win-x64.json `
+		$releaseRootDir/$packageServerDirName/win-x64/VpnHoodServer-win-x64.ps1 `
+		$releaseRootDir/$packageServerDirName/win-x64/VpnHoodServer-win-x64.zip `
+		$releaseRootDir/$packageServerDirName/docker/VpnHoodServer.docker.yml `
+		$releaseRootDir/$packageServerDirName/docker/VpnHoodServer.docker.sh;
+
+	Pop-Location
 }
 
-# publish using github CLI: https://github.com/github/hub
-$androidGoogleLatestDir = Join-Path $pubDir "Android.GooglePlay/apk/latest";
+#-----------
+# CONNECT REPO
+#-----------
+if ($connectRepo) {
+	Write-Host "*** Publish MainRepo releases" -BackgroundColor Blue
 
-gh release create "$versionTag" `
-	--title "$versionTag" `
-	(&{if($prerelease) {"--prerelease"} else {"--latest"}}) `
-	-F $releaseRootDir/ReleaseNote.txt `
-	$androidGoogleLatestDir/VpnHoodClient-android.apk `
-	$androidGoogleLatestDir/VpnHoodClient-android.json `
-	$releaseRootDir/$packageClientDirName/android-web/VpnHoodClient-android-web.apk `
-	$releaseRootDir/$packageClientDirName/android-web/VpnHoodClient-android-web.json `
-	$releaseRootDir/$packageClientDirName/windows-web/VpnHoodClient-win-x64.msi  `
-	$releaseRootDir/$packageClientDirName/windows-web/VpnHoodClient-win-x64.txt  `
-	$releaseRootDir/$packageClientDirName/windows-web/VpnHoodClient-win-x64.json `
-	$releaseRootDir/$packageServerDirName/linux-x64/VpnHoodServer-linux-x64.json `
-	$releaseRootDir/$packageServerDirName/linux-x64/VpnHoodServer-linux-x64.sh `
-	$releaseRootDir/$packageServerDirName/linux-x64/VpnHoodServer-linux-x64.tar.gz `
-	$releaseRootDir/$packageServerDirName/linux-arm64/VpnHoodServer-linux-arm64.json `
-	$releaseRootDir/$packageServerDirName/linux-arm64/VpnHoodServer-linux-arm64.sh `
-	$releaseRootDir/$packageServerDirName/linux-arm64/VpnHoodServer-linux-arm64.tar.gz `
-	$releaseRootDir/$packageServerDirName/win-x64/VpnHoodServer-win-x64.json `
-	$releaseRootDir/$packageServerDirName/win-x64/VpnHoodServer-win-x64.ps1 `
-	$releaseRootDir/$packageServerDirName/win-x64/VpnHoodServer-win-x64.zip `
-	$releaseRootDir/$packageServerDirName/docker/VpnHoodServer.docker.yml `
-	$releaseRootDir/$packageServerDirName/docker/VpnHoodServer.docker.sh;
+	# set Connect Variables
+	$connectRepoDir = Join-Path $vhDir "VpnHood.Client.App.Connect";
+	$androidGoogleLatestDir = Join-Path $connectRepoDir "pub/Android.GooglePlay/apk/latest";
+	
+	# Publishing to GitHub
+	Push-Location -Path $connectRepoDir;
 
-Pop-Location
+	gh release create "$versionTag" `
+		--title "$versionTag" `
+		(&{if($prerelease) {"--prerelease"} else {"--latest"}}) `
+		-F $releaseRootDir/ReleaseNote.txt `
+		$androidGoogleLatestDir/VpnHoodConnect-android.apk `
+		$androidGoogleLatestDir/VpnHoodConnect-android.json `
+		$releaseRootDir/$packageConnectDirName/android-web/VpnHoodConnect-android-web.apk `
+		$releaseRootDir/$packageConnectDirName/android-web/VpnHoodConnect-android-web.json `
+		$releaseRootDir/$packageConnectDirName/windows-web/VpnHoodConnect-win-x64.msi `
+		$releaseRootDir/$packageConnectDirName/windows-web/VpnHoodConnect-win-x64.txt `
+		$releaseRootDir/$packageConnectDirName/windows-web/VpnHoodConnect-win-x64.json;
+	
+	Pop-Location
+}

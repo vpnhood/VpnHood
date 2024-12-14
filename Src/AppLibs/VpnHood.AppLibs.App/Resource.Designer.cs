465fa4d98c677a3dbb713e51677366467dffcb59
Trudy <trudy@vpnhood.com>
2024-12-14 14:05:40 -0800

    Restructure
    Former-commit-id: 3a9143cec29d27bb65551b7a2c77adc4ab398aac [formerly 7225e53a3eabc3ad6b73a028385bf6c2f15409dd]
    Former-commit-id: a376b911441e06779870303962f6fa91a3065e70 [formerly 0f42fa1ac37be9e778cad3a5c10a197d21c0b7c0]
    Former-commit-id: 21afd87a3e7281e341733e7abf9d898b9858e4ce

--- Src/AppLibs/VpnHood.AppLibs.App/Resource.Designer.cs
+++ Src/AppLibs/VpnHood.AppLibs.App/Resource.Designer.cs
@@ -39,7 +39,7 @@ namespace VpnHood.AppLibs {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VpnHood.AppLibs.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VpnHood.AppLibs.App.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

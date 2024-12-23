7b3cf54b0ee0d2b4cc3c4cc96c8ac15f191562b5
Trudy <trudy@vpnhood.com>
2024-12-22 22:44:20 -0800

    improve logging

--- Src/Core/VpnHood.Core.Tunneling/GeneralEventId.cs
+++ Src/Core/VpnHood.Core.Tunneling/GeneralEventId.cs
@@ -4,10 +4,9 @@ namespace VpnHood.Core.Tunneling;
 
 public static class GeneralEventId
 {
-    public static EventId Essential = new((int)EventCode.Essential, nameof(Essential));
     public static EventId Session = new((int)EventCode.Session, nameof(Session));
+    public static EventId Essential = new((int)EventCode.Essential, nameof(Essential));
     public static EventId Sni = new((int)EventCode.Sni, nameof(Sni));
-    public static EventId SessionTrack = new((int)EventCode.SessionTrack, nameof(SessionTrack));
     public static EventId Nat = new((int)EventCode.Nat, nameof(Nat));
     public static EventId Ping = new((int)EventCode.Ping, nameof(Ping));
     public static EventId Dns = new((int)EventCode.Dns, nameof(Dns));
@@ -15,17 +14,20 @@ public static class GeneralEventId
     public static EventId Tls = new((int)EventCode.Tls, nameof(Tls));
     public static EventId Udp = new((int)EventCode.Udp, nameof(Udp));
     public static EventId Packet = new((int)EventCode.Packet, nameof(Packet));
-    public static EventId Track = new((int)EventCode.Track, nameof(Track));
     public static EventId StreamProxyChannel = new((int)EventCode.StreamChannel, nameof(StreamProxyChannel));
     public static EventId DatagramChannel = new((int)EventCode.DatagramChannel, EventCode.DatagramChannel.ToString());
-    public static EventId AccessManager = new((int)EventCode.AccessManager, nameof(AccessManager));
-    public static EventId NetProtect = new((int)EventCode.NetProtect, nameof(NetProtect));
-    public static EventId NetFilter = new((int)EventCode.NetFilter, nameof(NetFilter));
     public static EventId Request = new((int)EventCode.Request, nameof(Request));
     public static EventId TcpLife = new((int)EventCode.TcpLife, nameof(TcpLife));
     public static EventId Test = new((int)EventCode.Test, nameof(Test));
     public static EventId UdpSign = new((int)EventCode.UdpSign, nameof(UdpSign));
+
+    // server
+    public static EventId AccessManager = new((int)EventCode.AccessManager, nameof(AccessManager));
+    public static EventId NetProtect = new((int)EventCode.NetProtect, nameof(NetProtect));
+    public static EventId NetFilter = new((int)EventCode.NetFilter, nameof(NetFilter));
     public static EventId DnsChallenge = new((int)EventCode.DnsChallenge, nameof(DnsChallenge));
+    public static EventId SessionTrack = new((int)EventCode.SessionTrack, nameof(SessionTrack));
+    public static EventId Track = new((int)EventCode.Track, nameof(Track));
 
     private enum EventCode
     {

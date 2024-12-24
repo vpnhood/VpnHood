182b56072741c8aac67a28d1c7c7ea29e5e6d348
Trudy <trudy@vpnhood.com>
2024-12-23 16:57:57 -0800

    Add MemInfo property and PremiumByCode option
    Updated CHANGELOG.md to reflect recent changes and fixes.
    
    
    Former-commit-id: 984da98de851e18e5e885925d77037f613ff2b2d [formerly 919005fd8375bf50e8294ea55ee8426a24f535b8]
    Former-commit-id: a7e126f73f3e686acf0940270ebbcc5e7fadaaf4 [formerly 77c8f1c13dbe6d499b0427aecf6c19f7f36d5935]
    Former-commit-id: 0bc4ea1368c07149033d65a6f2d3cef84afb5d8e

--- Src/AppLib/VpnHood.AppLib.App/ClientProfiles/ServerLocationOptions.cs
+++ Src/AppLib/VpnHood.AppLib.App/ClientProfiles/ServerLocationOptions.cs
@@ -6,6 +6,7 @@ public class ServerLocationOptions
     public int? PremiumByTrial { get; set; }
     public int? PremiumByRewardedAd { get; set; }
     public bool PremiumByPurchase { get; set; }
+    public bool PremiumByCode { get; set; }
     public bool HasFree { get; set; }
     public bool HasPremium { get; set; }
     public bool HasUnblockable { get; set; }
---
+++ Src/Core/VpnHood.Core.Client.Device/DeviceMemInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace VpnHood.Core.Client.Device;
+
+public class DeviceMemInfo
+{
+    public required long TotalMemory { get; init; }
+    public required long AvailableMemory { get; init; }
+}

54adfa493c5b2fb370bca34b686545750f9632b8
Trudy <trudy@vpnhood.com>
2024-12-27 01:16:22 -0800

    Publish v5.0.627
    Former-commit-id: 8df200ae36acf2169b49e1c0cba1595571b7f4d5 [formerly 9bbc4c2ed8db0e9ac04165d6ce7cd91bcf781615]
    Former-commit-id: 7a2d21bc0f6f0a714f9596ac02ea9014f81da260 [formerly 97a251052922614ae6040be052217a116db603d4]
    Former-commit-id: 12e5c5e14d030f9b56645363695f34e3f8e6a648

--- CHANGELOG.Server.md
+++ CHANGELOG.Server.md
@@ -1,7 +1,7 @@
-# v5.0.622
+# v5.0.627
 * Update: Move releases repo from https://github.com/vpnhood/VpnHood/releases to https://github.com/vpnhood/VpnHood.App.Server/releases
 
-# v5.0.622
+# v5.0.627
 Old log: https://github.com/vpnhood/VpnHood/blob/main/CHANGELOG.md
 
 
--- Pub/Publish.App.Server.ps1
+++ Pub/Publish.App.Server.ps1
@@ -1,41 +1,14 @@
 param( 
 	[Parameter(Mandatory=$true)][object]$bump,
-	[Parameter(Mandatory=$true)][object]$nugets,
-	[Parameter(Mandatory=$true)][object]$distribute,
+	[Parameter(Mandatory=$true)][object]$distribute
 	);
 
-$nugets = $nugets -eq "1";
 $distribute = $distribute -eq "1";
 
 . "$PSScriptRoot/Core/Common.ps1" -bump $bump
 
 # clean all
-Remove-Item "$packagesRootDir/ReleaseNote.txt" -ErrorAction Ignore;
-
-# rebuild libraries
-if ($nugets) {
-	& "$solutionDir/Src/Core/VpnHood.Core.Common/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Tunneling/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Client/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Client.Device.Android/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Client.Device/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Client.Device.WinDivert/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Server/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Server.Access/_publish.ps1";
-	& "$solutionDir/Src/Core/VpnHood.Core.Server.Access.FileAccessManager/_publish.ps1";
-
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Abstractions/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.App/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Resources/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.WebServer/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Store/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Android.Common/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Android.GooglePlay/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Android.GooglePlay.Core/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Android.Ads.AdMob/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Win.Common/_publish.ps1";
-	& "$solutionDir/Src/AppLib/VpnHood.AppLib.Win.Common.WpfSpa/_publish.ps1";
-}
+Remove-Item "$packagesRootDir/ReleaseNote.Server.txt" -ErrorAction Ignore;
 
 & "$solutionDir/Src/Apps/Server.Net/Pub/publish_win.ps1";
 & "$solutionDir/Src/Apps/Server.Net/Pub/publish_linux_x64.ps1";

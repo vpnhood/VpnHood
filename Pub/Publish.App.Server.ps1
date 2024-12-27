f5ffdb286a211adbfe3abb7e93f259cad4256c95
Trudy <trudy@vpnhood.com>
2024-12-27 01:50:45 -0800

    Publish v5.0.628

--- Pub/Publish.App.Server.ps1
+++ Pub/Publish.App.Server.ps1
@@ -7,8 +7,8 @@ $distribute = $distribute -eq "1";
 
 . "$PSScriptRoot/Core/Common.ps1" -bump $bump
 
-# clean all
-Remove-Item "$packagesRootDir/ReleaseNote.Server.txt" -ErrorAction Ignore;
+# remove old release note
+Remove-Item "$packagesRootDir/$packageServerDirName/ReleaseNote.txt" -ErrorAction Ignore;
 
 & "$solutionDir/Src/Apps/Server.Net/Pub/publish_win.ps1";
 & "$solutionDir/Src/Apps/Server.Net/Pub/publish_linux_x64.ps1";

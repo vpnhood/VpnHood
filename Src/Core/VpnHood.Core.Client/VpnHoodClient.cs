7023d162e8cbad3dc8ad05ece72466b373bae23b
petervhood <peter@vpnhood.com>
2024-12-30 00:00:26 +0330

    Update client profile on subscription expiration. IsPacketCaptureIpFilterAvailable to the user setting.
    Former-commit-id: a639fe7022f2355522abfb3ab0bb4af3602a8bd2 [formerly 989be400b9c95a4f5c62475f83ca3dc95420a02a]
    Former-commit-id: b76f2e4fd4aad9499351d80a1e2af7afef15ad36 [formerly 5faa82ca93dc893dd2179d40c7c18935c8990fd9]
    Former-commit-id: 6748507d53cdf6ccc38f5b5a564826c3b3be2b2d

--- Src/Core/VpnHood.Core.Client/VpnHoodClient.cs
+++ Src/Core/VpnHood.Core.Client/VpnHoodClient.cs
@@ -88,6 +88,7 @@ public class VpnHoodClient : IJob, IAsyncDisposable
     public bool IncludeLocalNetwork { get; }
     public IpRangeOrderedList IncludeIpRanges { get; private set; }
     public IpRangeOrderedList PacketCaptureIncludeIpRanges { get; private set; }
+    public bool IsPacketCaptureIpFilterAvailable { get; set; }
     public string UserAgent { get; }
     public IPEndPoint? HostTcpEndPoint => _connectorService?.EndPointInfo.TcpEndPoint;
     public IPEndPoint? HostUdpEndPoint { get; private set; }

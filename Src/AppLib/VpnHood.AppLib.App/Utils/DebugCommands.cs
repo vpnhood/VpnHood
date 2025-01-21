93299e0a66cf4072f591f18ad66a818123ab5fc2
Trudy <trudy@vpnhood.com>
2025-01-21 01:26:24 -0800

    UseTcpOverTun feature
    Added new debug commands in DebugCommands.cs.
    Added UseTcpOverTun property in ClientOptions.cs and VpnHoodClient.cs.
    Updated logging and packet handling logic in VpnHoodClient.cs.
    
    
    Former-commit-id: a488e007d431d40f1e0af1b5a88e836febeb3e7e [formerly 1edaf51b2ab9e5d665de19ffd220f1652cb5b71b]
    Former-commit-id: 6ded9801a2b13576c3d40b97a95dbe8cc4af843a [formerly 3510b45f2b714dd077f4360ef93b6b0deeb463be]
    Former-commit-id: 71a38a3f17765c6c3ed4a604e9ba4ae552aee4a6

--- Src/AppLib/VpnHood.AppLib.App/Utils/DebugCommands.cs
+++ Src/AppLib/VpnHood.AppLib.App/Utils/DebugCommands.cs
@@ -2,19 +2,21 @@
 
 public static class DebugCommands
 {
+    public const string CaptureContext = "/capture-context";
     public const string DropUdp = "/drop-udp";
-    public const string NoTcpReuse = "/no-tcp-reuse";
+    public const string FullLog = "/full-log";
     public const string KillSpaServer = "/kill-spa-server";
+    public const string UseTcpOverTun = "/tcp-over-tun";
     public const string Verbose = "/verbose";
     public const string NullCapture = "/null-capture";
-    public const string CaptureContext = "/capture-context";
-    public const string FullLog = "/full-log";
+    public const string NoTcpReuse = "/no-tcp-reuse";
 
     public static string[] All => [
         CaptureContext,
         DropUdp, 
         FullLog,
-        KillSpaServer, 
+        KillSpaServer,
+        UseTcpOverTun,
         NoTcpReuse, 
         NullCapture,
         Verbose

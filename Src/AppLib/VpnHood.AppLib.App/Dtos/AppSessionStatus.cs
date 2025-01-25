e04603519902871f1e02173314e5a8389152300d
Trudy <trudy@vpnhood.com>
2025-01-25 11:38:14 -0800

    add AppSessionStatus
    Former-commit-id: 34d1508cf0f1dc8882d180969a4203c1d66c10e1 [formerly 28053cb6a9d800a6887a67b5f2a92e544506ac95]
    Former-commit-id: 538bb3a32abfb32e768776057e64a73305024e95 [formerly cb4ff155b022c6c54a4aeac0c091ab972ba1ef0a]
    Former-commit-id: f86039b31984ebde4fa37b4b0e008895570ed069

---
+++ Src/AppLib/VpnHood.AppLib.App/Dtos/AppSessionStatus.cs
@@ -0,0 +1,43 @@
+﻿using VpnHood.Core.Client.Abstractions;
+using VpnHood.Core.Client.ConnectorServices;
+using VpnHood.Core.Common.Messaging;
+
+namespace VpnHood.AppLib.Dtos;
+
+public class AppSessionStatus
+{
+    public required ConnectorStat ConnectorStat { get; init; }
+    public required Traffic Speed { get; init; }
+    public required Traffic SessionTraffic { get; init; }
+    public required Traffic CycleTraffic { get; init; }
+    public required Traffic TotalTraffic { get; init; }
+    public required int TcpTunnelledCount { get; init; }
+    public required int TcpPassthruCount { get; init; }
+    public required int DatagramChannelCount { get; init; }
+    public required bool IsUdpMode { get; init; }
+    public required bool IsWaitingForAd { get; init; }
+    public required bool CanExtendByRewardedAd { get; init; }
+    public required long SessionMaxTraffic { get; init; }
+    public required DateTime? SessionExpirationTime { get; init; }
+    public required int? ActiveClientCount { get; init; }
+
+    public static AppSessionStatus Create(ISessionStatus sessionStatus)
+    {
+        return new AppSessionStatus {
+            ConnectorStat = sessionStatus.ConnectorStat,
+            Speed = sessionStatus.Speed,
+            SessionTraffic = sessionStatus.SessionTraffic,
+            CycleTraffic = sessionStatus.CycleTraffic,
+            TotalTraffic = sessionStatus.TotalTraffic,
+            TcpTunnelledCount = sessionStatus.TcpTunnelledCount,
+            TcpPassthruCount = sessionStatus.TcpPassthruCount,
+            DatagramChannelCount = sessionStatus.DatagramChannelCount,
+            IsUdpMode = sessionStatus.IsUdpMode,
+            IsWaitingForAd = sessionStatus.IsWaitingForAd,
+            CanExtendByRewardedAd = sessionStatus.CanExtendByRewardedAd,
+            SessionMaxTraffic = sessionStatus.SessionMaxTraffic,
+            SessionExpirationTime = sessionStatus.SessionExpirationTime,
+            ActiveClientCount = sessionStatus.ActiveClientCount
+        };
+    }
+}

abbf0088e22759f4a5584d6122758dabd34293d6
Trudy <trudy@vpnhood.com>
2025-02-02 15:29:38 -0800

    ClientController

---
+++ Src/Core/VpnHood.Core.Client.Controller/ClientController.cs
@@ -0,0 +1,42 @@
+﻿using VpnHood.Core.Client.Device;
+using VpnHood.Core.Common.Tokens;
+
+namespace VpnHood.Core.Client.Abstractions;
+
+public class ClientController
+{
+    private readonly VpnHoodClient _client;
+    private ClientController(VpnHoodClient client)
+    {
+        _client = client;
+    }
+
+    public static ClientController Create(IPacketCapture packetCapture, string clientId, 
+        Token token, ClientOptions clientOptions)
+    {
+        var client = new VpnHoodClient(packetCapture, clientId, token, clientOptions);
+        return new ClientController(client);
+    }
+
+    public Task Start(ClientOptions clientOptions, CancellationToken cancellationToken)
+    {
+        return _client.Connect(cancellationToken);
+    }
+
+    public Task Stop()
+    {
+        return _client.DisposeAsync().AsTask();
+    }
+
+    public Task Reconfigure()
+    {
+        throw new NotImplementedException();
+
+    }
+
+    public ConnectionInfo GetConnectionInfo()
+    {
+        // connect to the server and get the connection info
+        throw new NotImplementedException();
+    }
+}

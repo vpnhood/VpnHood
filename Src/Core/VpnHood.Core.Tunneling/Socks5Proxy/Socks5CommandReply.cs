4ef7abe664167ecb528b8eae482fa40cc0af6152
Trudy <trudy@vpnhood.com>
2025-02-04 21:51:56 -0800

    Add Socks5Proxy but not usign it yet
    Former-commit-id: f8c0b1c910dc52eeb03663214da4b05f88045017 [formerly d02b42eb36923a15726e18b9d2495d319373f309]
    Former-commit-id: 0be15f161613a267050bc33e99db395d93efc06b [formerly 0942a4a0fa2945b192f065906639013a9799ec41]
    Former-commit-id: b6d1740d70a62b88739b98cdaea038566f7c8227

---
+++ Src/Core/VpnHood.Core.Tunneling/Socks5Proxy/Socks5CommandReply.cs
@@ -0,0 +1,14 @@
+namespace VpnHood.Core.Tunneling.Socks5Proxy;
+
+public enum Socks5CommandReply : byte
+{
+    Succeeded = 0x00,
+    GeneralSocksServerFailure = 0x01,
+    ConnectionNotAllowedByRuleset = 0x02,
+    NetworkUnreachable = 0x03,
+    HostUnreachable = 0x04,
+    ConnectionRefused = 0x05,
+    TtlExpired = 0x06,
+    CommandNotSupported = 0x07,
+    AddressTypeNotSupported = 0x08
+}

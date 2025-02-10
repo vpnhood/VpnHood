6c71aff5c9d24c644cb9e2042efdcd67e19e6a02
Trudy <trudy@vpnhood.com>
2025-02-09 23:21:30 -0800

    fix IpFilters temporary
    Former-commit-id: 2b4db430dd820b0ac78f82981a52dbb39eaf6f85 [formerly 0871c2e93b4c600c1cb79cba62b743784a1be485]
    Former-commit-id: 59d921714f77a956f10acedde697b467ba2eaf8b [formerly 5e2d3d7829c7d96a41caa23eaca32b9fd193d28c]
    Former-commit-id: 6ed5d41dd3e16f6db6fe024090c3f47aaa86718e

--- Src/AppLib/VpnHood.AppLib.WebServer/Api/IpFilters.cs
+++ Src/AppLib/VpnHood.AppLib.WebServer/Api/IpFilters.cs
@@ -1,8 +1,12 @@
-﻿namespace VpnHood.AppLib.WebServer.Api;
+﻿using System.Text.Json.Serialization;
+
+namespace VpnHood.AppLib.WebServer.Api;
 
 public class IpFilters
 {
+    [JsonPropertyName("packetCaptureIpFilterInclude")] //todo
     public required string VpnAdapterIpFilterInclude { get; set; }
+    [JsonPropertyName("packetCaptureIpFilterExclude")] //todo
     public required string VpnAdapterIpFilterExclude { get; set; }
     public required string AppIpFilterInclude { get; set; }
     public required string AppIpFilterExclude { get; set; }

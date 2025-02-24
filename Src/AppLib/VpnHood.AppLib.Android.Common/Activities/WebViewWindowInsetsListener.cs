751dd2ca937344877800ddfce7809be2ecd1a224
Trudy <trudy@vpnhood.com>
2025-02-23 16:23:05 -0800

    fix androd insets compatibility

--- Src/AppLib/VpnHood.AppLib.Android.Common/Activities/WebViewWindowInsetsListener.cs
+++ Src/AppLib/VpnHood.AppLib.Android.Common/Activities/WebViewWindowInsetsListener.cs
@@ -7,20 +7,28 @@ internal class WebViewWindowInsetsListener : Java.Lang.Object, View.IOnApplyWind
 {
     public WindowInsets OnApplyWindowInsets(View v, WindowInsets insets)
     {
-        // Get the insets for the IME and system bars
-        var mask = WindowInsets.Type.Ime();
-        if (VpnHoodApp.Instance.Features.AdjustForSystemBars)
-            mask |= WindowInsets.Type.SystemBars();
-
-        // Apply padding to prevent layout overlap with system bars
-        var rect = insets.GetInsets(mask);
-        v.SetPadding(rect.Left, rect.Top, rect.Right, rect.Bottom);
-
         // Set the background color
         var backgroundColor = VpnHoodApp.Instance.Resource.Colors.WindowBackgroundColor?.ToAndroidColor();
         if (backgroundColor != null)
             v.SetBackgroundColor(backgroundColor.Value);
 
-        return WindowInsets.Consumed; // Consume the insets
+        if (OperatingSystem.IsAndroidVersionAtLeast(30)) {
+            // Get the insets for the IME and system bars
+            var mask = 0;
+            if (insets.IsVisible(WindowInsets.Type.Ime()))
+                mask |= WindowInsets.Type.Ime();
+
+            // Adjust for system bars
+            if (VpnHoodApp.Instance.Features.AdjustForSystemBars)
+                mask |= WindowInsets.Type.SystemBars();
+
+            // Apply padding to prevent layout overlap with system bars
+            var rect = insets.GetInsets(mask);
+            v.SetPadding(rect.Left, rect.Top, rect.Right, rect.Bottom);
+            return insets; // Consume the insets
+
+        }
+
+        return insets; // Consume the insets
     }
 }

85f7269b8cd2eda4f8e88459cf60cf16b694c47c
Trudy <trudy@vpnhood.com>
2025-03-08 01:38:42 -0800

    cleanup

--- Src/Core/VpnHood.Core.VpnAdapters.WinTun/WinTunVpnAdapter.cs
+++ Src/Core/VpnHood.Core.VpnAdapters.WinTun/WinTunVpnAdapter.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 using System.IO.Compression;
 using System.Net;
-using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using Microsoft.Extensions.Logging;
 using PacketDotNet;

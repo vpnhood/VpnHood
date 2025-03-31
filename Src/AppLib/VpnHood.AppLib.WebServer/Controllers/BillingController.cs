fa3fc791e46f31cfeb5a7373d09bcb86c6f0c43a
Trudy <trudy@vpnhood.com>
2025-03-31 15:30:25 -0700

    Change /purchase-options route to use GET method
    Updated the BillingController to change the HTTP method for the "/purchase-options" route from POST to GET. This modification alters how clients interact with the GetPurchaseOptions method, now requiring a GET request instead of a POST request.

--- Src/AppLib/VpnHood.AppLib.WebServer/Controllers/BillingController.cs
+++ Src/AppLib/VpnHood.AppLib.WebServer/Controllers/BillingController.cs
@@ -26,7 +26,7 @@ internal class BillingController : WebApiController, IBillingController
         return BillingService.Purchase(AppUiContext.RequiredContext, planId);
     }
 
-    [Route(HttpVerbs.Post, "/purchase-options")]
+    [Route(HttpVerbs.Get, "/purchase-options")]
     public Task<AppPurchaseOptions> GetPurchaseOptions()
     {
         return VpnHoodApp.Instance.GetPurchaseOptions();

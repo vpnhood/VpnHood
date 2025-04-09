549fb231da8c09f4674b73d51ef7c83c9ca5f1c6
Trudy <trudy@vpnhood.com>
2025-04-08 19:52:34 -0700

    Publish v6.0.688

--- Pub/Core/Common.ps1
+++ Pub/Core/Common.ps1
@@ -77,11 +77,8 @@ function PushMainRepo()
 
 	# swtich to main branch
 	if (!$prerelease) {
-		git --git-dir=$gitDir --work-tree=$solutionDir checkout main
-		git --git-dir=$gitDir --work-tree=$solutionDir pull;
-		git --git-dir=$gitDir --work-tree=$solutionDir merge development;
-		git --git-dir=$gitDir --work-tree=$solutionDir push;
-		git --git-dir=$gitDir --work-tree=$solutionDir checkout development
+		Write-Host "Pushing to main branch..." -ForegroundColor Magenta;
+		git --git-dir=$gitDir --work-tree=$solutionDir push origin development:main;
 	}
 
 	Pop-Location	

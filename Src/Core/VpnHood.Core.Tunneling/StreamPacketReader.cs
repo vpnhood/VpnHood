41347b9f22f9db3f3b61bec3b9d93ad65c6fa348
Trudy <trudy@vpnhood.com>
2025-05-21 01:29:52 -0700

    Improve packet reading in StreamPacketReader using MemoryPool

--- Src/Core/VpnHood.Core.Tunneling/StreamPacketReader.cs
+++ Src/Core/VpnHood.Core.Tunneling/StreamPacketReader.cs
@@ -1,4 +1,5 @@
-﻿using VpnHood.Core.Packets;
+﻿using System.Buffers;
+using VpnHood.Core.Packets;
 using VpnHood.Core.Toolkit.Utils;
 
 namespace VpnHood.Core.Tunneling;
@@ -6,28 +7,38 @@ namespace VpnHood.Core.Tunneling;
 public class StreamPacketReader(Stream stream, int bufferSize)
 {
     private readonly ReadCacheStream _stream = new(stream, true, bufferSize);
-    private readonly Memory<byte> _packetBuffer = new byte[2000];
+    private readonly Memory<byte> _minHeader = new byte[20];
 
     /// <returns>null if read nothing</returns>
     public async Task<IpPacket?> ReadAsync(CancellationToken cancellationToken)
     {
 
         // read minimum packet header
-        const int minPacketSize = 20; // works for ipv4 and ipv6
-        await _stream.ReadExactAsync(_packetBuffer[..minPacketSize], cancellationToken);
+        await _stream.ReadExactAsync(_minHeader, cancellationToken);
 
         // read packet length
-        var packetLength = PacketUtil.ReadPacketLength(_packetBuffer.Span);
+        var packetLength = PacketUtil.ReadPacketLength(_minHeader.Span);
 
         // check packet length
-        if (packetLength > _packetBuffer.Length) 
+        if (packetLength > TunnelDefaults.MaxPacketSize) 
             throw new Exception($"Stream has been closed due an oversize packet. PacketLength: {packetLength}");
 
-        // read the rest of the packet
-        await _stream.ReadExactAsync(_packetBuffer[minPacketSize..packetLength], cancellationToken);
+        var memoryOwner = MemoryPool<byte>.Shared.Rent(packetLength);
+        try {
+            // copy the minimum header to the memory owner
+            _minHeader.CopyTo(memoryOwner.Memory);
 
-        // build the packet
-        var ipPacket = PacketBuilder.Parse(_packetBuffer.Span[..packetLength]);
-        return ipPacket;
+            // read the rest of the packet
+            await _stream.ReadExactAsync(memoryOwner.Memory[_minHeader.Length..packetLength], cancellationToken);
+
+            // build the packet
+            var ipPacket = PacketBuilder.Attach(memoryOwner);
+            return ipPacket;
+        }
+        catch{
+            memoryOwner.Dispose();
+            throw;
+        }
+
     }
 }

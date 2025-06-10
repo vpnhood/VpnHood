6987e4029fe369418c41695b05ba6385377738e3
Trudy <trudy@vpnhood.com>
2025-06-09 17:36:12 -0700

    Refactor linker settings and type preservation logic
    Removed `System.Net.Primitives` entries from `Linker.xml` and updated project files to eliminate the `TrimmerRootDescriptor`. Extensive cleanup in `CompilerServices.cs` by removing unused compiler attributes and classes. Added `AotPreserveHelper.cs` to ensure runtime preservation of specific types (`IPEndPoint`, `IPAddress`, `Version`, `TimeSpan`) during AOT compilation with a new `PreserveTypes` method.

---
+++ Src/Core/VpnHood.Core.Toolkit/AotPreserveHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+namespace VpnHood.Core.Toolkit;
+
+public static class AotPreserveHelper
+{
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPEndPoint))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPAddress))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Version))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(TimeSpan))]
+    public static void PreserveTypes() { }
+}

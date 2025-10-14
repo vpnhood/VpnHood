52f92fc3fa7927d2a717cbb7a72ea473f3ff30e6
Trudy <trudy@vpnhood.com>
2025-10-14 12:39:57 -0700

    Add DoNotParallelize attribute and assembly info comment
    Added a comment in `AssemblyInfo.cs` to explain the automatic
    handling of assembly attributes in SDK-style projects and
    provided a link for further customization details.
    
    Introduced the `[assembly: DoNotParallelize]` attribute to
    prevent parallel execution of tests or processes within the
    assembly.

---
+++ Tests/VpnHood.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+[assembly: DoNotParallelize]

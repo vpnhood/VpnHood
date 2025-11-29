d8b2b95a2cbfdc679f40a1207ab1e4c791cf8335
Trudy <trudy@vpnhood.com>
2025-11-29 00:28:25 -0800

    Remove `VpnHood.NetTester` project and all related files
    The `VpnHood.NetTester` project, including its client and server components, testing utilities (HTTP, TCP, QUIC, UDP), logging, argument parsing, and supporting infrastructure, has been completely removed.

--- Tests/VpnHood.NetTester/Testers/IStreamTesterClient.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace VpnHood.NetTester.Testers;
-
-public interface IStreamTesterClient
-{
-    public Task Start(long upSize, long downSize, int connectionCount, CancellationToken cancellationToken);
-}

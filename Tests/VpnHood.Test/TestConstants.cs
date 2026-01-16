38633dacf4b5e6d892b14c08e6103b12cb265b3d
Trudy <trudy@vpnhood.com>
2026-01-16 11:37:32 -0800

    Increase default HTTP timeout in tests to 3 seconds
    Raised DefaultHttpTimeout from 1s to 3s in TestConstants. This also increases DefaultUdpTimeout and DefaultPingTimeout, as they are derived from the HTTP timeout value.

--- Tests/VpnHood.Test/TestConstants.cs
+++ Tests/VpnHood.Test/TestConstants.cs
@@ -5,7 +5,7 @@ namespace VpnHood.Test;
 
 public static class TestConstants
 {
-    public static TimeSpan DefaultHttpTimeout => TimeSpan.FromSeconds(1).WhenNoDebugger();
+    public static TimeSpan DefaultHttpTimeout => TimeSpan.FromSeconds(3).WhenNoDebugger();
     public static TimeSpan DefaultUdpTimeout => DefaultHttpTimeout;
     public static TimeSpan DefaultPingTimeout => DefaultUdpTimeout;
 

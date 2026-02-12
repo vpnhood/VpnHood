a76f654327d5e8596750acb3c799455d31c42e07
Trudy <trudy@vpnhood.com>
2026-02-11 17:32:38 -0800

    Refactor test network to loopback IP mapping logic
    Simplified mapping by introducing explicit test IP constants for remote and local addresses (IPv4/IPv6). Replaced byte manipulation with direct address checks. Added methods for recovering test network addresses from loopback, and for mapping IpPacket objects with checksum updates. Introduced helper functions for address identification, improving clarity and maintainability.

--- Src/Core/VpnHood.Core.Tunneling/Utils/TunnelUtils.cs
+++ Src/Core/VpnHood.Core.Tunneling/Utils/TunnelUtils.cs
@@ -1,52 +1,133 @@
 ﻿using System.Net;
 using VpnHood.Core.Packets;
+using VpnHood.Core.Packets.Extensions;
 using VpnHood.Core.Toolkit.Net;
 
 namespace VpnHood.Core.Tunneling.Utils;
 
 public static class TunnelUtils
 {
+    public const int TestPortMin = 49150;
+    public const int TestPortMax = 49190;
+    public static IPAddress RemoteTestIpV4 = IPAddress.Parse("198.18.11.1");
+    public static IPAddress RemoteTestIpV6 = IPAddress.Parse("2001:db8::1");
+    public static IPAddress LocalTestIpV4 = IPAddress.Loopback;
+    public static IPAddress LocalTestIpV6 = IPAddress.IPv6Loopback;
+
+
     // conver TestNetwork IPs to loopback IPs, such as 198.18.x.x to 127.10.x.x,
     // to make sure the packets can be captured and return correctly in the test environment.
     public static IPAddress MapTestNetworkToLoopback(IPAddress ipAddress)
     {
-        if (!ipAddress.IsTestNetwork())
+        if (!IsRemoteTestAddress(ipAddress))
             return ipAddress;
 
-        if (ipAddress.IsV4()) {
-            var bytes = ipAddress.GetAddressBytes();
-            bytes[0] = 127;
-            bytes[1] = (byte)(bytes[1] - 8);
-            return new IPAddress(bytes);
-        }
+        if (ipAddress.Equals(RemoteTestIpV4))
+            return LocalTestIpV4;
 
-        if (ipAddress.IsV6())
-            return IPAddress.IPv6Loopback;
+        if (ipAddress.Equals(RemoteTestIpV6))
+            return LocalTestIpV6;
 
         return ipAddress;
     }
 
+    public static IPAddress RecoverTestNetworkFromLoopback(IPAddress ipAddress)
+    {
+        if (!IsLocalTestAddress(ipAddress))
+            return ipAddress;
+
+        if (ipAddress.Equals(LocalTestIpV4))
+            return RemoteTestIpV4;
+
+        if (ipAddress.Equals(LocalTestIpV6))
+            return RemoteTestIpV6;
+
+        return ipAddress;
+    }
+
+
     public static IPEndPoint MapTestNetworkToLoopback(IPEndPoint ipEndPoint)
     {
-        if (!ipEndPoint.Address.IsTestNetwork())
+        if (!IsRemoteTestAddress(ipEndPoint.Address))
             return ipEndPoint;
 
-        if (ipEndPoint.IsV4()) {
-            var bytes = ipEndPoint.Address.GetAddressBytes();
-            bytes[0] = 127;
-            bytes[1] = (byte)(bytes[1] - 8);
-            return new IPEndPoint(new IPAddress(bytes), ipEndPoint.Port);
-        }
+        if (ipEndPoint.Address.Equals(RemoteTestIpV4))
+            return new IPEndPoint(LocalTestIpV4, ipEndPoint.Port);
 
-        if (ipEndPoint.IsV6())
-            return new IPEndPoint(IPAddress.IPv6Loopback, ipEndPoint.Port);
+        if (ipEndPoint.Address.Equals(RemoteTestIpV6))
+            return new IPEndPoint(LocalTestIpV6, ipEndPoint.Port);
 
         return ipEndPoint;
     }
 
     public static IpPacket MapTestNetworkToLoopback(IpPacket ipPacket)
     {
+        if (!IsRemoteTestAddress(ipPacket.SourceAddress) && !IsRemoteTestAddress(ipPacket.DestinationAddress))
+            return ipPacket;
 
+        var isUpdated = false;
+        if (ipPacket.SourceAddress.Equals(RemoteTestIpV4)) {
+            ipPacket.SourceAddress = LocalTestIpV4;
+            isUpdated = true;
+        }
+        else if (ipPacket.SourceAddress.Equals(RemoteTestIpV6)) {
+            ipPacket.SourceAddress = LocalTestIpV6;
+            isUpdated = true;
+        }
+
+        if (ipPacket.DestinationAddress.Equals(RemoteTestIpV4)) {
+            ipPacket.DestinationAddress = LocalTestIpV4;
+            isUpdated = true;
+        }
+        else if (ipPacket.DestinationAddress.Equals(RemoteTestIpV6)) {
+            ipPacket.DestinationAddress = LocalTestIpV6;
+            isUpdated = true;
+        }
+
+        if (isUpdated)
+            ipPacket.UpdateAllChecksums();
+
+        return ipPacket;
+    }
+
+    public static IpPacket RecoverTestNetworkToLoopback(IpPacket ipPacket)
+    {
+        if (!IsLocalTestAddress(ipPacket.SourceAddress) && !IsLocalTestAddress(ipPacket.DestinationAddress))
+            return ipPacket;
+
+        var isUpdated = false;
+        if (ipPacket.SourceAddress.Equals(LocalTestIpV4)) {
+            ipPacket.SourceAddress = RemoteTestIpV4;
+            isUpdated = true;
+        }
+        else if (ipPacket.SourceAddress.Equals(LocalTestIpV6)) {
+            ipPacket.SourceAddress = RemoteTestIpV6;
+            isUpdated = true;
+        }
+
+        if (ipPacket.DestinationAddress.Equals(LocalTestIpV4)) {
+            ipPacket.DestinationAddress = RemoteTestIpV4;
+            isUpdated = true;
+        }
+        else if (ipPacket.DestinationAddress.Equals(LocalTestIpV6)) {
+            ipPacket.DestinationAddress = RemoteTestIpV6;
+            isUpdated = true;
+        }
+
+        if (isUpdated)
+            ipPacket.UpdateAllChecksums();
+
+        return ipPacket;
+    }
+
+    private static bool IsRemoteTestAddress(IPAddress ipAddress)
+    {
+        return ipAddress.Equals(RemoteTestIpV4) || ipAddress.Equals(RemoteTestIpV6);
+    }
+
+    private static bool IsLocalTestAddress(IPAddress ipAddress)
+    {
+        return ipAddress.Equals(LocalTestIpV4) || ipAddress.Equals(LocalTestIpV6);
     }
 
 

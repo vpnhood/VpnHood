eeb9305436d600e4b380fbb893a4d5a4d1282e82
Trudy <trudy@vpnhood.com>
2026-02-27 10:59:51 -0800

    Add AppServerLocationInfo with country translation support
    Introduced AppServerLocationInfo class inheriting from ServerLocationInfo, adding a TranslatedCountryName property for localized country names. Updated AppDtoConverterExtensions to convert ServerLocationInfo to AppServerLocationInfo using a new ToAppDto extension method. Added necessary using statements for new functionality.

---
+++ Src/AppLib/VpnHood.AppLib.App/Dtos/AppServerLocationInfo.cs
@@ -0,0 +1,17 @@
+﻿using VpnHood.Core.Common.Tokens;
+
+namespace VpnHood.AppLib.Dtos;
+
+public class AppServerLocationInfo : ServerLocationInfo
+{
+    public string TranslatedCountryName => VpnHoodApp.Instance.Services.LocationService
+        .TryGetCountryInfo(CountryCode)?.TranslatedName ?? CountryName;
+    public static AppServerLocationInfo FromInfo(ServerLocationInfo serverLocationInfo)
+    {
+        return new AppServerLocationInfo {
+            CountryCode = serverLocationInfo.CountryCode,
+            RegionName = serverLocationInfo.RegionName,
+            Tags = serverLocationInfo.Tags,
+        };
+    }
+}

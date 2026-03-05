29963b18bd2ed13c14b652f7ef27697235af51f4
Trudy <trudy@vpnhood.com>
2026-03-05 09:56:22 -0800

    Enhance AOT preservation for key types and converters
    Expanded AotPreserveHelper to cover more types and converters with DynamicDependency attributes, made it non-static, and ensured its method is called from VhLogger to prevent trimming. Added DynamicDependency to IpRange constructors for better AOT compatibility.

--- Src/Core/VpnHood.Core.Toolkit/AotPreserveHelper.cs
+++ Src/Core/VpnHood.Core.Toolkit/AotPreserveHelper.cs
@@ -1,22 +1,42 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Net;
+using System.Runtime.CompilerServices;
 using VpnHood.Core.Toolkit.Converters;
+using VpnHood.Core.Toolkit.Net;
 
 namespace VpnHood.Core.Toolkit;
 
-public static class AotPreserveHelper
+public class AotPreserveHelper
 {
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPEndPoint))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPAddress))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Version))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(TimeSpan))]
+
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ArrayConverter<,>))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(NullToEmptyArrayConverter<>))]
+
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPAddressConverter))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPEndPointConverter))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(NullToEmptyArrayConverter<>))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(TimeSpanConverter))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(VersionConverter))]
-    public static void PreserveTypes()
+
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IpRange))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IpRangeConverter))]
+
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IPNetwork))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IpNetwork))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IpNetworkConverter))]
+
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string PreserveTypes()
     {
+        // DynamicDependency attributes are read statically by the trimmer.
+        // This method just needs to be reachable from a non-trimmed call site.
+        _instanceId ??= Guid.NewGuid().ToString();
+        return _instanceId;
     }
+
+    private string? _instanceId;
 }

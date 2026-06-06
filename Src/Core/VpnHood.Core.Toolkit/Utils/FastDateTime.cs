159c1f88386b038f2cb17b0e4e87c85dc49302a0
Trudy <trudy@vpnhood.com>
2026-06-05 23:51:30 -0700

    refactor: migrate FastDateTime to use TickCount64 and remove platform-specific source logic

--- Src/Core/VpnHood.Core.Toolkit/Utils/FastDateTime.cs
+++ Src/Core/VpnHood.Core.Toolkit/Utils/FastDateTime.cs
@@ -1,39 +1,32 @@
 namespace VpnHood.Core.Toolkit.Utils;
 
-// todo: SimplyUse Tick64
 public static class FastDateTime
 {
-    // On iOS (especially inside a NetworkExtension/PacketTunnelProvider sandbox),
-    // accessing TimeZoneInfo.Local — which DateTime.Now relies on — can hang indefinitely.
-    // To stay safe, we use UtcNow as the source on iOS.
-    // ToDo: check AOT
-    private static readonly bool UseUtcSource = false;
-
     private static readonly Lock Locker = new();
-    private static int _lastTickCount = Environment.TickCount;
-    private static int _lastTickCountUtc = Environment.TickCount;
+    private static long _lastTickCount = Environment.TickCount64;
+    private static long _lastTickCountUtc = Environment.TickCount64;
 
     public static TimeSpan Precision { get; set; } = TimeSpan.FromSeconds(1);
 
     public static DateTime Now {
         get {
             lock (Locker) {
-                var tickCount = Environment.TickCount;
-                if (tickCount - _lastTickCount >= Precision.TotalMilliseconds || tickCount < _lastTickCount) {
-                    field = UseUtcSource ? DateTime.UtcNow : DateTime.Now;
+                var tickCount = Environment.TickCount64;
+                if (tickCount - _lastTickCount >= Precision.TotalMilliseconds) {
+                    field = DateTime.Now;
                     _lastTickCount = tickCount;
                 }
 
                 return field;
             }
         }
-    } = UseUtcSource ? DateTime.UtcNow : DateTime.Now;
+    } = DateTime.Now;
 
     public static DateTime UtcNow {
         get {
             lock (Locker) {
-                var tickCount = Environment.TickCount;
-                if (tickCount - _lastTickCountUtc >= Precision.TotalMilliseconds || tickCount < _lastTickCountUtc) {
+                var tickCount = Environment.TickCount64;
+                if (tickCount - _lastTickCountUtc >= Precision.TotalMilliseconds) {
                     field = DateTime.UtcNow;
                     _lastTickCountUtc = tickCount;
                 }
